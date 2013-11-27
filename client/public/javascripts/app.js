3b83ba4e733f3992a8814e41978c4e0fff71e592
Joseph Silvestre <joseph.silvestre38@gmail.com>
2013-11-27 09:36:04 +0100

    Brunched

--- client/public/javascripts/app.js
+++ client/public/javascripts/app.js
@@ -627,6 +627,82 @@ window.require.register("locale/en", function(exports, require, module) {
   };
   
 });
+window.require.register("locale/fr", function(exports, require, module) {
+  module.exports = {
+    "menu_accounts": "Comptes",
+    "menu_balance": "Soldes",
+    "menu_search": "Recherche",
+    "menu_add_bank": "Ajouter des comptes",
+    "overall_balance": "Solde total : ",
+    "add_bank_bank": "Banque",
+    "add_bank_credentials": "Identifiants",
+    "add_bank_login": "Nom d'utilisateur",
+    "add_bank_login_placeholder": "Entrez votre nom d'utilisateur ici",
+    "add_bank_password": "Mot de passe",
+    "add_bank_password_placeholder": "Entrez votre mot de passe ici",
+    "add_bank_security_notice": "Information concernant la sécurité",
+    "add_bank_security_notice_text": "Votre nom d'utilisateur et votre mot de passe sont chiffrés dans la base de données. En conséquence, seules les applications possdéant la permission d'accéder au 'BankAccess' pourront voir ces informations déchiffrés. Soyez sûr que la sécurité est la priorité de cette application.",
+    "add_bank_cancel": "cancel",
+    "add_bank_ok": "Verify & Save",
+    "balance_please_choose_account": "Veuillez sélectionner un compte dans le menu de gauche pour afficher ses opérations.",
+    "balance_banks_empty": "Il n'y a pas de comptes bancaires dans votre Cozy pour l'instant. Ajoutez-en un dès maintenant !",
+    "header_date": "Date",
+    "header_title": "Titre",
+    "header_amount": "Montant",
+    "balance_last_checked": "Dernière vérification",
+    "balance_recheck_now": "Vérifier maintenant.",
+    "search_date_from": "Depuis",
+    "search_date_to": "Jusqu'à",
+    "search_amount_from": "De",
+    "search_amount_to": "A",
+    "search_text": "Le titre contient",
+    "accounts_delete_bank": "supprimer cette banque de Cozy",
+    "accounts_delete_bank_title": "Une confirmation est nécessaire",
+    "accounts_delete_bank_prompt": "Êtes-vous sûr ? Cette opération est irréverisible et supprimera TOUTES les données relatives à cette banque de votre Cozy.",
+    "accounts_delete_bank_confirm": "supprimer définitivement",
+    "accounts_delete_account": "supprimer ce compte de Cozy",
+    "accounts_delete_account_title": "Une confirmation est nécessaire",
+    "accounts_delete_account_prompt": "Êtes-vous sûr ? Cette opération est irréverisible et supprimera TOUTES les données relatives à ce compte de votre Cozy.",
+    "accounts_delete_account_confirm": "supprimer définitivement",
+    "accounts_alerts_title": "Rapports et alertes",
+    "accounts_alerts_title_periodic": "Rapports périodiques",
+    "accounts_alerts_periodic_add": "ajouter au rapport périodique",
+    "accounts_alerts_title_balance": "Alertes sur le solde",
+    "accounts_alerts_balance_add": "ajouter une nouvelle alerte sur le solde du compte",
+    "accounts_alerts_title_transaction": "ALertes sur les transactions",
+    "accounts_alerts_transaction_add": "ajouter une nouvelle alerte sur les transactions",
+    "accounts_alerts_report_text_1": "Ajouter à",
+    "accounts_alerts_report_text_2": "rapport par email.",
+    "accounts_alerts_balance_text_1": "Lorsque le solde est",
+    "accounts_alerts_balance_text_2": "que",
+    "accounts_alerts_transaction_text_1": "Lorsque le montant d'une transaction est",
+    "accounts_alerts_transaction_text_2": "que",
+    "accounts_alerts_daily": "quotidien",
+    "accounts_alerts_monthly": "mensuel",
+    "accounts_alerts_weekly": "hebdomadaire",
+    "accounts_alerts_lower": "plus petit",
+    "accounts_alerts_highier": "plus grand",
+    "accounts_alerts_save": "enregistrer",
+    "accounts_alerts_cancel": "annuler",
+    "accounts_alerts_delete": "supprimer",
+    "alert_sure_delete_bank": "Vous êtes sur le point de supprimer toutes les données lié à cette banque. Cette opération est irréversible. Êtes-vous sûr ?",
+    "alert_sure_delete_account": "Vous êtes sur le point de supprimer toutes les données lié à ce compte. Cette opération est irréversible. Êtes-vous sûr ?",
+    "error_loading_accounts": "Une erreur est survenue lors du charzgement de vos comptes bancaires. Veuillez rafraîchir la page ou rééessayer plus tard.",
+    "fatal_error": "Une erreur inconnue a eu lieu, veuillez rafraîchir la page.",
+    "error_check_credentials_btn": "Echec de la connexion au serveur. Cliquez pour réessayer.",
+    "error_check_credentials": "Nous n'avons pas vu nous connecter au serveur de votre banque. Veuillez vérifier que vos identifiants sont corrects et réessayer à nouveau.",
+    "access already exists": "Vous essayez d'ajouter un accès bancaire déjà existant.",
+    "access already exists button": "Cet accès bancaire existe déjà.",
+    "loading": "chargement en cours...",
+    "verifying": "vérification en cours...",
+    "cancel": "annuler",
+    "removing": "suppression en cours...",
+    "error": "erreur...",
+    "sent": "envoyé avec succès...",
+    "error_refresh": "Une erreur est survenue, veuillez recharger la page et réessayer."
+  };
+  
+});
 window.require.register("models/bank", function(exports, require, module) {
   var Bank, BankAccountsCollection, _ref,
     __hasProp = {}.hasOwnProperty,
@@ -1410,12 +1486,19 @@ window.require.register("views/balance", function(exports, require, module) {
     };
 
     BalanceView.prototype.noMoreEmpty = function() {
-      var _ref1, _ref2;
-      console.log("no more empty");
+      var _ref1, _ref2,
+        _this = this;
       if ((_ref1 = this.$(".arrow")) != null) {
         _ref1.hide();
       }
-      return (_ref2 = this.$(".loading")) != null ? _ref2.hide() : void 0;
+      if ((_ref2 = this.$(".loading")) != null) {
+        _ref2.hide();
+      }
+      return window.collections.banks.fetch({
+        success: function() {
+          return _this.render();
+        }
+      });
     };
 
     BalanceView.prototype.render = function() {
@@ -1975,7 +2058,6 @@ window.require.register("views/new_bank", function(exports, require, module) {
       event.preventDefault();
       view = this;
       button = $(event.target);
-      console.log(button);
       oldText = button.html();
       button.addClass("disabled");
       button.html(window.i18n("verifying") + "<img src='./loader_green.gif' />");
