dc1e42830070ae5eb716ad353922230c2ce05d44
Benjamin Bouvier <public@benj.me>
2014-10-09 22:57:09 +0200

    Flux-ify Chart component.

---
+++ client/components/ChartComponent.js
@@ -0,0 +1,254 @@
+/** @jsx React.DOM */
+
+// Constants
+var Events = require('../Events');
+var debug = require('../Helpers').debug;
+
+// Global variables
+var store = require('../store');
+var flux = require('../flux/dispatcher');
+
+var $chart = null;
+
+function DEBUG(text) {
+    return debug('Chart Component - ' + text);
+}
+
+// Components
+module.exports = React.createClass({
+
+    getInitialState: function() {
+        return {
+            operations: [],
+            categories: []
+        }
+    },
+
+    _redraw: function() {
+        DEBUG('redraw');
+        this.setState({
+            operations: store.operations,
+            categories: store.categories
+        }, this._onChange);
+    },
+
+    componentDidMount: function() {
+        store.on(Events.OPERATIONS_LOADED, this._redraw);
+        store.on(Events.CATEGORIES_LOADED, this._redraw);
+        $chart = $('#chart');
+    },
+
+    componentWillUnmount: function() {
+        store.removeListener(Events.OPERATIONS_LOADED, this._redraw);
+        store.removeListener(Events.CATEGORIES_LOADED, this._redraw);
+    },
+
+    _onChange: function() {
+        var val = this.refs.select.getDOMNode().value;
+        if (val === 'all') {
+            CreateChartAllByCategoryByMonth(this.state.operations);
+            return;
+        }
+        CreateChartByCategoryByMonth(val, this.state.operations);
+    },
+
+    render: function() {
+        var categoryOptions = this.state.categories.map(function (c) {
+            return (<option key={c.id} value={c.id}>{c.title}</option>);
+        });
+
+        return (
+        <div>
+            <h1>Charts</h1>
+            <select onChange={this._onChange} defaultValue='all' ref='select'>
+                <option value='all'>All</option>
+                {categoryOptions}
+            </select>
+            <div id='chart'></div>
+        </div>
+        );
+    }
+});
+
+// Charts
+function CreateChartByCategoryByMonth(catId, operations) {
+    var ops = [];
+    for (var i = 0; i < operations.length; i++) {
+        var op = operations[i];
+        if (op.categoryId === catId)
+            ops.push(op);
+    }
+    CreateChartAllByCategoryByMonth(ops);
+}
+
+function CreateChartAllByCategoryByMonth(operations) {
+    function datekey(op) {
+        var d = op.date;
+        return d.getFullYear() + '-' + d.getMonth();
+    }
+
+    var map = {};
+    var dateset = {};
+    for (var i = 0; i < operations.length; i++) {
+        var op = operations[i];
+        var c = store.categoryToLabel(op.categoryId);
+        map[c] = map[c] || {};
+
+        var dk = datekey(op);
+        map[c][dk] = map[c][dk] || [];
+        map[c][dk].push(op.amount);
+        dateset[dk] = true;
+    }
+
+    var series = [];
+    for (var c in map) {
+        var data = [];
+
+        for (var dk in dateset) {
+            map[c][dk] = map[c][dk] || [];
+            var s = 0;
+            var arr = map[c][dk];
+            for (var i = 0; i < arr.length; i++)
+                s += arr[i];
+            data.push(s);
+        }
+
+        var serie = {
+            name: c,
+            data: data
+        };
+
+        series.push(serie);
+    }
+
+    var categories = [];
+    for (var dk in dateset)
+        categories.push(dk);
+
+    var title = 'By category';
+    var yAxisLegend = 'Amount';
+
+    $chart.highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: title
+        },
+        xAxis: {
+            categories: categories
+        },
+        yAxis: {
+            title: {
+                text: yAxisLegend
+            }
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y:.1f} eur</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: series
+    });
+}
+
+// TODO unused right now
+function CreateChartAllOperations(account, operations) {
+    createChart(account.initialAmount, operations.slice(), account.title);
+}
+
+function CreateChartByCategory(catId, catLabel, operations) {
+    var ops = operations.slice().filter(function(x) {
+        return x.categoryId == catId;
+    });
+
+    createChart(0, ops, catLabel);
+}
+
+function createChart(initialAmount, operations, title) {
+    if (operations.length === 0)
+        return;
+
+    var ops = operations.sort(function (a,b) { return +a.date - +b.date });
+    var cumulativeAmount = initialAmount;
+    // Must contain array pairs [+date, value]
+    var positive = (initialAmount > 0) ? initialAmount : 0;
+    var negative = (initialAmount < 0) ? -initialAmount : 0;
+    var balance = initialAmount;
+
+    var posd = [];
+    var negd = [];
+    var bald = [];
+
+    var opmap = {};
+    var posmap = {};
+    var negmap = {};
+
+    ops.map(function(o) {
+        // Convert date into a number: it's going to be converted into a string
+        // when used as a key.
+        var a = o.amount;
+        var d = +o.date;
+        opmap[d] = opmap[d] || 0;
+        opmap[d] += a;
+
+        if (a < 0) {
+            negmap[d] = negmap[d] || 0;
+            negmap[d] += -a;
+        } else if (a > 0) {
+            posmap[d] = posmap[d] || 0;
+            posmap[d] += a;
+        }
+    })
+
+    for (var date in opmap) {
+        // date is a string now: convert it back to a number for highcharts.
+        balance += opmap[date];
+        bald.push([+date, balance]);
+
+        if (posmap[date]) {
+            positive += posmap[date];
+        }
+        if (negmap[date]) {
+            negative += negmap[date];
+        }
+        posd.push([+date, positive]);
+        negd.push([+date, negative]);
+    }
+
+    // Create the chart
+    $chart.highcharts('StockChart', {
+        rangeSelector : {
+            selected : 1,
+            inputEnabled: $chart.width() > 480
+        },
+
+        title : {
+            text : title
+        },
+
+        series : [{
+            name : 'Balance',
+            data : bald,
+            tooltip: { valueDecimals: 2 }
+        }, {
+            name: 'Credit',
+            data: posd,
+            tooltip: { valueDecimals: 2 }
+        }, {
+            name: 'Debit',
+            data: negd,
+            tooltip: { valueDecimals: 2 }
+        }]
+    });
+}
+
--- client/main.js
+++ client/main.js
@@ -16,6 +16,7 @@ var Events = require('./Events');
 var AccountListComponent = require('./components/AccountListComponent');
 var BankListComponent = require('./components/BankListComponent');
 var CategoryComponent = require('./components/CategoryComponent');
+var ChartComponent = require('./components/ChartComponent');
 var OperationListComponent = require('./components/OperationListComponent');
 var SimilarityComponent = require('./components/SimilarityComponent');
 
@@ -24,46 +25,6 @@ var flux = require('./flux/dispatcher');
 var store = require('./store');
 
 // Now this really begins.
-
-/*
- * React Components
- */
-
-
-// Props: operations, categories
-var ChartComponent = React.createClass({
-
-    _onChange: function() {
-        var val = this.refs.select.getDOMNode().value;
-        if (val === 'all') {
-            CreateChartAllByCategoryByMonth(this.props.operations);
-            return;
-        }
-
-        var c = CategoryMap[val];
-        CreateChartByCategoryByMonth(val, this.props.operations);
-    },
-
-    render: function() {
-        var categoryOptions = this.props.categories.map(function (c) {
-            return (<option key={c.id} value={c.id}>{c.title}</option>);
-        });
-
-        return (
-        <div>
-            <h1>Charts</h1>
-            <select onChange={this._onChange} defaultValue='all' ref='select'>
-                <option value='all'>All</option>
-                {categoryOptions}
-            </select>
-            <div id='chart'></div>
-        </div>
-        );
-    }
-});
-
-var CategoryMap = {};
-
 var Kresus = React.createClass({
 
     getInitialState: function() {
@@ -117,11 +78,7 @@ var Kresus = React.createClass({
                     </div>
 
                     <div className='content' id='panel-charts'>
-                        <ChartComponent
-                            account={this.state.currentAccount}
-                            operations={this.state.operations}
-                            categories={this.state.categories}
-                        />
+                        <ChartComponent />
                     </div>
 
                     <div className='content' id='panel-categories'>
@@ -136,194 +93,4 @@ var Kresus = React.createClass({
     }
 });
 
-var S = document.querySelector.bind(document);
-React.renderComponent(<Kresus />, S('#main'));
-
-
-/*
- * CHARTS
- */
-
-$chart = $('#chart');
-
-function CreateChartByCategoryByMonth(catId, operations) {
-    var ops = [];
-    for (var i = 0; i < operations.length; i++) {
-        var op = operations[i];
-        if (op.categoryId === catId)
-            ops.push(op);
-    }
-    CreateChartAllByCategoryByMonth(ops);
-}
-
-function CreateChartAllByCategoryByMonth(operations) {
-    function datekey(op) {
-        var d = op.date;
-        return d.getFullYear() + '-' + d.getMonth();
-    }
-
-    var map = {};
-    var dateset = {};
-    for (var i = 0; i < operations.length; i++) {
-        var op = operations[i];
-        var c = op.categoryLabel;
-        map[c] = map[c] || {};
-
-        var dk = datekey(op);
-        map[c][dk] = map[c][dk] || [];
-        map[c][dk].push(op.amount);
-        dateset[dk] = true;
-    }
-
-    var series = [];
-    for (var c in map) {
-        var data = [];
-
-        for (var dk in dateset) {
-            map[c][dk] = map[c][dk] || [];
-            var s = 0;
-            var arr = map[c][dk];
-            for (var i = 0; i < arr.length; i++)
-                s += arr[i];
-            data.push(s);
-        }
-
-        var serie = {
-            name: c,
-            data: data
-        };
-
-        series.push(serie);
-    }
-
-    var categories = [];
-    for (var dk in dateset)
-        categories.push(dk);
-
-    var title = 'By category';
-    var yAxisLegend = 'Amount';
-
-    $chart.highcharts({
-        chart: {
-            type: 'column'
-        },
-        title: {
-            text: title
-        },
-        xAxis: {
-            categories: categories
-        },
-        yAxis: {
-            title: {
-                text: yAxisLegend
-            }
-        },
-        tooltip: {
-            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-            '<td style="padding:0"><b>{point.y:.1f} eur</b></td></tr>',
-            footerFormat: '</table>',
-            shared: true,
-            useHTML: true
-        },
-        plotOptions: {
-            column: {
-                pointPadding: 0.2,
-                borderWidth: 0
-            }
-        },
-        series: series
-    });
-}
-
-// TODO unused right now
-function CreateChartAllOperations(account, operations) {
-    createChart(account.initialAmount, operations.slice(), account.title);
-}
-
-function CreateChartByCategory(catId, catLabel, operations) {
-    var ops = operations.slice().filter(function(x) {
-        return x.categoryId == catId;
-    });
-
-    createChart(0, ops, catLabel);
-}
-
-function createChart(initialAmount, operations, title) {
-    if (operations.length === 0)
-        return;
-
-    var ops = operations.sort(function (a,b) { return +a.date - +b.date });
-    var cumulativeAmount = initialAmount;
-    // Must contain array pairs [+date, value]
-    var positive = (initialAmount > 0) ? initialAmount : 0;
-    var negative = (initialAmount < 0) ? -initialAmount : 0;
-    var balance = initialAmount;
-
-    var posd = [];
-    var negd = [];
-    var bald = [];
-
-    var opmap = {};
-    var posmap = {};
-    var negmap = {};
-
-    ops.map(function(o) {
-        // Convert date into a number: it's going to be converted into a string
-        // when used as a key.
-        var a = o.amount;
-        var d = +o.date;
-        opmap[d] = opmap[d] || 0;
-        opmap[d] += a;
-
-        if (a < 0) {
-            negmap[d] = negmap[d] || 0;
-            negmap[d] += -a;
-        } else if (a > 0) {
-            posmap[d] = posmap[d] || 0;
-            posmap[d] += a;
-        }
-    })
-
-    for (var date in opmap) {
-        // date is a string now: convert it back to a number for highcharts.
-        balance += opmap[date];
-        bald.push([+date, balance]);
-
-        if (posmap[date]) {
-            positive += posmap[date];
-        }
-        if (negmap[date]) {
-            negative += negmap[date];
-        }
-        posd.push([+date, positive]);
-        negd.push([+date, negative]);
-    }
-
-    // Create the chart
-    $chart.highcharts('StockChart', {
-        rangeSelector : {
-            selected : 1,
-            inputEnabled: $chart.width() > 480
-        },
-
-        title : {
-            text : title
-        },
-
-        series : [{
-            name : 'Balance',
-            data : bald,
-            tooltip: { valueDecimals: 2 }
-        }, {
-            name: 'Credit',
-            data: posd,
-            tooltip: { valueDecimals: 2 }
-        }, {
-            name: 'Debit',
-            data: negd,
-            tooltip: { valueDecimals: 2 }
-        }]
-    });
-}
-
+React.renderComponent(<Kresus />, document.querySelector('#main'));
