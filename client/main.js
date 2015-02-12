7e983322efafddd48b418067280642d95cc7b91f
Benjamin Bouvier <public@benj.me>
2015-02-12 22:09:55 +0100

    This commit about CSS is so uninteresting that you're not going not to see it. NOOOOO. PLEASE, DON'T! Well, I've told you.

--- client/main.js
+++ client/main.js
@@ -27,11 +27,21 @@ var Kresus = React.createClass({
         // Let's go.
         store.getCategories();
         store.once(Events.server.loaded_categories, store.getAllBanks);
+        store.once(Events.server.loaded_operations, this._adjustSidebarHeight);
+        this._adjustSidebarHeight();
+    },
+
+    _adjustSidebarHeight: function() {
+        // What an horrible hack.  So this triggers rendering two times, just
+        // to make sure the sidebar on the left has the right size as the
+        // entire app.  Bleh.  Don't judge me.
+        $('#sidebar').height(0)
+                     .height($('html').height());
     },
 
     _show: function(name) {
         return function() {
-            this.setState({ showing: name });
+            this.setState({ showing: name }, this._adjustSidebarHeight);
         }.bind(this);
     },
 
@@ -69,7 +79,7 @@ var Kresus = React.createClass({
 
         return (
         <div className="row">
-            <div className="sidebar hidden-xs col-sm-3">
+            <div id="sidebar" className="sidebar hidden-xs col-sm-3">
                 <div className="logo sidebar-light">
                     <a href="#">KRESUS</a>
                 </div>
