c9f5636ecf82e137ab33288846bd66c75f8fafdb
Benjamin Bouvier <public@benj.me>
2015-05-03 16:59:52 +0200

    Add hamburger menu in mobile mode.

--- client/main.js
+++ client/main.js
@@ -81,47 +81,54 @@ class Kresus extends React.Component {
             : <div/>;
 
         return (
-        <div className="row">
-            <div className="sidebar hidden-xs col-sm-3">
-                <div className="logo sidebar-light">
-                    <a href="#"><T k='KRESUS'>KRESUS</T></a>
-                </div>
-
-                <div className="sidebar-section-list">
-                    <ul>
-                        <li className={IsActive('reports')} onClick={this.show('reports')}>
-                            <span className="sidebar-section-reports"> </span>
-                            <T k='menu.reports'>Reports</T>
-                        </li>
-                        <li className={IsActive('charts')} onClick={this.show('charts')}>
-                            <span className="sidebar-section-charts"> </span>
-                            <T k='menu.charts'>Graphics</T>
-                        </li>
-                        <li className={IsActive('similarities')} onClick={this.show('similarities')}>
-                            <span className="sidebar-section-similarities"> </span>
-                            <T k='menu.similarities'>Duplicates</T>
-                        </li>
-                        <li className={IsActive('categories')} onClick={this.show('categories')}>
-                            <span className="sidebar-section-categories"> </span>
-                            <T k='menu.categories'>Categories</T>
-                        </li>
-                        <li className={IsActive('settings')} onClick={this.show('settings')}>
-                            <span className="sidebar-section-settings"> </span>
-                            <T k='menu.settings'>Settings</T>
-                        </li>
-                    </ul>
-                </div>
-
-                {maybeBankAndAccountList}
+        <div>
+            <div className="row navbar navbar-inverse visible-xs">
+                <button className="navbar-toggle" data-toggle="offcanvas" data-target=".sidebar">
+                    <span className="glyphicon glyphicon-menu-hamburger"></span>
+                </button>
+                <a className="navbar-brand" href="#"><T k='KRESUS'>KRESUS</T></a>
             </div>
 
-            <div className="col-sm-3"></div>
+            <div className="row">
+                <div className="sidebar offcanvas-xs col-sm-3 col-xs-10">
+                    <div className="logo sidebar-light">
+                        <a href="#"><T k='KRESUS'>KRESUS</T></a>
+                    </div>
 
-            <div className="main-block col-xs-12 col-sm-9">
-                <div className="main-container">
+                    <div className="sidebar-section-list">
+                        <ul>
+                            <li className={IsActive('reports')} onClick={this.show('reports')}>
+                                <span className="sidebar-section-reports"> </span>
+                                <T k='menu.reports'>Reports</T>
+                            </li>
+                            <li className={IsActive('charts')} onClick={this.show('charts')}>
+                                <span className="sidebar-section-charts"> </span>
+                                <T k='menu.charts'>Graphics</T>
+                            </li>
+                            <li className={IsActive('similarities')} onClick={this.show('similarities')}>
+                                <span className="sidebar-section-similarities"> </span>
+                                <T k='menu.similarities'>Duplicates</T>
+                            </li>
+                            <li className={IsActive('categories')} onClick={this.show('categories')}>
+                                <span className="sidebar-section-categories"> </span>
+                                <T k='menu.categories'>Categories</T>
+                            </li>
+                            <li className={IsActive('settings')} onClick={this.show('settings')}>
+                                <span className="sidebar-section-settings"> </span>
+                                <T k='menu.settings'>Settings</T>
+                            </li>
+                        </ul>
+                    </div>
 
-                    {mainComponent}
+                    {maybeBankAndAccountList}
+                </div>
 
+                <div className="col-sm-3"></div>
+
+                <div className="main-block col-xs-12 col-sm-9">
+                    <div className="main-container">
+                        {mainComponent}
+                    </div>
                 </div>
             </div>
         </div>
