11fe98378940089f373d92a1dbcd6effd97a5a8b
NAO <sebastien.meriot@gmail.com>
2015-05-11 00:23:28 +0200

    Add a lint task for QA

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var browserify = require('browserify');
 var buffer = require('vinyl-buffer');
 var coffee = require('gulp-coffee');
+var lint = require('gulp-coffeelint');
 var g = require('gulp');
 var concat = require('gulp-concat');
 var babelify = require('babelify');
@@ -10,7 +11,9 @@ var source = require('vinyl-source-stream');
 g.task('default', ['b']);
 g.task('b' /* build */, ['build-client', 'build-server']);
 
+
 const BUILDDIR = 'build/';
+
 
 g.task('client-remove-dir', function() {
     return rm(BUILDDIR + 'client/');
@@ -81,6 +84,18 @@ g.task('build-server', function() {
     g.src('tests/**/*.json')
      .pipe(g.dest(BUILDDIR + 'tests/'))
     ;
+});
+
+g.task('clean', function() {
+  rm(BUILDDIR);
+});
+
+g.task('lint', function() {
+  g
+  .src(['server/**/*.coffee', 'server.coffee'])
+  .pipe(lint())
+  .pipe(lint.reporter())
+  ;
 });
 
 g.task('w' /* watch */, ['build-client', 'build-server'], function() {
