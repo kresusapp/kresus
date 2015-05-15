767331606ab804530e4fdf07923ccf90fabc6361
Benjamin Bouvier <public@benj.me>
2015-05-15 17:49:56 +0200

    Unify weboob scripts.

--- weboob/py/endpoint.py
+++ weboob/py/endpoint.py
@@ -3,15 +3,7 @@ from weboob.exceptions import BrowserIncorrectPassword
 
 from connector import Connector
 
-import sys
-
-def EnsurePrint(wat):
-    try:
-        print wat
-        return wat
-    except:
-        wat = unicode(wat).encode('utf-8')
-        print wat
+import sys, json
 
 class BaseBankHandler(object):
     '''
@@ -23,7 +15,7 @@ class BaseBankHandler(object):
         self.password = password
         self.website = website
 
-    def load_from_connector(self, name, method_name):
+    def load_from_connector(self, name):
         '''
         Load given connector (name) and apply the given method on it.
         Supported method: get_accounts and get_history.
@@ -45,16 +37,13 @@ class BaseBankHandler(object):
         try:
             connector = Connector(name, params_connector)
             results = {}
-            callback = getattr(connector, method_name)
-            results[name] = callback()
+            results[name] = self.get_content(connector)
         except ModuleLoadError:
             raise Exception("Could not load module: %s" % name)
         except BrowserIncorrectPassword:
             raise Exception("Wrong credentials")
         except Exception as e:
-            EnsurePrint(unicode(e))
-            raise Exception("Something went wrong (weboob modules should "
-                            "probably be updated)")
+            raise Exception("Unexpected Weboob error: %s" % unicode(e))
 
         return results
 
@@ -64,6 +53,9 @@ class BankHandler(BaseBankHandler):
     This handler is dedicated to retrieve data from bank accounts.
     """
 
+    def get_content(self, connector):
+        return connector.get_balances()
+
     def post(self, name):
         """
         Grab data about all accounts from a given bank identifier.
@@ -71,8 +63,7 @@ class BankHandler(BaseBankHandler):
         Bank type is given as URL parameter, credentials are given in body.
         For available bank type check: http://weboob.org/modules
         """
-
-        return self.load_from_connector(name, 'get_balances')
+        return self.load_from_connector(name)
 
 
 class BankHistoryHandler(BaseBankHandler):
@@ -80,6 +71,8 @@ class BankHistoryHandler(BaseBankHandler):
     This handler is dedicated to retrieve transaction history of bank accounts.
     """
 
+    def get_content(self, connector):
+        return connector.get_history()
 
     def post(self, name):
         """
@@ -88,4 +81,34 @@ class BankHistoryHandler(BaseBankHandler):
         Bank type is given as URL parameter, credentials are given in body.
         For available bank type check: http://weboob.org/modules
         """
-        return self.load_from_connector(name, 'get_history')
+        return self.load_from_connector(name)
+
+# Arguments format: {'account' | 'history'} bankuuid login password maybe_website
+
+args = []
+requested_content = None
+for l in sys.stdin:
+    if requested_content is None:
+        requested_content = l.strip()
+        continue
+    args.append(l.strip())
+
+if len(args) < 3:
+    print('missing arguments')
+    quit()
+
+bankuuid = args[0]
+id = args[1]
+password = args[2]
+website = None
+if len(args) == 4:
+    website = args[3]
+
+content = None
+if requested_content == 'account':
+    content = BankHandler(id, password, website).post(bankuuid)
+elif requested_content == 'history':
+    content = BankHistoryHandler(id, password, website).post(bankuuid)
+
+assert(content is not None)
+print json.dumps(content, ensure_ascii=False).encode('utf-8')
--- weboob/scripts/operations.sh
+++ weboob/scripts/operations.sh
@@ -3,6 +3,7 @@
 cd weboob
 source env/bin/activate
 args=`while read x ; do echo $x ; done`
-python py/operations.py << EOF
+python py/endpoint.py<< EOF
+    history
     $args
 EOF
