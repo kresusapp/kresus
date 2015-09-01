3639c7a24c792a32c216108ca89ee337be55cfe3
goofy-bz <goofy@babelzilla.org>
2015-09-02 00:25:58 +0200

    quelques bricoles

--- client/locales/fr.js
+++ client/locales/fr.js
@@ -82,7 +82,7 @@ module.exports = {
     },
 
     loadscreen: {
-        title: "Merci de patienter pendant l'installation des dépendences de Kresus",
+        title: "Merci de patienter pendant l'installation des dépendances de Kresus",
         prolix: "Dans quelques minutes, rechargez la page, et si Kresus ne s'affiche pas entièrement, merci de contacter un des mainteneurs de Kresus en lui indiquant le contenu de la fenêtre de debug ci-dessous.",
     },
 
@@ -115,11 +115,11 @@ module.exports = {
         full_label: 'Libellé complet :',
         category: 'Catégorie',
         kresus_init_title: "Veuillez patienter pendant l'initialisation de Kresus",
-        kresus_init_content: "Kresus est en train de télécharger de la magie, accrochez-vous !",
+        kresus_init_content: "Kresus est en train de télécharger de la magie, accrochez-vous !",
 
         last_sync: 'Dernière synchronisation avec votre banque :',
         sync_now: 'Synchroniser maintenant',
-        syncing: 'Récupération de vos dernières opérations en cours...',
+        syncing: 'Récupération de vos dernières opérations en cours…',
 
         title: 'Opérations',
     },
@@ -147,11 +147,11 @@ module.exports = {
 
         reinstall_weboob: 'Réinstaller Weboob',
         go_reinstall_weboob: "Lancer la réinstallation",
-        reinstall_weboob_help: "Cette procédure va entièrement réinstaller Weboob. Cela peut prendre quelques minutes, durant lesquelles vous ne pourrez pas importer vos comptes et opérations. A n'utiliser qu'en dernier recours !",
+        reinstall_weboob_help: "Cette procédure va entièrement réinstaller Weboob. Cela peut prendre quelques minutes, durant lesquelles vous ne pourrez pas importer vos comptes et opérations. À n'utiliser qu'en dernier recours !",
 
         update_weboob: 'Mettre Weboob à jour',
         go_update_weboob: "Lancer la mise à jour",
-        update_weboob_help: "Cette procédure va mettre à jour Weboob sans le réinstaller entièrement. Cela peut prendre quelques minutes, durant lesquelles vous ne pourrez pas importer vos comptes et opérations. A utiliser quand mettre à jour ne synchronise plus vos opérations !",
+        update_weboob_help: "Cette procédure va mettre à jour Weboob sans le réinstaller entièrement. Cela peut prendre quelques minutes, durant lesquelles vous ne pourrez pas importer vos comptes et opérations. À utiliser quand mettre à jour ne synchronise plus vos opérations !",
 
         export_instance: "Exporter l'instance",
         go_export_instance: "Exporter",
@@ -170,14 +170,14 @@ module.exports = {
         tab_weboob: 'Gestion de Weboob',
 
         erase_account: "Cela va supprimer le compte '%{title}' et toutes les opérations bancaires qu'il contient. Si c'est le dernier compte lié à cette banque, le lien bancaire sera supprimé. Êtes-vous sûrs de vouloir supprimer ce compte ?",
-        erase_bank: "Cela va supprimer la banque nommée '%{name}', tous les comptes et toutes les opérations liées à cette banque. Êtes-vous sûrs de vouloir supprimer cette banque et tous ses comptes liés ?",
+        erase_bank: "Cela va supprimer la banque nommée '%{name}', tous les comptes et toutes les opérations liées à cette banque. Êtes-vous sûrs de vouloir supprimer cette banque et tous ses comptes liés ?",
         missing_login_or_password: "Le login et le mot de passe sont obligatoires",
         submit: 'Sauvegarder',
 
         delete_account_button: "Supprimer compte",
         delete_bank_button: "Supprimer banque",
         reload_accounts_button: "Mettre à jour les comptes",
-        change_password_button: "Mettre à jour le password",
+        change_password_button: "Mettre à jour le mot de passe",
         add_bank_button: "Ajouter une banque",
 
         default_chart_type: "Graphiques : type d'opérations par défaut",
@@ -187,7 +187,7 @@ module.exports = {
     similarity: {
         nothing_found: "Aucune paire d'opérations similaires n'a été trouvée.",
         title: "Doublons",
-        help: "Il arrive que lors de l'import des opérations bancaires, certaines d'entre elles soient importées en double, par exemple quand la banque ajoute des informations sur une opération bancaires quelques jours après que celle-ci ait eu lieu. Cet écran vous montre les potentiels doublons (opérations qui ont le même montant sur une période temporelle donnée). Remarque : les catégories sont transférées lors de la suppression : si dans une paire de doublons A / B dans laquelle A a une catégorie et B n'en a pas, supprimer A réaffectera automatiquement sa catégorie à B.",
+        help: "Il arrive lors de l'import des opérations bancaires que certaines d'entre elles soient importées en double, par exemple quand la banque ajoute des informations sur une opération bancaire quelques jours après que celle-ci a eu lieu. Cet écran vous montre les potentiels doublons (opérations qui ont le même montant sur une période temporelle donnée). Remarque : les catégories sont transférées lors de la suppression : si dans une paire de doublons A / B dans laquelle A a une catégorie et B n'en a pas, supprimer A réaffectera automatiquement sa catégorie à B.",
         date: "Date",
         label: "Libellé de l'opération",
         amount: "Montant",
