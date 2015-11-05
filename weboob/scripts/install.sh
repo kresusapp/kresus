85fc523e9cab124a60c94cf9b2fdff58b784bbc6
Brendan Abolivier <contact@brendanabolivier.com>
2015-11-05 12:37:40 +0100

    Added check for virtualenv's name

--- weboob/scripts/install.sh
+++ weboob/scripts/install.sh
@@ -2,4 +2,14 @@
 
 cd weboob
 rm -rf env
-mkdir -p ./env && virtualenv ./env && source ./env/bin/activate && pip install -r requirements.txt && cd .. && ./weboob/scripts/test.sh
+
+# Let's find out if Python's virtualenv is installed and, if so, what name its binary has
+pyvenv=$(which virtualenv || which virtualenv2 || echo "")
+if [[ -z $pyvenv ]]
+then
+    # Virtualenv isn't installed, we abort the installation
+    echo "Virtualenv is not installed"
+    exit 1
+fi
+
+mkdir -p ./env && $pyvenv ./env && source ./env/bin/activate && pip install -r requirements.txt && cd .. && ./weboob/scripts/test.sh
