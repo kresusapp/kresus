2a76f51764e92ee0b942d2f3753f4f5a1076ddc3
Benjamin Bouvier <public@benj.me>
2015-11-25 00:42:04 +0100

    Use cozydb.api.sendMailToUser instead of request-json to send emails;

--- server/lib/emailer.js
+++ server/lib/emailer.js
@@ -1,5 +1,4 @@
-import {JsonClient as Client} from 'request-json';
-
+import cozydb from 'cozydb';
 import {promisify} from '../helpers';
 
 let log = require('printit')({
@@ -12,29 +11,24 @@ const DATA_SYSTEM_URL = "http://localhost:9101/";
 class Emailer
 {
     constructor() {
-        this.client = new Client(DATA_SYSTEM_URL);
-        this.client.post = promisify(::this.client.post);
-
         if (process.kresus.standalone) {
-            log.warn("email sender not implemented yet in standalone mode");
-        } else if (process.kresus.prod) {
-            this.client.setBasicAuth(process.env.NAME, process.env.TOKEN);
+            this.internalSendToUser = promisify(function(opts, cb) {
+                log.warn("Trying to send email in standalone mode, where email is NYI.");
+                cb(null);
+            });
+        } else {
+            this.internalSendToUser = promisify(::cozydb.api.sendMailToUser);
         }
     }
 
     // opts = {from, subject, content, html}
     async sendToUser(opts) {
-
-        if (process.kresus.standalone) {
-            return log.warn("Trying to send email in standalone mode, where email is NYI.");
-        }
-
         opts.from = opts.from || "Kresus <kresus-noreply@cozycloud.cc>";
         if (!opts.subject)
             return log.warn("Emailer.send API misuse: subject is required");
         if (!opts.content && !opts.html)
             return log.warn("Emailer.send API misuse: either content or html is required");
-        await this.client.post("mail/to-user/", opts);
+        await this.internalSendToUser(opts);
     }
 };
 
