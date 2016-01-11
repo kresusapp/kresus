ab7d48d886ee7a3303641957f0edf0d62ba0fe83
Benjamin Bouvier <public@benj.me>
2016-01-11 22:20:09 +0100

    Fix Travis CI so that a failing test prevents the others from running;

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Lint
-./scripts/lint.sh ./server
-
+./scripts/lint.sh ./server && \
 # Compare locales
-./node_modules/babel-cli/bin/babel-node.js --presets es2015 ./scripts/compare-locales.js
+./node_modules/babel-cli/bin/babel-node.js --presets es2015 ./scripts/compare-locales.js && \
+# Add new tests here
+echo "PASS!"
