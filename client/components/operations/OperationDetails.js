0be16a8453b577a18bfc99f985a8cd9fbc0d5ef7
ZeHiro <antoine.bossy@free.fr>
2016-02-02 07:38:05 +0100

    Lint operations/OperationDetails

--- client/components/operations/OperationDetails.js
+++ client/components/operations/OperationDetails.js
@@ -34,57 +34,64 @@ export default class OperationDetails extends React.Component {
         let maybeAttachment = '';
         if (op.binary !== null) {
             let opLink = ComputeAttachmentLink(op);
-            maybeAttachment = <span>
-                <a href={opLink} target="_blank">
-                    <span className="glyphicon glyphicon-file"></span>
-                    {$t('client.operations.attached_file')}
-                </a>
-            </span>;
+            maybeAttachment = (
+                <span>
+                    <a href={ opLink } target="_blank">
+                        <span className="glyphicon glyphicon-file"></span>
+                        { $t('client.operations.attached_file') }
+                    </a>
+                </span>
+            );
         } else if (op.attachments && op.attachments.url !== null) {
-            maybeAttachment = <span>
-                <a href={op.attachments.url} target="_blank">
-                    <span className="glyphicon glyphicon-file"></span>
-                    {$t('client.' + op.attachments.linkTranslationKey)}
-                </a>
-            </span>;
+            maybeAttachment = (
+                <span>
+                    <a href={ op.attachments.url } target="_blank">
+                        <span className="glyphicon glyphicon-file"></span>
+                        { $t(`client.${op.attachments.linkTranslationKey}`) }
+                    </a>
+                </span>
+            );
         }
 
-        return <tr className={this.props.rowClassName}>
-            <td>
-                <a href="#" onClick={this.props.toggleDetails}>
-                    <i className="fa fa-minus-square"></i>
-                </a>
-            </td>
-            <td colSpan="5" className="text-uppercase">
-                <ul>
-                    <li>
-                        {$t('client.operations.full_label')}
-                        {op.raw}
-                    </li>
-                    <li className="form-inline">
-                        {$t('client.operations.custom_label')}
-                        <DetailedViewLabelComponent operation={op} />
-                    </li>
-                    <li>
-                        {$t('client.operations.amount')}
-                        {op.amount}</li>
-                    <li className="form-inline">
-                        {$t('client.operations.type')}
-                        <OperationTypeSelectComponent
-                          operation={op}
-                          onSelectId={this.onSelectOperationType.bind(this)}
-                        />
-                    </li>
-                    <li className="form-inline">
-                        {$t('client.operations.category')}
-                        <CategorySelectComponent
-                          operation={op}
-                          onSelectId={this.onSelectCategory.bind(this)}
-                        />
-                    </li>
-                    {maybeAttachment}
-                </ul>
-            </td>
-        </tr>;
+        return (
+            <tr className={ this.props.rowClassName }>
+                <td>
+                    <a href="#" onClick={ this.props.toggleDetails }>
+                        <i className="fa fa-minus-square"></i>
+                    </a>
+                </td>
+                <td colSpan="5" className="text-uppercase">
+                    <ul>
+                        <li>
+                            { $t('client.operations.full_label') }
+                            { op.raw }
+                        </li>
+                        <li className="form-inline">
+                            { $t('client.operations.custom_label') }
+                            <DetailedViewLabelComponent operation={ op } />
+                        </li>
+                        <li>
+                            { $t('client.operations.amount') }
+                            { op.amount }
+                        </li>
+                        <li className="form-inline">
+                            { $t('client.operations.type') }
+                            <OperationTypeSelectComponent
+                              operation={ op }
+                              onSelectId={ this.onSelectOperationType.bind(this) }
+                            />
+                        </li>
+                        <li className="form-inline">
+                            { $t('client.operations.category') }
+                            <CategorySelectComponent
+                              operation={ op }
+                              onSelectId={ this.onSelectCategory.bind(this) }
+                            />
+                        </li>
+                        { maybeAttachment }
+                    </ul>
+                </td>
+            </tr>
+        );
     }
 }
