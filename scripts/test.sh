e1e6ff137faed7fe0f598422ad714e455a954718
ZeHiro <mail-github@abossy.fr>
2016-07-30 19:15:50 +0200

    Provide test infrastructure

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,5 +4,7 @@
 ./scripts/lint.sh ./ && \
 # Compare locales
 ./node_modules/babel-cli/bin/babel-node.js --presets es2015 ./scripts/compare-locales.js && \
+# Run proper tests
+./node_modules/mocha/bin/mocha ./tests/ --recursive --require babel-register && \
 # Add new tests here
 echo "PASS!"
