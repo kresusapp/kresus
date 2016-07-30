8ba06995ff6cc5516912875646b0de7fbdfacdf5
ZeHiro <mail-github@abossy.fr>
2016-07-30 19:15:50 +0200

    Provide test infrastructure

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,4 +5,10 @@
 # Compare locales
 ./node_modules/babel-cli/bin/babel-node.js --presets es2015 ./scripts/compare-locales.js && \
 # Add new tests here
+make build && \
+mkdir -p ./build/tests && \
+./node_modules/babel-cli/bin/babel.js \
+     --presets es2015,stage-0 --plugins transform-runtime \
+     ./tests/ -d ./build/tests && \
+node_modules/mocha/bin/mocha build/tests/ --recursive && \
 echo "PASS!"
