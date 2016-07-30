9325752a9f6b67acad70a4e6d85b0e0f125ca8e0
ZeHiro <mail-github@abossy.fr>
2016-07-30 19:15:50 +0200

    Provide test infrastructure

---
+++ tests/test.js
@@ -0,0 +1,9 @@
+import should from 'should';
+import { assert } from '../shared/helpers';
+
+describe('This is a demonstration', function() {
+    it('true should be true', function() {
+        let a = true;
+        a.should.equal(true);
+    });
+});
