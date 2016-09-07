16b0d7947693bc3ceca7f5c593565490f3da022f
ZeHiro <mail+cloud9@abossy.fr>
2016-09-07 17:49:56 +0000

    Fix

--- client/components/ui/amount-input.js
+++ client/components/ui/amount-input.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import { assertHas } from '../../helpers';
+import { assertHas, translate as $t } from '../../helpers';
 
 export default class AmountInput extends React.Component {
     constructor(props) {
         assertHas(props, 'onChange');
-        assertHas(props, 'inputRef')
+        assertHas(props, 'inputRef');
         super(props);
         this.state = { isNegative: true };
         this.handleChange = this.props.onChange.bind(this);
@@ -13,21 +13,25 @@ export default class AmountInput extends React.Component {
     }
     
     handleClick() {
-        console.log("OK");
-        this.setState({ isNegative: !this.state.isNegative },  this.handleChange());
+        this.setState({ isNegative: !this.state.isNegative },  this.handleChange);
     }
     
     getValue() {
-        return this.state.isNegative ? - this.refs[`input-#{this.props.inputRef}`].value : this.refs[`input-#{this.props.inputRef}`].value;
+        let value = this.refs[`input-${this.props.inputRef}`].value;
+        if (value === '') {
+            return '';
+        }
+        return this.state.isNegative ? - value : value;
     }
     
     render() {
         return (
             <div className="input-group">
-                <span className="input-group-addon" onClick= { this.handleClick } id={ `sign-#{this.props.inputRef}` }>
-                    { this.state.isNegative ? '-' : '+'}
+                <span className="input-group-addon" onClick= { this.handleClick } id={ `sign-${this.props.inputRef}` }
+                  title={ $t('client.toggle_sign') }>
+                    <i className={ `fa fa-${this.state.isNegative ? 'minus' : 'plus'}` }></i>
                 </span>
-                <input type="number" className="form-control" ref={ `input-#{this.props.inputRef}` } onChange={ this.handleChange }  aria-describedby={ `sign-#{this.props.inputRef}` }/>
+                <input type="number" className="form-control" ref={ `input-${this.props.inputRef}` } onChange={ this.handleChange }  aria-describedby={ `sign-#{this.props.inputRef}` }/>
             </div>
         );
     }
