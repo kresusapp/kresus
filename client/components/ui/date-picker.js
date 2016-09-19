7d03536ccac78b171875373e3f83811a8747ff31
Nicolas Frandeboeuf <nicofrand@gmail.com>
2016-09-19 13:22:53 +0200

    Prefill datepickers with current values

--- client/components/ui/date-picker.js
+++ client/components/ui/date-picker.js
@@ -91,7 +91,22 @@ export default class DatePicker extends React.Component {
     }
 
     render() {
-        return <input className="form-control" type="text" ref="elem" />;
+        let defaultDate = "";
+        if (this.props.defaultValue) {
+            let defaultValue = new Date(this.props.defaultValue);
+            let defaultMonth = "" + (defaultValue.getMonth() + 1);
+            let defaultDay = "" + defaultValue.getDate();
+
+            if (defaultMonth.length < 2)
+                defaultMonth = "0" + defaultMonth;
+
+            if (defaultDay.length < 2)
+                defaultDay = "0" + defaultDay;
+
+            defaultDate = `${defaultValue.getFullYear()}/${defaultMonth}/${defaultDay}`;
+        }
+
+        return <input className="form-control" type="text" ref="elem" data-value={ defaultDate } />;
     }
 
 }
