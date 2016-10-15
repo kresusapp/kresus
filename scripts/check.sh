bab9fba7c9a2572df157648b3f5365896747a8bc
Benjamin Bouvier <public@benj.me>
2016-10-15 12:41:52 +0200

    Split the tests into their own script and rename `make test` into `make check`;

---
+++ scripts/check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Lint
+./scripts/lint.sh ./ && \
+# Compare locales
+./node_modules/babel-cli/bin/babel-node.js --presets es2015 ./scripts/compare-locales.js && \
+# Check all banks have a logo
+./node_modules/babel-cli/bin/babel-node.js --presets es2015 ./scripts/check-logos.js && \
+# Run proper tests
+./scripts/test.sh && \
+# Add new tests here
+echo "PASS!"
