32987f7369433eb527adae50d7efafa7d5055576
Benjamin Bouvier <public@benj.me>
2016-11-06 23:43:20 +0100

    Fix box clicking on MacOSX;
    MacOS doesn't trigger the onFocus handler, somehow; using the onClick handler
    in addition to the onFocus handler seems to do the trick.

--- client/components/ui/button-select.js
+++ client/components/ui/button-select.js
@@ -59,6 +59,7 @@ export default class SelectableButtonComponent extends React.Component {
             return (
                 <button
                   className="form-control btn-transparent label-button"
+                  onClick={ this.handleToggleEdit }
                   onFocus={ this.handleToggleEdit }>
                     { label }
                 </button>
