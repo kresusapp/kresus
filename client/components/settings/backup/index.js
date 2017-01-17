61a8addc5243ce43b38d1bf8db108841272cda45
Nicolas Frandeboeuf <nicofrand@gmail.com>
2017-01-17 12:22:24 +0100

    Remove useless panels in settings

--- client/components/settings/backup/index.js
+++ client/components/settings/backup/index.js
@@ -21,54 +21,46 @@ const BackupSection = connect(
         };
 
         return (
-            <div className="top-panel panel panel-default">
-                <div className="panel-heading">
-                    <h3 className="title panel-title">{ $t('client.settings.tab_backup') }</h3>
+            <form className="top-panel">
+                <div className="form-group">
+                    <div className="row">
+                        <label
+                          htmlFor="exportInstance"
+                          className="col-xs-4 control-label">
+                            { $t('client.settings.export_instance') }
+                        </label>
+                        <div className="col-xs-8">
+                            <button
+                              type="button"
+                              onClick={ handleExport }
+                              id="exportInstance"
+                              className="btn btn-primary">
+                                { $t('client.settings.go_export_instance') }
+                            </button>
+                            <span className="help-block">
+                                { $t('client.settings.export_instance_help') }
+                            </span>
+                        </div>
+                    </div>
                 </div>
 
-                <div className="panel-body">
-                    <form>
-                        <div className="form-group">
-                            <div className="row">
-                                <label
-                                  htmlFor="exportInstance"
-                                  className="col-xs-4 control-label">
-                                    { $t('client.settings.export_instance') }
-                                </label>
-                                <div className="col-xs-8">
-                                    <button
-                                      type="button"
-                                      onClick={ handleExport }
-                                      id="exportInstance"
-                                      className="btn btn-primary">
-                                        { $t('client.settings.go_export_instance') }
-                                    </button>
-                                    <span className="help-block">
-                                        { $t('client.settings.export_instance_help') }
-                                    </span>
-                                </div>
-                            </div>
+                <div className="form-group">
+                    <div className="row">
+                        <label
+                          htmlFor="importInstance"
+                          className="col-xs-4 control-label">
+                            { $t('client.settings.import_instance') }
+                        </label>
+                        <div className="col-xs-8">
+                            <ImportModule />
+                            <span className="help-block">
+                                { $t('client.settings.import_instance_help') }
+                            </span>
                         </div>
-
-                        <div className="form-group">
-                            <div className="row">
-                                <label
-                                  htmlFor="importInstance"
-                                  className="col-xs-4 control-label">
-                                    { $t('client.settings.import_instance') }
-                                </label>
-                                <div className="col-xs-8">
-                                    <ImportModule />
-                                    <span className="help-block">
-                                        { $t('client.settings.import_instance_help') }
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-
-                    </form>
+                    </div>
                 </div>
-            </div>
+
+            </form>
         );
     }
 );
