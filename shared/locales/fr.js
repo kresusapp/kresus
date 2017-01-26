e8192e0cc16ec87dc571fe1f8ea6b335ea67a163
Nicolas Frandeboeuf <nicofrand@gmail.com>
2017-01-26 18:19:01 +0100

    Charts amount type: use checkbox

--- shared/locales/fr.js
+++ shared/locales/fr.js
@@ -56,6 +56,7 @@ module.exports = {
             title: 'Graphiques',
 
             type: 'Type',
+            amount_type: `Type d'opérations`,
             all_types: 'Les deux',
             positive: 'Revenus',
             negative: 'Dépenses',
@@ -70,7 +71,8 @@ module.exports = {
             unselect_all_categories: 'Désélectionner toutes les catégories',
             select_all_categories: 'Sélectionner toutes les catégories',
 
-            default_type: `Type d'opérations par défaut`,
+            default_type: 'Type par défaut',
+            default_amount_type: `Type d'opérations par défaut`,
             default_period: 'Période par défaut'
         },
 
