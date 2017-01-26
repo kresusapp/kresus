59ec0dc796928042f4a7c6f23bc2d81e07e31ef7
Nicolas Frandeboeuf <nicofrand@gmail.com>
2017-01-26 18:19:01 +0100

    Charts amount type: use checkbox

--- client/components/charts/default-params-modal.js
+++ client/components/charts/default-params-modal.js
@@ -7,24 +7,30 @@ import { translate as $t } from '../../helpers';
 
 import Modal from '../ui/modal';
 import OpCatChartPeriodSelect from '../charts/operations-by-category-period-select';
-import OpCatChartTypeSelect from '../charts/operations-by-category-type-select';
+import OpAmountTypeSelect from './operations-by-amount-type-select';
 
 class DefaultParamsModal extends React.Component {
     constructor(props) {
         super(props);
 
         this.handleSave = this.handleSave.bind(this);
-        this.handleTypeChange = this.handleTypeChange.bind(this);
+        this.handleAmountTypeChange = this.setState.bind(this);
         this.handlePeriod = this.handlePeriodChange.bind(this);
 
-        this.type = this.props.type;
         this.period = this.props.period;
+
+        this.state = {
+            showPositiveOps: props.showPositiveOps,
+            showNegativeOps: props.showNegativeOps
+        };
     }
 
     handleSave() {
         let close = false;
-        if (this.type !== this.props.type) {
-            this.props.setType(this.type);
+
+        if (this.state.showPositiveOps !== this.props.showPositiveOps ||
+            this.state.showNegativeOps !== this.props.showNegativeOps) {
+            this.props.setAmountType(this.state.showPositiveOps, this.state.showNegativeOps);
             close = true;
         }
 
@@ -38,10 +44,6 @@ class DefaultParamsModal extends React.Component {
         }
     }
 
-    handleTypeChange(event) {
-        this.type = event.currentTarget.value;
-    }
-
     handlePeriodChange(event) {
         this.period = event.currentTarget.value;
     }
@@ -49,19 +51,16 @@ class DefaultParamsModal extends React.Component {
     render() {
         let modalBody = (<div>
             <div className="form-group clearfix">
-                <label
-                  htmlFor="defaultChartType"
-                  className="col-xs-4 control-label">
-                    { $t('client.charts.default_type') }
+                <label className="col-md-4 col-xs-12">
+                    { $t('client.charts.default_amount_type') }
                 </label>
-                <div className="col-xs-8">
-                    <OpCatChartTypeSelect
-                      defaultValue={ this.props.type }
-                      onChange={ this.handleTypeChange }
-                      ref="defaultChartType"
-                      htmlId="defaultChartType"
-                    />
-                </div>
+
+                <OpAmountTypeSelect
+                  className="col-md-8 col-xs-12"
+                  showPositiveOps={ this.state.showPositiveOps }
+                  showNegativeOps={ this.state.showNegativeOps }
+                  onChange={ this.handleAmountTypeChange }
+                />
             </div>
 
             <div className="form-group clearfix">
@@ -113,29 +112,47 @@ DefaultParamsModal.propTypes = {
     // Unique identifier of the modal
     modalId: React.PropTypes.string.isRequired,
 
-    // The current default chart type
-    type: React.PropTypes.string.isRequired,
+    // Whether to display positive operations
+    showPositiveOps: React.PropTypes.bool.isRequired,
+
+    // Whether to display negative operations
+    showNegativeOps: React.PropTypes.bool.isRequired,
+
+    // The function to set the default chart type
+    setAmountType: React.PropTypes.func.isRequired,
 
     // The current default chart period
     period: React.PropTypes.string.isRequired,
-
-    // The function to set the default chart type
-    setType: React.PropTypes.func.isRequired,
 
     // The function to set the default chart period
     setPeriod: React.PropTypes.func.isRequired
 };
 
 const Export = connect(state => {
+    let amountType = get.setting(state, 'defaultChartType');
+
     return {
-        type: get.setting(state, 'defaultChartType'),
+        showPositiveOps: ['all', 'positive'].includes(amountType),
+        showNegativeOps: ['all', 'negative'].includes(amountType),
         period: get.setting(state, 'defaultChartPeriod')
     };
 }, dispatch => {
     return {
-        setType(val) {
-            actions.setSetting(dispatch, 'defaultChartType', val);
+        setAmountType(showPositiveOps, showNegativeOps) {
+            let type = null;
+            if (showPositiveOps && showNegativeOps) {
+                type = 'all';
+            } else if (showPositiveOps) {
+                type = 'positive';
+            } else if (showNegativeOps) {
+                type = 'negative';
+            }
+
+            if (type !== null) {
+                actions.setSetting(dispatch, 'defaultChartType', type);
+            }
         },
+
         setPeriod(val) {
             actions.setSetting(dispatch, 'defaultChartPeriod', val);
         }
