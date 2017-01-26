ae9959b7e3fef94ccf48e756dabd48b8362734ca
Nicolas Frandeboeuf <nicofrand@gmail.com>
2017-01-26 18:19:01 +0100

    Charts amount type: use checkbox

---
+++ client/components/charts/operations-by-amount-type-select.js
@@ -0,0 +1,83 @@
+import React from 'react';
+
+import { translate as $t } from '../../helpers';
+
+const OpAmountTypeSelect = props => {
+
+    let showPositiveOpsInput;
+    let showNegativeOpsInput;
+    const onChange = props.onChange;
+
+    const handleChange = event => {
+        let name = event.target.getAttribute('name');
+        let isChecked = event.target.checked;
+        let result = {
+            [name]: isChecked
+        };
+
+        // If both are now unchecked, automatically select the other
+        let otherInput;
+        let otherInputName;
+        if (name === 'showPositiveOps') {
+            otherInput = showNegativeOpsInput;
+            otherInputName = 'showNegativeOps';
+        } else {
+            otherInput = showPositiveOpsInput;
+            otherInputName = 'showPositiveOps';
+        }
+
+        if (!isChecked && !otherInput.checked) {
+            result[otherInputName] = true;
+        }
+
+        onChange(result);
+    };
+
+    let refPositive = node => {
+        showPositiveOpsInput = node;
+    };
+
+    let refNegative = node => {
+        showNegativeOpsInput = node;
+    };
+
+    return (<div className={ props.className }>
+        <label className="col-xs-12 col-md-6 checkbox-inside-label">
+            <input
+              type="checkbox"
+              name="showPositiveOps"
+              checked={ props.showPositiveOps }
+              onChange={ handleChange }
+              ref={ refPositive }
+            />
+            <span>{ $t('client.charts.positive') }</span>
+        </label>
+
+        <label className="col-xs-12 col-md-6 checkbox-inside-label">
+            <input
+              type="checkbox"
+              name="showNegativeOps"
+              checked={ props.showNegativeOps }
+              onChange={ handleChange }
+              ref={ refNegative }
+            />
+            <span>{ $t('client.charts.negative') }</span>
+        </label>
+    </div>);
+};
+
+OpAmountTypeSelect.propTypes = {
+    // The components CSS classes
+    className: React.PropTypes.string,
+
+    // Whether to display positive operations
+    showPositiveOps: React.PropTypes.bool.isRequired,
+
+    // Whether to display negative operations
+    showNegativeOps: React.PropTypes.bool.isRequired,
+
+    // A callback called whenever one of the inputs change
+    onChange: React.PropTypes.func.isRequired
+};
+
+export default OpAmountTypeSelect;
