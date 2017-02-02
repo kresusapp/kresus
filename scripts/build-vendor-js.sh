06dc2cce39e524d3cf710ce5626fa92fb7742673
ZeHiro <mail-github@abossy.fr>
2017-02-03 00:01:33 +0100

    Add minification

--- scripts/build-vendor-js.sh
+++ scripts/build-vendor-js.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
- cat ./client/vendor/*.js \
-    ./node_modules/bootstrap/dist/js/bootstrap.min.js \
-    ./client/vendor/**/*.js > ./build/client/js/vendor.js
+ cat ./node_modules/jquery/dist/jquery.js \
+     ./node_modules/jquery.cookie/jquery.cookie.js \
+     ./node_modules/jquery-minicolors/jquery.minicolors.js \
+     ./client/vendor/*.js \
+     ./node_modules/bootstrap/dist/js/bootstrap.js \
+     ./client/vendor/**/*.js > ./build/client/js/vendor.js
--- scripts/release.sh
+++ scripts/release.sh
@@ -16,6 +16,10 @@ yarn
 echo "Building..."
 NODE_ENV=production make build
 rm -rf build/server/weboob/data
+
+echo "Minifying javascript..."
+make minify
+
 git add -f build/
 
 echo "Removing dev dependencies and installing production dependencies before shrinkwrap..."
