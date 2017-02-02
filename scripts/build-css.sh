1e5d832291d2758c981ed68386f225fea12e9d93
ZeHiro <mail-github@abossy.fr>
2017-02-02 21:38:56 +0100

    Use bootstrap npm package

--- scripts/build-css.sh
+++ scripts/build-css.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-cat ./client/css/*.css ./client/css/**/*.css \
+cat ./node_modules/bootstrap/dist/css/bootstrap.min.css \
+    ./node_modules/bootstrap/dist/css/bootstrap-theme.min.css \
+    ./client/css/*.css ./client/css/**/*.css \
     ./node_modules/c3/c3.css \
     ./node_modules/dygraphs/dist/dygraph.css > ./build/client/css/main.css
