6419b41abb6fd425133404ca6ccc9ab9c3fb66bc
ZeHiro <mail-github@abossy.fr>
2017-02-03 00:01:33 +0100

    Add minification

--- scripts/build-css.sh
+++ scripts/build-css.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-cat ./node_modules/bootstrap/dist/css/bootstrap.min.css \
-    ./node_modules/bootstrap/dist/css/bootstrap-theme.min.css \
-    ./client/css/*.css ./client/css/**/*.css \
+
+cat ./node_modules/bootstrap/dist/css/bootstrap.css \
+    ./node_modules/bootstrap/dist/css/bootstrap-theme.css \
+    ./client/css/*.css \
+    ./client/css/**/*.css \
+    ./node_modules/dygraphs/dist/dygraph.css \
     ./node_modules/c3/c3.css \
-    ./node_modules/dygraphs/dist/dygraph.css > ./build/client/css/main.css
+    ./node_modules/jquery-minicolors/jquery.minicolors.css |
+    ./node_modules/.bin/minify --css > ./build/client/css/main.css
