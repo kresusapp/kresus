85dcdac6d79c02ff2f633d233f4a41066bbfff31
ZeHiro <mail-github@abossy.fr>
2017-02-02 23:59:43 +0100

    Use npm package for jquery, jquery.cookie and jquery.minicolors

--- scripts/build-vendor-js.sh
+++ scripts/build-vendor-js.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
- cat ./client/vendor/*.js \
-    ./node_modules/bootstrap/dist/js/bootstrap.min.js \
-    ./client/vendor/**/*.js > ./build/client/js/vendor.js
+ cat ./node_modules/jquery/dist/jquery.js \
+     ./node_modules/bootstrap/dist/js/bootstrap.min.js \
+     ./node_modules/jquery.cookie/jquery.cookie.js \
+     ./node_modules/jquery-minicolors/jquery.minicolors.js \
+     ./client/vendor/*.js \
+     ./node_modules/bootstrap/dist/js/bootstrap.min.js \
+     ./client/vendor/**/*.js > ./build/client/js/vendor.js
