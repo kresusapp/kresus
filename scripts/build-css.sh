c08cd0ceabab1bdcedd40db6aec9c95076abbd7c
ZeHiro <mail-github@abossy.fr>
2017-02-03 00:47:46 +0100

    Use npm package for pickadate

--- scripts/build-css.sh
+++ scripts/build-css.sh
@@ -2,7 +2,9 @@
 cat ./node_modules/bootstrap/dist/css/bootstrap.css \
     ./node_modules/bootstrap/dist/css/bootstrap-theme.css \
     ./client/css/*.css \
-    ./client/css/**/*.css \
     ./node_modules/dygraphs/dist/dygraph.css \
     ./node_modules/c3/c3.css \
+    ./node_modules/pickadate/lib/themes/classic.css \
+    ./node_modules/pickadate/lib/themes/classic.date.css \
+    ./node_modules/pickadate/lib/themes/classic.time.css \
     ./node_modules/jquery-minicolors/jquery.minicolors.css > ./build/client/css/main.css
