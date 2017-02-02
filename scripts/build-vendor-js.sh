2079af8149776fde7f06df7f95b4c07b5e5e2a52
ZeHiro <mail-github@abossy.fr>
2017-02-03 00:47:46 +0100

    Use npm package for pickadate

--- scripts/build-vendor-js.sh
+++ scripts/build-vendor-js.sh
@@ -2,6 +2,8 @@
  cat ./node_modules/jquery/dist/jquery.js \
      ./node_modules/jquery.cookie/jquery.cookie.js \
      ./node_modules/jquery-minicolors/jquery.minicolors.js \
+     ./node_modules/pickadate/lib/picker.js \
+     ./node_modules/pickadate/lib/picker.date.js \
+     ./node_modules/pickadate/lib/picker.time.js \
      ./client/vendor/*.js \
-     ./node_modules/bootstrap/dist/js/bootstrap.min.js \
-     ./client/vendor/**/*.js > ./build/client/js/vendor.js
+     ./node_modules/bootstrap/dist/js/bootstrap.js > ./build/client/js/vendor.js
