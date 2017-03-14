699655f785df68e0fa9f81a58cd5376769890f74
ZeHiro <mail-github@abossy.fr>
2017-03-14 22:34:14 +0100

    Ensure translation keys required by banks are present in the tranlsation files. Fix #547

--- scripts/check-banks.js
+++ scripts/check-banks.js
@@ -6,7 +6,7 @@ import * as fs from 'fs';
 import banks from '../shared/banks.json';
 
 let log = require('printit')({
-    prefix: 'check-logos'
+    prefix: 'check-banks'
 });
 
 let imagesDir = path.join(path.dirname(fs.realpathSync(__filename)),
@@ -16,13 +16,24 @@ let imagesDir = path.join(path.dirname(fs.realpathSync(__filename)),
                           'banks');
 
 let missingLogo = false;
+
+let fieldTranslationKeys = new Set();
+log.info('Checking all banks have a logo');
 for (let bank of banks) {
     try {
+        // Test for logo existence
         let imagePath = path.join(imagesDir, `${bank.uuid}.png`);
         fs.accessSync(imagePath, fs.F_OK);
     } catch (e) {
         log.error(`Missing logo for ${bank.uuid}.`);
         missingLogo = true;
+    }
+    if (typeof bank.customFields !== 'undefined') {
+        for (let field of bank.customFields) {
+            if (typeof field.labelKey !== 'undefined' && !fieldTranslationKeys.has(field.labelKey)){
+                fieldTranslationKeys.add(field.labelKey, '');
+            }
+        }
     }
 }
 
@@ -30,7 +41,9 @@ if (missingLogo) {
     log.error('At least one missing logo.');
     process.exit(1);
 }
+log.info('All banks have a logo: OK');
 
+log.info('Checking all bank logos are used');
 let imageFiles = [];
 fs.readdirSync(imagesDir).forEach(child => {
     let file = path.join(imagesDir, child);
@@ -57,5 +70,26 @@ if (orphanImage) {
     process.exit(1);
 }
 
-log.info('CheckLogos: OK.');
+log.info('Checking all bank form labelKey have a translation');
+let localesPath = path.join(path.dirname(fs.realpathSync(__filename)), '..', 'shared', 'locales');
+let missingBankLocale = false;
+
+for (let locale of fs.readdirSync(localesPath)) {
+    let localeFile = require(path.join(localesPath, locale));
+    for (let key of fieldTranslationKeys.keys()) {
+        // Deep inspection of localeFile
+        let value = key.split('.').reduce((trans, k) => { return typeof trans === 'undefined' ? trans : trans[k] }, localeFile);
+        if (typeof value === 'undefined') {
+            log.error(`Missing key ${key} in ${locale} file`);
+            missingBankLocale = true;
+        }
+    }
+}
+
+if (missingBankLocale) {
+    log.error('At least one missing translation for bank form keys.');
+    process.exit(1);
+}
+
+log.info('Bank data is OK.');
 process.exit(0);
