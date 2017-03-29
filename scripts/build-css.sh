0b4ee674de44d8d40acd3f4fe303d5a891a5c7e8
ZeHiro <mail-github@abossy.fr>
2017-03-29 22:43:33 +0200

    Fix

--- scripts/build-css.sh
+++ scripts/build-css.sh
@@ -10,4 +10,6 @@ cat ./node_modules/normalize.css/normalize.css \
     ./node_modules/pickadate/lib/themes/classic.date.css \
     ./node_modules/pickadate/lib/themes/classic.time.css \
     ./node_modules/jquery-minicolors/jquery.minicolors.css \
-    ./build/client/css/sprite.css > ./build/client/css/main.css
+    ./build/client/css/sprite.css > ./build/client/css/main.css &&
+rm ./build/client/css/sprite.css
+
