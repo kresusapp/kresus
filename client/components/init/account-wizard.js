967013d8551b186ad566b69676427da97907b502
ZeHiro <mail-github@abossy.fr>
2017-04-05 08:32:20 +0200

    Fix

--- client/components/init/account-wizard.js
+++ client/components/init/account-wizard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, Redirect } from 'react-router';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { translate as $t } from '../../helpers';
 
 import ImportModule from '../settings/backup/import';
