586967c4ac55789f4bb3d5b67dbe4159468a5712
Benjamin Bouvier <public@benj.me>
2017-05-09 22:14:12 +0200

    Simplify tests;

--- tests/test-is-weboob-installed.js
+++ tests/test-is-weboob-installed.js
@@ -1,60 +1,46 @@
 import should from 'should';
+import semver from 'semver';
 
 import { get } from '../client/store';
-import { MIN_WEBOOB_VERSION } from '../client/helpers';
-import { normalizeVersion } from '../client/helpers';
-import semver from 'semver';
+import { MIN_WEBOOB_VERSION, normalizeVersion  } from '../client/helpers';
+
+function makeState(installed, version) {
+    return {
+        settings: {
+            map: {
+                'weboob-installed': `${installed}`,
+                'weboob-version': version
+            }
+        }
+    };
+}
 
 describe('isWeboobInstalled', () => {
     describe('should return false when', () => {
         it("'weboob-installed' set to false", () => {
-            let state = { settings: { map : { 'weboob-installed': 'false' } } };
+            let state = makeState(false, undefined);
             get.isWeboobInstalled(state).should.equal(false);
         });
+
         it("'weboob-installed' set to true and 'weboob-version' < MIN_WEBOOB_VERSION", () => {
-            let state = {
-                settings: {
-                    map : {
-                            'weboob-installed': 'true',
-                            'weboob-version': '1.0.0',
-                        }
-                    }
-            };
+            let state = makeState(true, '0.0.1');
             get.isWeboobInstalled(state).should.equal(false);
         });
+
         it("'weboob-installed' set to true and 'weboob-version' === 0.h", () => {
-            let state = {
-                settings: {
-                    map : {
-                            'weboob-installed': 'true',
-                            'weboob-version': '0.h',
-                        }
-                    }
-            };
+            let state = makeState(true, '0.h');
             get.isWeboobInstalled(state).should.equal(false);
         });
     });
+
     describe('should return true when', () => {
         it("'weboob-installed' set to true and 'weboob-version' === MIN_WEBOOB_VERSION", () => {
-            let state = {
-                settings: {
-                    map : {
-                            'weboob-installed': 'true',
-                            'weboob-version': MIN_WEBOOB_VERSION,
-                        }
-                    }
-            };
+            let state = makeState(true, MIN_WEBOOB_VERSION);
             get.isWeboobInstalled(state).should.equal(true);
         });
-        it("'weboob-installed' set to true and MIN_WEBOOB_VERSION <= 'weboob-version'", () => {
-            let state = {
-                settings: {
-                    map : {
-                            'weboob-installed': 'true',
-                            'weboob-version': semver.inc(normalizeVersion(MIN_WEBOOB_VERSION), 'minor'),
-                        }
-                    }
-            };
+
+        it("'weboob-installed' set to true and 'weboob-version' > MIN_WEBOOB_VERSION", () => {
+            let state = makeState(true, semver.inc(normalizeVersion(MIN_WEBOOB_VERSION), 'minor'));
             get.isWeboobInstalled(state).should.equal(true);
         });
     });
