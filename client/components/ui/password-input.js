3d647ff410b39dc208690ae4f98b12a82508614e
Nicolas Frandeboeuf <mail@nicofrand.eu>
2017-05-17 08:46:55 +0200

    Allow to show/hide password in password inputs (fix #578)

---
+++ client/components/ui/password-input.js
@@ -0,0 +1,64 @@
+import React from 'react';
+
+import { translate as $t } from '../../helpers';
+
+class PasswordInput extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showPassword: false
+        };
+
+        this.input = null;
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState({
+            showPassword: !this.state.showPassword
+        });
+    }
+
+    getValue() {
+        return this.input.value.trim();
+    }
+
+    render() {
+        let inputCb = node => {
+            this.input = node;
+        };
+
+        let iconClass = 'eye';
+        let type = 'password';
+
+        if (this.state.showPassword) {
+            iconClass = 'eye-slash';
+            type = 'text';
+        }
+
+        return (
+            <div className="input-group">
+                <input
+                  type={ type }
+                  className="form-control"
+                  id={ this.props.id }
+                  ref={ inputCb }
+                />
+                <span
+                  type="button"
+                  className={ `clickable input-group-addon fa fa-${iconClass}` }
+                  onClick={ this.handleClick }
+                  title={ $t('client.general.show_password') }
+                />
+            </div>
+        );
+    }
+}
+
+PasswordInput.propTypes = {
+    // The id attribute used to match labels
+    id: React.PropTypes.string.isRequired
+};
+
+export default PasswordInput;
