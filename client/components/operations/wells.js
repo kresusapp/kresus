c4bab4d2b5f3b26d1f6e585ef0f2f02877af25d0
ZeHiro <mail-github@abossy.fr>
2017-06-09 11:57:53 +0200

    Externalize wells of the report page so that they are not rerendered when changing (label/type/category) an operation

---
+++ client/components/operations/wells.js
@@ -0,0 +1,131 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
+
+import { translate as $t,
+         wellsColors,
+         formatDate } from '../../helpers';
+
+import { get } from '../../store';
+
+import AmountWell from './amount-well';
+
+const { BALANCE, RECEIVED, SPENT, SAVED } = wellsColors;
+
+function filterOperationsThisMonth(operations) {
+    let now = new Date();
+    return operations.filter(op => {
+        let d = new Date(op.date);
+        return d.getFullYear() === now.getFullYear() && d.getMonth() === now.getMonth();
+    });
+}
+
+function computeTotal(filterFunction, operations, initial = 0) {
+    let total = operations
+                .filter(filterFunction)
+                .reduce((a, b) => a + b.amount, initial);
+    return Math.round(total * 100) / 100;
+}
+
+const Wells = props => {
+    return (
+        <div className="operation-wells">
+            <AmountWell
+              backgroundColor={ BALANCE }
+              icon="balance-scale"
+              title={ $t('client.operations.current_balance') }
+              subtitle={ props.lastCheckDate }
+              content={ props.balance }
+            />
+
+            <AmountWell
+              backgroundColor={ RECEIVED }
+              icon="arrow-down"
+              title={ $t('client.operations.received') }
+              subtitle={ props.filteredSub }
+              content={ props.earningsSum }
+            />
+
+            <AmountWell
+              backgroundColor={ SPENT }
+              icon="arrow-up"
+              title={ $t('client.operations.spent') }
+              subtitle={ props.filteredSub }
+              content={ props.spendingsSum }
+            />
+
+            <AmountWell
+              backgroundColor={ SAVED }
+              icon="database"
+              title={ $t('client.operations.saved') }
+              subtitle={ props.filteredSub }
+              content={ props.savingSum }
+            />
+        </div>
+    );
+
+};
+
+const thisMonthOperationsSelector = createSelector(
+    (state, accountId) => get.operationsByAccountIds(state, accountId),
+    ops => filterOperationsThisMonth(ops)
+);
+
+const Export = connect((state, props) => {
+    const { account } = props;
+    const filteredOperations = get.filteredOperationsByAccountId(state, account.id);
+    const thisMonthOperations = thisMonthOperationsSelector(state, account.id);
+    const hasSearchFields = get.hasSearchFields(state);
+
+    // Operations to be considered to compute the wells sums.
+    let wellOperations;
+
+    // Subtitle of earnings, spendings and saving wells.
+    let filteredSub;
+
+    if (hasSearchFields) {
+        wellOperations = filteredOperations;
+        filteredSub = $t('client.amount_well.current_search');
+    } else {
+        wellOperations = thisMonthOperations;
+        filteredSub = $t('client.amount_well.this_month');
+    }
+
+    const { formatCurrency } = account;
+
+    // Sum to be displayed in the earnings well.
+    const earningsSum = computeTotal(x => x.amount > 0, wellOperations, 0);
+
+    // Sum to be displayed in the spendings well.
+    const spendingsSum = computeTotal(x => x.amount < 0, wellOperations, 0);
+
+    // Sum to be displayed in the savings well.
+    const savingSum = earningsSum + spendingsSum;
+
+    // Balance of the current account.
+    // TODO: Use a selector, when it is used for the menu.
+    const operations = get.operationsByAccountIds(state, props.account.id);
+    const balance = computeTotal(() => true, operations, props.account.initialAmount);
+
+    // Date of the last sync.
+    let asOf = $t('client.operations.as_of');
+    let lastCheckedDate = formatDate.toShortString(props.account.lastChecked);
+    let lastCheckDate = `${asOf} ${lastCheckedDate}`;
+
+    return {
+        filteredSub,
+        earningsSum: formatCurrency(earningsSum),
+        spendingsSum: formatCurrency(spendingsSum),
+        savingSum: formatCurrency(savingSum),
+        balance: formatCurrency(balance),
+        lastCheckDate
+    };
+})(Wells);
+
+Export.propTypes = {
+    account: PropTypes.object.isRequired
+};
+
+export default Export;
