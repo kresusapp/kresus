404bf57018036bf8e89477edb72e1fc74e52d273
ZeHiro <mail-github@abossy.fr>
2017-06-11 01:14:12 +0200

    Memoize category and options selects for better rendering performances. Rendering of select is 10x faster and memory consumption is lowered by a 100ko for a 40 categories select.

--- client/components/operations/index.js
+++ client/components/operations/index.js
@@ -78,9 +78,6 @@ class OperationsComponent extends React.Component {
                                    key={ o.id }
                                    operation={ o }
                                    formatCurrency={ this.props.account.formatCurrency }
-                                   categories={ this.props.categories }
-                                   getCategory={ this.props.getCategory }
-                                   types={ this.props.types }
                                    onOpenModal={ handleOpenModal }
                                    onLongPress={ handleOpenModal }
                                  />
@@ -155,9 +152,6 @@ class OperationsComponent extends React.Component {
                 <DetailsModal
                   ref={ refDetailsModal }
                   formatCurrency={ format }
-                  categories={ this.props.categories }
-                  types={ this.props.types }
-                  getCategory={ this.props.getCategory }
                 />
 
                 <div className="operation-wells">
@@ -307,18 +301,12 @@ const Export = connect((state, ownProps) => {
     let operations = get.operationsByAccountIds(state, accountId);
     let hasSearchFields = get.hasSearchFields(state);
     let filteredOperations = filter(operations, get.searchFields(state));
-    let categories = get.categories(state);
-    let types = get.types(state);
-    let getCategory = categoryId => get.categoryById(state, categoryId);
 
     return {
         account,
         operations,
         filteredOperations,
-        hasSearchFields,
-        categories,
-        types,
-        getCategory
+        hasSearchFields
     };
 })(OperationsComponent);
 
