5787a5ddc40bbcc74bb3a3c15ee4d98a76774eb1
ZeHiro <mail-github@abossy.fr>
2017-06-30 19:38:36 +0200

    Allow to disable an access. Fixes #597

--- client/components/settings/bank-accesses/edit-access-modal.js
+++ client/components/settings/bank-accesses/edit-access-modal.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import { translate as $t } from '../../../helpers';
+import { get } from '../../../store';
 
 import CustomBankField from './custom-bank-field';
 import Modal from '../../ui/modal';
 import PasswordInput from '../../ui/password-input';
 
 class EditAccessModal extends React.Component {
-
     constructor(props) {
         super(props);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {
+            showDisableButton: true
+        };
 
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleClick = this.handleClick.bind(this);
         this.extractCustomFieldValue = this.extractCustomFieldValue.bind(this);
 
         this.loginInput = null;
         this.passwordInput = null;
+        this.form = null;
         this.customFieldsInputs = [];
     }
 
@@ -26,38 +32,48 @@ class EditAccessModal extends React.Component {
 
     handleSubmit(event) {
         event.preventDefault();
-
-        let newLogin = this.loginInput.value.trim();
-        let newPassword = this.passwordInput.getValue();
-        if (!newPassword.length) {
-            alert($t('client.editaccessmodal.not_empty'));
-            return;
-        }
-
-        let customFields;
-        if (this.props.customFields) {
-            customFields = this.props.customFields.map(this.extractCustomFieldValue);
-            if (customFields.some(f => !f.value)) {
-                alert($t('client.editaccessmodal.customFields_not_empty'));
+        let update;
+        if (this.props.access.isActive) {
+            update = { isActive: false };
+        } else {
+            let newLogin = this.loginInput.value.trim();
+            let newPassword = this.passwordInput.getValue();
+            if (!newPassword.length) {
+                alert($t('client.editaccessmodal.not_empty'));
                 return;
             }
+
+            let customFields;
+            let { access } = this.props;
+            if (access.customFields && access.customFields.length) {
+                customFields = access.customFields.map(this.extractCustomFieldValue);
+                if (customFields.some(f => !f.value)) {
+                    alert($t('client.editaccessmodal.customFields_not_empty'));
+                    return;
+                }
+            }
+
+            update = { isActive: true, login: newLogin, password: newPassword, customFields };
+            this.passwordInput.clear();
         }
-
-        this.props.onSave(newLogin, newPassword, customFields);
-        this.passwordInput.clear();
-
+        this.props.onSave(update);
         $(`#${this.props.modalId}`).modal('hide');
     }
 
-    render() {
-        this.customFieldsInputs = [];
+    handleClick() {
+        this.setState({ showDisableButton: false });
+    }
 
+    render() {
         let customFields;
-        if (this.props.customFields) {
-            customFields = this.props.customFields.map((field, index) => {
-                let refCustomFieldInput = input => {
-                    this.customFieldsInputs.push(input);
-                };
+        let { access } = this.props;
+
+        let refCustomFieldInput = input => {
+            this.customFieldsInputs.push(input);
+        };
+
+        if (access.customFields && access.customFields.length) {
+            customFields = access.customFields.map((field, index) => {
                 return (
                     <CustomBankField
                       key={ index }
@@ -78,15 +94,84 @@ class EditAccessModal extends React.Component {
         let refPasswordInput = element => {
             this.passwordInput = element;
         };
+        let refForm = element => {
+            this.form = element;
+        };
 
-        let modalBody = (
-            <div>
-                { $t('client.editaccessmodal.body') }
+        let explanations = null;
+        if (access.isActive) {
+            if (this.state.showDisableButton) {
+                explanations = (
+                    <p className="alert alert-info">
+                        { $t('client.editaccessmodal.info_before_disable_access') }
+                    </p>
+                );
+            } else {
+                explanations = (
+                    <p className="alert alert-warning">
+                        { $t('client.editaccessmodal.warn_before_submit_disable_access') }
+                    </p>
+                );
+            }
+        } else if (this.state.showDisableButton) {
+            explanations = (
+                <p className="alert alert-info">
+                    { $t('client.editaccessmodal.disabled_access') }
+                </p>
+            );
+        }
 
-                <form
-                  id={ `${this.props.modalId}-form` }
-                  className="form-group"
-                  onSubmit={ this.handleSubmit }>
+        let maybeButton = null;
+        if (this.state.showDisableButton) {
+            if (access.isActive) {
+                maybeButton = (
+                    <button
+                      type="button"
+                      className="btn btn-default"
+                      onClick={ this.handleClick }>
+                        { $t('client.editaccessmodal.disable') }
+                    </button>
+                );
+            } else {
+                maybeButton = (
+                    <button
+                      type="button"
+                      className="btn btn-default"
+                      onClick={ this.handleClick }>
+                        { $t('client.editaccessmodal.enable') }
+                    </button>
+                );
+            }
+        }
+
+        let maybeDisableForm = null;
+        if (access.isActive || this.state.showDisableButton) {
+            maybeDisableForm = (
+                <div>
+                    <h4>
+                        { $t(access.isActive ?
+                          'client.editaccessmodal.disable_access' :
+                          'client.editaccessmodal.enable_access') }
+                    </h4>
+                    { explanations }
+                    { maybeButton }
+                </div>
+            );
+        }
+
+        let maybeCredentials = null;
+
+        // Display the credentials form if :
+        // - the access is enabled and the user didn't click on the disable button.
+        // - the access is disabled ant the user clicked on the enable button.
+        if ((access.isActive && this.state.showDisableButton) ||
+            (!access.isActive && !this.state.showDisableButton)) {
+            maybeCredentials = (
+                <div>
+                    <h4>
+                        { $t('client.editaccessmodal.edit_credentials') }
+                    </h4>
+                    { $t('client.editaccessmodal.body') }
                     <div className="form-group">
                         <label htmlFor="login">
                             { $t('client.settings.login') }
@@ -109,8 +194,23 @@ class EditAccessModal extends React.Component {
                           ref={ refPasswordInput }
                         />
                     </div>
-
                     { customFields }
+                    { maybeDisableForm ? <hr /> : null }
+                </div>
+            );
+        }
+
+        let modalBody = (
+            <div>
+                <form
+                  id={ `${this.props.modalId}-form` }
+                  ref={ refForm }
+                  onSubmit={ this.handleSubmit }>
+
+                    { maybeCredentials }
+
+                    { maybeDisableForm }
+
                 </form>
             </div>
         );
@@ -133,7 +233,19 @@ class EditAccessModal extends React.Component {
         );
 
         let focusPasswordField = () => {
-            this.passwordInput.focus();
+            if (this.state.isActive) {
+                this.passwordInput.focus();
+            }
+        };
+
+        // Reset the modal before open, if the user closed the modal in the middle
+        // of the disable process.
+        const handleBeforeOpen = () => {
+            this.setState({ showDisableButton: true });
+        };
+
+        let resetForm = () => {
+            this.form.reset();
         };
 
         return (
@@ -151,9 +263,8 @@ class EditAccessModal extends React.Component {
 }
 
 const Export = connect((state, props) => {
-    let access = get.accessById(state, props.accessId);
     return {
-        access
+        access: get.accessById(state, props.accessId)
     };
 })(EditAccessModal);
 
