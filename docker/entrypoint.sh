60d11ea12fa696e5bd834d46dc0c2b1fa2d18113
Benjamin Bouvier <public@benj.me>
2017-07-02 02:32:30 +0200

    Make Dockerfiles great again;

---
+++ docker/entrypoint.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -e
+
+# Add local user
+# Either use the LOCAL_USER_ID if passed in at runtime or
+# fallback
+
+USER_ID=${LOCAL_USER_ID:-9001}
+
+echo "[ENTRYPOINT] Starting with UID : $USER_ID"
+usermod -u $USER_ID user
+export HOME=/home/user
+
+cd /weboob
+if [ ! -d /weboob/.git ]; then
+    echo "[ENTRYPOINT] Installing weboob..."
+    git clone --depth 1 https://git.weboob.org/weboob/devel .
+    echo "[ENTRYPOINT] Done installing."
+else
+    echo "[ENTRYPOINT] Updating weboob..."
+    git pull --depth 1
+    echo "[ENTRYPOINT] Done installing."
+fi
+
+echo "[ENTRYPOINT] Updating Weboob dependencies..."
+cd /weboob
+python ./setup.py requirements > /tmp/requirements.txt
+pip install -r /tmp/requirements.txt
+rm /tmp/requirements.txt
+echo "[ENTRYPOINT] Done updating weboob dependencies."
+
+echo "[ENTRYPOINT] Trying to update kresus..."
+npm update -g kresus
+echo "[ENTRYPOINT] Done."
+
+echo "[ENTRYPOINT] Changing rights..."
+chown -R user:user /home/user
+chmod -R 777 /weboob
+
+# TODO temporary fix until we get a proper location for the weboob data dir
+chown -R user:user /usr/local/lib/node_modules/kresus || echo "(no kresus found in /usr/local/lib)"
+
+echo "[ENTRYPOINT] Running kresus as user."
+exec su user -c "$@"
