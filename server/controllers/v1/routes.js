35285afdb637f4282b291ee8270b5bffd63984a1
Phyks (Lucas Verney) <phyks@phyks.me>
2017-07-20 17:38:42 +0200

    Draft of v1 API

--- server/controllers/v1/routes.js
+++ server/controllers/v1/routes.js
@@ -16,6 +16,7 @@ export default function (app) {
         title: 'Initialization'
     });
 
+    // TODO
     const allAPI = initializationAPI.api('/all');
     allAPI.get({
         handler: allControllers.all
@@ -30,34 +31,38 @@ export default function (app) {
     });
 
     // Accesses
-    const accessesAPI = v1API.api({
-        title: 'Accesses'
+    const accesses = v1API.api('/accesses', {
+        title: 'Accesses',
+        description: 'Accesses are pairs of credentials to access bank websites. An access gives access to a given bank, and to potentially multiple bank accounts.'
     });
-
-    const accessesEndpointAPI = accessesAPI.api('/accesses');
-    accessesEndpointAPI.post({
+    accesses.post({
+        title: 'Create a new access',
         handler: accessesControllers.create
     });
 
-    const accessesPollAPI = accessesEndpointAPI.api('/poll');
-    accessesPollAPI.get({
-        handler: accessesControllers.poll
-    });
-
-    const accessAPI = accessesEndpointAPI.api('/:accessId');
-    accessAPI.put({
+    const access = accesses.api('/:accessId');
+    access.put({
+        title: 'Edit a given access',
         handler: accessesControllers.update
     });
-    accessAPI.delete({
+    access.delete({
+        title: 'Delete a given access',
         handler: accessesControllers.destroy
     });
 
-    const accessGetAccountsAPI = accessAPI.api('/accounts');
-    accessGetAccountsAPI.get({
+    const accessAccounts = access.api('/accounts');
+    accessAccounts.get({
+        title: 'Get the accounts associated with a given access',
         handler: accessesControllers.getAccounts
     });
-    const accessFetchAPI = accessAPI.api('/fetch');
 
+    const accessesPoll = accesses.api('/poll');
+    accessesPoll.get({
+        handler: accessesControllers.poll
+    });
+
+    // TODO
+    const accessFetchAPI = access.api('/fetch');
     const accessFetchOperationsAPI = accessFetchAPI.api('/operations');
     accessFetchOperationsAPI.get({
         handler: accessesControllers.fetchOperations
@@ -68,73 +73,110 @@ export default function (app) {
     });
 
     // Accounts
-    const accountsAPI = v1API.api({
-        title: 'Accounts'
+    const accounts = v1API.api('/accounts', {
+        title: 'Bank accounts'
+    });
+    accounts.get({
+        title: 'List all bank accounts',
+        handler: (request, reponse) => {
+            // TODO
+        }
     });
 
-
-    const accountsEndpointAPI = accountsAPI.api('/accounts');
-
-    const accountAPI = accountsEndpointAPI.api(':accountId');
-    accountAPI.delete({
+    const account = accounts.api(':accountId');
+    account.get({
+        title: 'Get a given bank account',
+        handler: (request, reponse) => {
+            // TODO
+        }
+    });
+    account.delete({
+        title: 'Delete a given bank account',
         handler: accountsControllers.destroy
     });
 
-    const accountOperationsAPI = accountAPI.api('/operations');
-    accountOperationsAPI.get({
+    const accountOperations = account.api('/operations');
+    accountOperations.get({
+        title: 'Get operations from a given bank account',
         handler: accountsControllers.getOperations
     });
 
-    const accountResyncBalanceAPI = accountAPI.api('/resync-balance');
+    // TODO
+    const accountResyncBalanceAPI = account.api('/resync-balance');
     accountResyncBalanceAPI.get({
         handler: accountsControllers.resyncBalance
     });
 
     // Categories
-    const categoriesAPI = v1API.api({
+    const categories = v1API.api('/categories', {
         title: 'Categories'
     });
-
-    const categoriesEndpointAPI = categoriesAPI.api('/categories');
-    categoriesEndpointAPI.post({
+    categories.get({
+        title: 'List all categories',
+        handler: (request, reponse) => {
+            // TODO
+        }
+    });
+    categories.post({
+        title: 'Create a new category',
         handler: categoriesControllers.create
     });
 
-
-    const categoryAPI = categoriesAPI.api('/:categoryId');
-    categoryAPI.put({
+    const category = categories.api('/:categoryId');
+    category.get({
+        title: 'Get a given category',
+        handler: (request, response) => {
+            // TODO
+        }
+    });
+    category.put({
+        title: 'Edit a given category',
         handler: categoriesControllers.update
     });
-    categoryAPI.delete({
+    category.delete({
+        title: 'Delete a given category',
         handler: categoriesControllers.destroy
     });
 
     // Operations
-    const operationsAPI = v1API.api({
+    const operations = v1API.api('/operations', {
         title: 'Operations'
     });
-
-
-
-    const operationsEndpointAPI = operationsAPI.api('operations');
-    operationsEndpointAPI.post({
+    operations.get({
+        title: 'List all operations',
+        handler: (request, reponse) => {
+            // TODO
+        }
+    });
+    operations.post({
+        title: 'Create a new operation',
         handler: operationsControllers.create
     });
 
-    const operationAPI = operationsEndpointAPI.api('/:operationId');
-    operationAPI.put({
+    const operation = operations.api('/:operationId');
+    operation.get({
+        title: 'Get a given operation',
+        handler: (request, reponse) => {
+            // TODO
+        }
+    });
+    operation.put({
+        title: 'Edit a given operation',
         handler: operationsControllers.update
     });
-    operationAPI.delete({
+    operation.delete({
+        title: 'Delete a given operation',
         handler: operationsControllers.destroy
     });
 
-    const operationMergeWithAPI = operationAPI.api('/mergeWith/:otherOperationId');
+    // TODO
+    const operationMergeWithAPI = operation.api('/mergeWith/:otherOperationId');
     operationMergeWithAPI.put({
         handler: operationsControllers.merge
     });
 
-    const operationFileAPI = operationAPI.api('/:file');
+    // TODO
+    const operationFileAPI = operation.api('/:file');
     operationFileAPI.get({
         handler: operationsControllers.file
     });
@@ -144,44 +186,75 @@ export default function (app) {
         title: 'Settings'
     });
     settings.post({
+        title: 'Update stored Kresus settings',
         handler: settingsControllers.save
     });
 
-    const weboobSettings = settings.api('/weboob');
-    weboobSettings.put({
-        handler: settingsControllers.updateWeboob
+    // Weboob
+    const weboob = v1API.api('/weboob', {
+        title: 'Weboob management'
     });
-
-    const testEmailSettings = settings.api('/test-email');
-    testEmailSettings.post({
-        handler: settingsControllers.testEmail
+    const weboobUpdate = weboob.api('/actions');
+    weboob.post({
+        title: 'Run some command on the Weboob daemon',
+        handler: settingsControllers.updateWeboob
     });
 
     // Alerts
     const alerts = v1API.api('/alerts', {
         title: 'Alerts'
     });
+    alerts.get({
+        title: 'List all alerts',
+        handler: (request, reponse) => {
+            // TODO
+        }
+    });
     alerts.post({
+        title: 'Create a new alert on your bank accounts',
         handler: alertsControllers.create
     });
 
     const alert = alerts.api('/:alertId');
+    alert.get({
+        title: 'Get a given alert',
+        handler: (request, reponse) => {
+            // TODO
+        }
+    });
     alert.put({
+        title: 'Edit a given alert on your bank accounts',
         handler: alertsControllers.update
     });
     alert.delete({
+        title: 'Delete a given alert on your bank accounts',
         handler: alertsControllers.destroy
     });
 
+    // Tests
+    const tests = v1API.api('/tests', {
+        title: 'Tests'
+    });
+    // TODO
+    const testSendMail = tests.api('/send-email');
+    testSendMail.post({
+        title: 'Test email sending',
+        description: 'Check that the Kresus instance can send email',
+        handler: settingsControllers.testEmail
+    });
+
     // API reference
-    const referenceAPI = v1API.api('/reference');
+    const referenceAPI = v1API.api('/reference', {
+        title: 'API reference'
+    });
     referenceAPI.get({
+        title: 'Get the API documentation',
         handler: (request, response) => {
             response.send(v1API.toHTML());
         }
     });
 
-    // Bind on URL parameters
+    // Binding on URL parameters
     app.param('accessId', accessesControllers.preloadAccess);
     app.param('accountId', accountsControllers.preloadAccount);
     app.param('alertId', alertsControllers.loadAlert);
