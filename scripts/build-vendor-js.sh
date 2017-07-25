7c1ec282701138df5c40e74a20f08de20bce4557
Phyks (Lucas Verney) <phyks@phyks.me>
2017-07-25 11:23:01 +0200

    Rework scripts and Makefile according to latest Webpack config

--- scripts/build-vendor-js.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
- cat ./node_modules/jquery/dist/jquery.js \
-     ./node_modules/jquery.cookie/jquery.cookie.js \
-     ./node_modules/jquery-minicolors/jquery.minicolors.js \
-     ./node_modules/bootstrap-kresus/js/bootstrap.js > ./build/client/js/vendor.js
