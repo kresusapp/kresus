2d906c465445c632e07d2d4e0bf9d4ceef88714e
Phyks (Lucas Verney) <phyks@phyks.me>
2017-07-25 17:15:55 +0200

    Put Charts component in a separate chunk, lazy loaded

---
+++ client/components/lazyLoader.js
@@ -0,0 +1,45 @@
+import { PureComponent } from 'react';
+
+import { assert } from '../helpers';
+
+/*
+ * Wrapper around components for lazy loading
+ * Adapted from https://reacttraining.com/react-router/web/guides/code-splitting
+ *
+ * IMPORTANT: This LazyLoader can handle only a single component. This means it
+ * won't work if you update the `load` prop after building it. This is enforced
+ * by assert.
+ */
+export default class LazyLoader extends PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mod: null
+        };
+    }
+
+    componentWillMount() {
+        this.load(this.props);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        assert(nextProps.load === this.props.load);
+    }
+
+    load(props) {
+        props.load(mod => {
+            this.setState({
+                // handle both ES imports and CommonJS
+                mod: mod.default ? mod.default : mod
+            });
+        });
+    }
+
+    render() {
+        return (
+            this.state.mod ?
+            this.props.children(this.state.mod) :
+            null
+        );
+    }
+}
