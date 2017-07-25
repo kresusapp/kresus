cccc856bf0e5e929079d593216af25e3124ef61d
Phyks (Lucas Verney) <phyks@phyks.me>
2017-07-25 11:23:01 +0200

    Rework scripts

--- scripts/build-server.sh
+++ scripts/build-server.sh
@@ -7,38 +7,12 @@ then
 fi
 echo "Building in $NODE_ENV mode..."
 
-rm -rf ./build/
-
-echo "Copying static files..."
-mkdir -p ./build/client
-cp -r ./static/* ./build/client
+rm -rf ./build/server
 
 echo "Creating banks icons sprite..."
 mkdir -p ./build/client/images
 mkdir -p ./build/client/css
 ./scripts/sprite.sh ./build/client/images/sprite.svg ./build/client/css/sprite.css
-
-echo "Concatening and copying CSS..."
-./scripts/build-css.sh
-
-echo "Concatening and copying vendor JS..."
-mkdir -p ./build/client/js
-./scripts/build-vendor-js.sh
-
-echo "Building client JS..."
-
-# Only remove developement imports and long package ids on production
-if [ "$NODE_ENV" = "production" ]
-then
-    SUPP_PRODUCTION_BUILD_ARGS="-g uglifyify \
-                                -p bundle-collapser/plugin"
-fi
-
-./node_modules/browserify/bin/cmd.js ./client/main.js -v \
-    -t [ babelify --presets es2015,react --plugins transform-runtime ] \
-    -g [ envify --NODE_ENV $NODE_ENV ] \
-    $SUPP_PRODUCTION_BUILD_ARGS \
-    -o ./build/client/js/main.js
 
 echo "Copying shared files..."
 mkdir -p ./build/server/shared
