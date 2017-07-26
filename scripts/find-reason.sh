7834ca951ab42d4ec9fdb5cef2d262909435969e
Phyks (Lucas Verney) <phyks@phyks.me>
2017-07-26 10:39:40 +0200

    Add analyzing features
    When passing `ANALYZE=true` to Webpack process, it will analyze the
    resulting bundle to show size of the imported modules in each chunk.
    
    It saves the reports in `build/reports` and opens the default browser
    with the generated report displayed. It uses `webpack-bundle-analyzer`
    under the hood.
    
    It also exports the raw JSON file from Webpack, which can be used to
    look at `reasons` key and see why a given file is being included in the
    chunk.
    
    Import react-datepicker without importing the minified blob, better for
    webpack-bundle-analyzer and to avoid bugs with minification step
    
    Also add a script to find why a given module has been included in the
    webpack bundle, thanks @nicofrand

---
+++ scripts/find-reason.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Parse a JSON output from Webpack to find why a given module was imported.
+# Requires jq and an `ANALYZE=true` build of the client code.
+
+# Go to Kresus root
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/..
+
+# Analyze
+cat build/reports/client.json | jq '.chunks[] .modules[] | select(.name | contains($SEARCH)) as $mod | .reasons[] | select(.moduleName | contains($SEARCH) | not) | .moduleName + "," + $mod.name' --arg SEARCH $1 | \
+    (echo "Source file,Included file" && cat) | sed -e 's/^"//' -e 's/"$//' | column -t -s","
