8e2a53c1caa4e8e80c599faa4c11f55efb668db6
Phyks (Lucas Verney) <phyks@phyks.me>
2017-07-26 10:39:40 +0200

    Add analyzing features
    When passing `ANALYZE=true` to Webpack process, it will analyze the
    resulting bundle to show size of the imported modules in each chunk.
    
    It saves the reports in `build/reports` and opens the default browser
    with the generated report displayed. It uses `webpack-bundle-analyzer`
    under the hood.
    
    It also exports the raw JSON file from Webpack, which can be used to
    look at `reasons` key and see why a given file is being included in the
    chunk.
    
    Import react-datepicker without importing the minified blob, better for
    webpack-bundle-analyzer and to avoid bugs with minification step
    
    Also add a script to find why a given module has been included in the
    webpack bundle, thanks @nicofrand

--- scripts/webpack/base.js
+++ scripts/webpack/base.js
@@ -6,7 +6,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const SpritesmithPlugin = require('webpack-spritesmith');
 
-module.exports = {
+const config = {
     entry: [
         './node_modules/normalize.css/normalize.css',
         './node_modules/font-awesome/css/font-awesome.css',
@@ -126,3 +126,38 @@ module.exports = {
         })
     ]
 }
+
+if (process.env.ANALYZE) {
+    const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+    config.plugins = config.plugins.concat([
+        // Generate analyzer reports if needed
+        new BundleAnalyzerPlugin({
+            // Can be `server`, `static` or `disabled`.
+            // In `server` mode analyzer will start HTTP server to show bundle report.
+            // In `static` mode single HTML file with bundle report will be generated.
+            // In `disabled` mode you can use this plugin to just generate Webpack Stats JSON file by setting `generateStatsFile` to `true`.
+            analyzerMode: 'static',
+
+            // Path to bundle report file that will be generated in `static` mode.
+            // Relative to bundles output directory.
+            reportFilename: '../reports/client.html',
+
+            // Module sizes to show in report by default.
+            // Should be one of `stat`, `parsed` or `gzip`.
+            // See "Definitions" section for more information.
+            defaultSizes: 'parsed',
+
+            // Automatically open report in default browser
+            openAnalyzer: true,
+
+            // If `true`, Webpack Stats JSON file will be generated in bundles output directory
+            generateStatsFile: true,
+
+            // Name of Webpack Stats JSON file that will be generated if `generateStatsFile` is `true`.
+            // Relative to bundles output directory.
+            statsFilename: '../reports/client.json',
+        })
+    ]);
+}
+
+module.exports = config;
