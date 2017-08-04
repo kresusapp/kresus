f226e7f452e2b3bdd801445d93b1f695db48c0cd
Phyks (Lucas Verney) <phyks@phyks.me>
2017-08-04 19:59:57 +0200

    Make the CI great again

--- scripts/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Compare locales
-./node_modules/babel-cli/bin/babel-node.js --presets es2015 ./scripts/compare-locales.js && \
-# Check all bank related data is correct (logo, translation keys ...)
-./node_modules/babel-cli/bin/babel-node.js --presets es2015 ./scripts/check-banks.js && \
-# Run proper tests
-./node_modules/mocha/bin/mocha ./tests/ --recursive --require babel-register
