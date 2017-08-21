5f332d5e8e2793cf77a5c1bed4bf54fb46e75ed4
Phyks (Lucas Verney) <phyks@phyks.me>
2017-08-21 22:06:03 +0200

    Rework dev server to prevent zombies processes
    This closes #637.

--- scripts/dev-server.sh
+++ scripts/dev-server.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
+set -e
 
-mkdir -p ./build
-mkdir -p ./build/server
-mkdir -p ./build/server/shared
+function process {
+    mkdir -p ./build
+    mkdir -p ./build/server
+    mkdir -p ./build/server/shared
 
-# Shared code
-(./node_modules/onchange/cli.js './shared/*.json' -v -- cp -r ./shared/*.json ./build/server/shared) &
+    # Shared code
+    (./node_modules/onchange/cli.js "./shared/*.json" -iv -- cp -r ./shared/*.json ./build/server/shared) &
 
-# Server JS
-./node_modules/babel-cli/bin/babel.js \
-    --presets es2015,stage-0 \
-    --plugins transform-runtime \
-    ./server/ \
-    -d ./build/server \
-    -w &
+    # Server JS
+    (./node_modules/babel-cli/bin/babel.js \
+        --presets es2015,stage-0 \
+        --plugins transform-runtime \
+        ./server/ \
+        -d ./build/server \
+        -w) &
 
-# Server py
-(./node_modules/onchange/cli.js './server/weboob/main.py' -v -- cp ./server/weboob/main.py ./build/server/weboob && chmod +x ./build/server/weboob/main.py) &
+    # Server py
+    (./node_modules/onchange/cli.js "./server/weboob/main.py" -iv -- cp ./server/weboob/main.py ./build/server/weboob && chmod +x ./build/server/weboob/main.py) &
+}
+
+process
+wait
