19bbbddf55c16939ecac2f1bda15a5895d30ace4
Phyks (Lucas Verney) <phyks@phyks.me>
2017-08-21 22:06:03 +0200

    Rework dev server to prevent zombies processes, fixes #637.

--- scripts/dev-server.sh
+++ scripts/dev-server.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
+set -e
 
 mkdir -p ./build
 mkdir -p ./build/server
 mkdir -p ./build/server/shared
 
 # Shared code
-(./node_modules/onchange/cli.js './shared/*.json' -v -- cp -r ./shared/*.json ./build/server/shared) &
+(./node_modules/onchange/cli.js "./shared/*.json" -iv -- cp -r ./shared/*.json ./build/server/shared) &
 
 # Server JS
-./node_modules/babel-cli/bin/babel.js \
+(./node_modules/babel-cli/bin/babel.js \
     --presets es2015,stage-0 \
     --plugins transform-runtime \
     ./server/ \
     -d ./build/server \
-    -w &
+    -w) &
 
 # Server py
-(./node_modules/onchange/cli.js './server/weboob/main.py' -v -- cp ./server/weboob/main.py ./build/server/weboob && chmod +x ./build/server/weboob/main.py) &
+(./node_modules/onchange/cli.js "./server/weboob/main.py" -iv -- cp ./server/weboob/main.py ./build/server/weboob && chmod +x ./build/server/weboob/main.py) &
+
+wait
