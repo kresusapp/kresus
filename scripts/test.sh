49bc0397769b34b3cdb1ada964e2a30ab45f0bd3
Phyks (Lucas Verney) <phyks@phyks.me>
2017-08-23 12:31:36 +0200

    Make the CI great again
    Thanks to @ZeHiro for the ci checking script.

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
 set -e
 
-# Compare locales
-./node_modules/babel-cli/bin/babel-node.js --presets es2015 ./scripts/compare-locales.js
-# Check all bank related data is correct (logo, translation keys ...)
-./node_modules/babel-cli/bin/babel-node.js --presets es2015 ./scripts/check-banks.js
 # Run proper tests
 BABEL_ENV=tests mocha --require babel-polyfill --compilers js:babel-register --recursive ./tests
