080f7b3682fc1a0b99fdaed29cf93190ebd34921
Phyks (Lucas Verney) <phyks@phyks.me>
2017-09-25 01:10:55 +0200

    Provide a fake bank using Weboob backend
    Also fix an issue with `Rebuild index` being `print`ed on `stdout` by
    Weboob.

--- scripts/dev-server.sh
+++ scripts/dev-server.sh
@@ -6,7 +6,6 @@ rm -rf ./build/server
 
 # Create directories
 mkdir -p ./build/server
-mkdir ./build/server/weboob
 mkdir ./build/server/shared
 mkdir ./build/server/shared/locales
 
@@ -18,14 +17,16 @@ cp ./shared/locales/*.json ./build/server/shared/locales
 (./node_modules/onchange/cli.js "./shared/*.json" -v -- cp '{{changed}}' ./build/server/shared) &
 (./node_modules/onchange/cli.js "./shared/locales/*.json" -v -- cp '{{changed}}' ./build/server/shared/locales) &
 
-# Server JS
+# Server JS code
 (./node_modules/babel-cli/bin/babel.js \
     --presets env,stage-0 \
     ./server/ \
     -d ./build/server \
     -w) &
 
-# Server py
-(./node_modules/onchange/cli.js "./server/weboob/main.py" -iv -- cp ./server/weboob/main.py ./build/server/weboob/ && chmod +x ./build/server/weboob/main.py) &
+# Server Weboob code
+# Initial build, required because {{changed}} is not filled at initial run.
+cp -r ./server/weboob ./build/server/weboob && chmod +x ./build/server/weboob/main.py
+(./node_modules/onchange/cli.js "./server/weboob/**/*.py" -v -- cp './{{changed}}' './build/{{changed}}' && chmod +x ./build/server/weboob/main.py) &
 
 wait
