afee956de052991b4c3710316bb6543d627a5a15
Phyks (Lucas Verney) <phyks@phyks.me>
2017-09-25 01:10:55 +0200

    Provide a fake bank using Weboob backend
    Also fix an issue with `Rebuild index` being `print`ed on `stdout` by
    Weboob.

---
+++ server/weboob/fakemodules/fakeweboobbank/module.py
@@ -0,0 +1,227 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import copy
+import datetime
+import random
+import time
+from decimal import Decimal
+
+from weboob.core.ouiboube import WebNip
+from weboob.capabilities.bank import Currency
+from weboob.capabilities.bank import CapBank, Account, Transaction
+from weboob.exceptions import (
+    ActionNeeded,
+    BrowserIncorrectPassword,
+    BrowserPasswordExpired,
+    NoAccountsException,
+    ModuleInstallError,
+    ModuleLoadError
+)
+from weboob.tools.backend import Module, BackendConfig
+from weboob.tools.value import ValueBackendPassword, Value
+
+__all__ = ['FakeBankModule']
+
+
+# With 2% rate, import a non-existing module
+n = random.randrange(100)
+if n < 2:
+    import NotExistingModule
+
+
+class GenericException(Exception):
+    """
+    A generic exception that we could miss to catch.
+    """
+    pass
+
+
+class FakeBankModule(Module, CapBank):
+    NAME = 'fakeweboobbank'
+    MAINTAINER = u'Phyks'
+    EMAIL = 'phyks@phyks.me'
+    VERSION = WebNip.VERSION
+    DESCRIPTION = u'Fake bank module'
+    LICENSE = 'AGPLv3+'
+    CONFIG = BackendConfig(ValueBackendPassword('login',    label='Identifiant', masked=False),
+                           ValueBackendPassword('password', label='Code personnel'),
+                           Value('website', label='Type de compte', default='par',
+                                 choices={'par': 'Particuliers',
+                                          'pro': 'Professionnels'
+                                         }),
+                           Value('trucmuche', label='Ce que vous voulez',
+                                 default='')
+                          )
+    BROWSER = None
+
+    def random_errors(self):
+        for exc in [
+                ActionNeeded,
+                BrowserIncorrectPassword,
+                BrowserPasswordExpired,
+                NoAccountsException,
+                ModuleInstallError,
+                NotImplementedError,
+                GenericException,
+                Exception
+        ]:
+            # With 2% rate, raise an exception
+            n = random.randrange(100)
+            if n < 2:
+                raise exc('Random error')
+        # Raise a ModuleLoadError with 2% rate
+        n = random.randrange(100)
+        if n < 2:
+            raise ModuleLoadError('FakeWeboobBank', 'Random error')
+
+    def iter_accounts(self):
+        # Throw random errors
+        self.random_errors()
+
+        accounts = []
+
+        first_account = Account()
+        first_account.id = 'FR235711131719@fakebank'
+        first_account.label = 'Compte chèque'
+        first_account.currency = Currency.get_currency('42 €')
+        first_account.iban = 'FR235711131719'
+        first_account.balance = random.uniform(0, 150)
+        accounts.append(first_account)
+
+        second_account = Account()
+        second_account.id = 'livretA@fakebank'
+        second_account.label = 'Livret A'
+        second_account.currency = Currency.get_currency('$42')
+        second_account.balance = Decimal(500.0)
+        accounts.append(second_account)
+
+        third_account = Account()
+        third_account.id = 'PEL@fakebank'
+        third_account.label = 'Plan Epargne Logement'
+        third_account.balance = Decimal(0.0)
+        accounts.append(third_account)
+
+        return accounts
+
+    def generate_date(self, low_day, high_day, low_month, high_month):
+        year = datetime.datetime.now().year
+        low = datetime.datetime.strptime(
+            '%d/%d/%d' % (low_day, low_month, year),
+            '%d/%m/%Y'
+        )
+        high = datetime.datetime.strptime(
+            '%d/%d/%d' % (high_day, high_month, year),
+            '%d/%m/%Y'
+        )
+        return datetime.datetime.fromtimestamp(
+            random.randint(
+                int(time.mktime(low.timetuple())),
+                int(time.mktime(high.timetuple()))
+            )
+        )
+
+    def generate_type(self):
+        return random.choice([
+            Transaction.TYPE_UNKNOWN,
+            Transaction.TYPE_TRANSFER,
+            Transaction.TYPE_ORDER,
+            Transaction.TYPE_CHECK,
+            Transaction.TYPE_DEPOSIT,
+            Transaction.TYPE_PAYBACK,
+            Transaction.TYPE_WITHDRAWAL,
+            Transaction.TYPE_CARD,
+            Transaction.TYPE_LOAN_PAYMENT,
+            Transaction.TYPE_BANK,
+            Transaction.TYPE_CASH_DEPOSIT,
+            Transaction.TYPE_CARD_SUMMARY,
+            Transaction.TYPE_DEFERRED_CARD
+        ])
+
+    def generate_label(self, positive=False):
+        if positive:
+            return random.choice([
+                ('VIR Nuage Douillet', 'VIR Nuage Douillet REFERENCE Salaire'),
+                ('Impots', 'Remboursement impots en votre faveur'),
+                ('', 'VIR Pots de vin et magouilles pas claires'),
+                ('Case départ', 'Passage par la case depart'),
+                ('Assurancetourix', 'Remboursement frais médicaux pour plâtre généralisé')
+            ])
+        return random.choice([
+            ('Café Moxka', 'Petit expresso rapido Café Moxka'),
+            ('MerBnB', 'Paiement en ligne MerBNB'),
+            ('Tabac Debourg', 'Bureau de tabac SARL Clopi Cloppa'),
+            ('Rapide PSC', 'Paiement sans contact Rapide'),
+            ('MacDollars PSC', 'Paiement sans contact Macdollars'),
+            ('FNAK', 'FNAK CB blabla'),
+            ('CB Sefaurat', 'Achat de parfum chez Sefaurat'),
+            ('Polyprix CB', 'Courses chez Polyprix'),
+            ('Croisement CB', 'Courses chez Croisement'),
+            ('PRLV UJC', 'PRLV UJC'),
+            ('CB Spotifaille', 'CB Spotifaille London'),
+            ('Antiquaire', 'Antiquaire'),
+            ('Le Perroquet Bourré', 'Le Perroquet Bourré SARL'),
+            ('Le Vol de Nuit', 'Bar Le Vol De Nuit SARL'),
+            ('Impots fonciers',
+             'Prelevement impots fonciers numero reference 47839743892 client 43278437289'),
+            ('ESPA Carte Hassan Cehef', 'Paiement carte Hassan Cehef'),
+            ('Indirect Energie', 'ESPA Indirect Energie SARL'),
+            ('', 'VIR Mr Jean Claude Dusse'),
+            ('Nuage Douillet', 'ESPA Abonnement Nuage Douillet'),
+            ('Glagla Frigidaire', 'CB GLAGLA FRIGIDAIRE'),
+            ('Digiticable', 'ESPA Digiticable'),
+            ('NOGO Sport', 'CB NOGO Sport'),
+            ('FramaHard', 'ESPA Don FramaHard'),
+            ('Sergent Tchoutchou', 'CB online Sergent Tchoutchou'),
+            ('RAeTP', 'CB Raleurs Ambulants et Traficoteurs Patentés')
+        ])
+
+    def generate_single_transaction(self):
+        now = datetime.datetime.now()
+
+        transaction = Transaction()
+        transaction.type = self.generate_type()
+
+        n = random.randrange(100)
+        if n < 2:
+            # with a 2% rate, generate a special operation to test duplicates
+            # (happening on 4th of current month).
+            transaction.amount = Decimal(-300.0)
+            transaction.label = "Loyer"
+            transaction.raw = "Loyer habitation"
+            transaction.date = self.generate_date(4, 4, now.month, now.month)
+            return transaction
+
+        transaction.date = self.generate_date(1, now.day, 1, now.month - 1)
+
+        if n < 15:
+            transaction.label, transaction.raw = self.generate_label(
+                positive=True
+            )
+            transaction.amount = Decimal(
+                random.randint(100, 800) + random.random()
+            )
+            return transaction
+
+        if n < 30:
+            transaction.rdate = transaction.date
+        elif n < 60:
+            transaction.date = None
+            transaction.rdate = None
+
+        transaction.amount = Decimal(random.randint(-60, 0) + random.random())
+        transaction.label, transaction.raw = self.generate_label()
+        transaction.type = self.generate_type()
+        return transaction
+
+    def iter_history(self, account):
+        # Throw random errors
+        self.random_errors()
+
+        transactions = []
+
+        # Generate some transactions
+        for _ in range(random.randrange(15)):
+            transactions.append(self.generate_single_transaction())
+
+        return transactions
--- server/weboob/main.py
+++ server/weboob/main.py
@@ -203,6 +203,11 @@ class Connector(object):
             # required by Kresus.
             sources_list_lines = [
                 'https://updates.weboob.org/%(version)s/main/',
+                (
+                    'file://%s/fakemodules/' % (
+                        os.path.dirname(os.path.abspath(__file__))
+                    )
+                )
             ]
 
             # Get sources.list lines.
@@ -213,6 +218,10 @@ class Connector(object):
         """
         Update Weboob modules.
         """
+        # Weboob has an offending print statement when it "Rebuilds index",
+        # which happen at every run if the user has a local repository. We need
+        # to silence it, hence the temporary redirect of stdout.
+        sys.stdout = open(os.devnull, "w")
         try:
             self.weboob.update(progress=DummyProgress())
         except:
@@ -225,6 +234,9 @@ class Connector(object):
             self.write_weboob_sources_list()
             # Retry update
             self.weboob.update(progress=DummyProgress())
+        finally:
+            # Restore stdout
+            sys.stdout = sys.__stdout__
 
     def create_backend(self, modulename, parameters):
         """
