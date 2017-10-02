8aee2e9faa45398ead5e4eceeb32e9386d208eb4
Antoine BOSSY <mail+github@abossy.fr>
2017-10-02 19:52:03 +0200

    Do not display the smtp password in the logs

--- server/apply-config.js
+++ server/apply-config.js
@@ -83,6 +83,11 @@ module.exports = function prepareProcessKresus(standalone, config) {
     }
     process.kresus.smtpRejectUnauthorizedTLS = toBool(smtpRejectUnauthorizedTLS);
 
+    // Do not display the password in the log.
+    let displayedPassword = '*'.repeat(process.kresus.smtpPassword ?
+                                       process.kresus.smtpPassword.length :
+                                       0);
+
     let mode = standalone ? 'standalone' : 'cozy';
     log.info(`Running Kresus in ${mode} mode, with the following parameters:
 - KRESUS_DIR = ${process.kresus.dataDir}
@@ -96,7 +101,7 @@ module.exports = function prepareProcessKresus(standalone, config) {
 - KRESUS_EMAIL_HOST = ${process.kresus.smtpHost}
 - KRESUS_EMAIL_PORT = ${process.kresus.smtpPort}
 - KRESUS_EMAIL_USER = ${process.kresus.smtpUser}
-- KRESUS_EMAIL_PASSWORD = ${process.kresus.smtpPassword}
+- KRESUS_EMAIL_PASSWORD = ${displayedPassword}
 - KRESUS_EMAIL_FORCE_TLS = ${process.kresus.smtpForceTLS}
 - KRESUS_EMAIL_REJECT_UNAUTHORIZED_TLS = ${process.kresus.smtpRejectUnauthorizedTLS}
 `);
