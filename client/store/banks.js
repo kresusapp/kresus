e5419290409f02c27875d8e5986adf7d5bb071a3
Antoine BOSSY <mail+github@abossy.fr>
2017-10-08 16:54:24 +0200

    Move defaultAccountId from state.settings to state.banks + add optimistic update to setting the default account*

--- client/store/banks.js
+++ client/store/banks.js
@@ -10,6 +10,8 @@ import { assert,
 
 import { Account, Access, Alert, Bank, Operation } from '../models';
 
+import DefaultSettings from '../../shared/default-settings';
+
 import Errors, { genericErrorHandler } from '../errors';
 
 import * as backend from './backend';
@@ -648,6 +650,11 @@ function reduceDeleteAccountInternal(state, accountId) {
         ret = u.updateIn('accesses', u.reject(a => a.id === bankAccess), ret);
     }
 
+    // Reset defaultAccountId if necessary.
+    if (accountId === getDefaultAccountId(state)) {
+        ret = u({ defaultAccountId: DefaultSettings.get('defaultAccountId') }, ret);
+    }
+
     return ret;
 }
 
@@ -820,6 +827,17 @@ function reduceDeleteCategory(state, action) {
                       state);
 }
 
+function reduceSetDefaultAccount(state, action) {
+    if (action.status === SUCCESS) {
+        return state;
+    }
+    if (action.status === FAIL) {
+        return u({ defaultAccountId: action.previousDefaultAccountId }, state);
+    }
+    // Optimistic update.
+    return u({ defaultAccountId: action.accountId }, state);
+}
+
 // Initial state.
 const bankState = u({
     // A list of the banks.
@@ -847,6 +865,7 @@ const reducers = {
     RUN_ACCOUNTS_SYNC: reduceRunAccountsSync,
     RUN_BALANCE_RESYNC: reduceResyncBalance,
     RUN_OPERATIONS_SYNC: reduceRunOperationsSync,
+    SET_DEFAULT_ACCOUNT: reduceSetDefaultAccount,
     SET_OPERATION_CATEGORY: reduceSetOperationCategory,
     SET_OPERATION_CUSTOM_LABEL: reduceSetOperationCustomLabel,
     SET_OPERATION_TYPE: reduceSetOperationType,
@@ -944,7 +963,8 @@ export function initialState(external, allAccesses, allAccounts, allOperations, 
         currentAccountId,
         constants: {
             defaultCurrency
-        }
+        },
+        defaultAccountId
     }, {});
 }
 
@@ -1016,4 +1036,8 @@ export function alertPairsByType(state, alertType) {
     }
 
     return pairs;
+}
+
+export function getDefaultAccountId(state) {
+    return state.defaultAccountId;
 }
