24c7dec37773660108ea5da4e05776d106c721a7
ZeHiro <public+github@abossy.fr>
2017-10-11 12:26:37 +0000

    Lint

--- client/components/operations/details.js
+++ client/components/operations/details.js
@@ -143,14 +143,14 @@ let DetailsModal = props => {
         return null;
     }
 
-    let onDelete = props.makeHandleDeleteOperation(props.operation);
-
     let views = {
         details: switchView => {
             return fillShowDetails(props, () => switchView('confirm-delete'));
         },
         'confirm-delete': switchView => {
-            return fillConfirmDelete(props, () => switchView('details'), onDelete);
+            return fillConfirmDelete(props,
+                                     () => switchView('details'),
+                                     props.handleDeleteOperation);
         }
     };
 
