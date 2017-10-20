758ce38698ecca4de3f5fef7cf744dde7f2f1279
Phyks (Lucas Verney) <phyks@phyks.me>
2017-10-20 09:41:28 -0400

    Use istanbul for code coverage

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,4 +2,4 @@
 set -e
 
 # Run proper tests
-BABEL_ENV=tests mocha --require babel-polyfill --compilers js:babel-register --recursive ./tests
+BABEL_ENV=tests nyc --require babel-register --reporter text --source-map false --instrument false --exclude "tests/**" mocha --recursive ./tests
