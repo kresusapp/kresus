52e1cec3670924f247ddc7b3dd5604c5b05bf659
Phyks (Lucas Verney) <phyks@phyks.me>
2017-10-30 02:05:39 +0100

    Full refactor of the onboarding UI
    * Rework the theme and the messages.
    * Let user scroll on small screens, fix #627.
    * Set initial language according to user's locale
    
    Introduce a `locale` configuration option to set the default locale to
    be used in Kresus. Server part uses this default locale when it starts
    and creates a matching configuration option in database. Client defaults
    to `en` locale and upon completing a request to `/all` fetches the
    default locale to be used from the server.
    
    Additionally, onboarding screen now has a locale selector to choose the
    locale to use.
    
    Known caveat: If admin sets a default locale, starts the server, then
    edits the configuration to set a different default locale and restarts
    the server, the new configuration option will not be taken into account.

--- client/components/init/account-wizard.js
+++ client/components/init/account-wizard.js
@@ -1,52 +1,117 @@
 import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 import { translate as $t } from '../../helpers';
 
-import ImportModule from '../settings/backup/import';
 import NewBankForm from '../settings/bank-accesses/form';
-import WeboobParameters from '../settings/weboob';
-import TabMenu from '../ui/tab-menu.js';
+import ImportModule from '../settings/backup/import';
+import LocaleSelector from '../menu/locale-selector';
 
 const PATH_PREFIX = '/initialize';
 
 export default class AccountWizard extends React.Component {
-    constructor(props) {
-        super(props);
-        this.menuItems = new Map();
-        this.menuItems.set(`${PATH_PREFIX}/new-bank`, $t('client.settings.new_bank_form_title'));
-        this.menuItems.set(`${PATH_PREFIX}/import`, $t('client.accountwizard.import_title'));
-        this.menuItems.set(`${PATH_PREFIX}/advanced`, $t('client.accountwizard.advanced'));
-    }
+    renderBankForm = () => (
+        <div>
+            <header>
+                <h1>{$t('client.accountwizard.letsgo')}</h1>
+            </header>
+            <NewBankForm isOnboarding={true} />
+        </div>
+    );
 
-    renderBankForm = () => <NewBankForm expanded={true} />;
+    handleNewBankClick = () => this.props.history.push(`${PATH_PREFIX}/new-bank`);
+    handleImportClick = () => this.props.history.push(`${PATH_PREFIX}/import`);
+    handleDemoClick = () => this.props.history.push(`${PATH_PREFIX}/demo-mode`);
+
+    renderMenu = () => (
+        <div>
+            <header>
+                <LocaleSelector />
+                <h1>{$t('client.accountwizard.welcome')}</h1>
+            </header>
+            <p>{$t('client.accountwizard.description')}</p>
+
+            <nav className="init-wells">
+                <div onClick={this.handleNewBankClick}>
+                    <h3>
+                        <i className="fa fa-plus small-only" />
+                        {$t('client.accountwizard.menu.add_first_access_title')}
+                    </h3>
+                    <div>
+                        <p>
+                            <i className="fa fa-plus" />
+                        </p>
+                        <p>{$t('client.accountwizard.menu.add_first_access_desc')}</p>
+                    </div>
+                    <p className="add-first-access-well-button">
+                        <Link to={`${PATH_PREFIX}/new-bank`}>
+                            {$t('client.accountwizard.menu.add_first_access_action')}
+                        </Link>
+                    </p>
+                </div>
+
+                <div onClick={this.handleImportClick}>
+                    <h3>
+                        <i className="fa fa-upload small-only" />
+                        {$t('client.accountwizard.menu.import_title')}
+                    </h3>
+                    <div>
+                        <p>
+                            <i className="fa fa-download" />
+                        </p>
+                        <p>{$t('client.accountwizard.menu.import_desc')}</p>
+                    </div>
+                    <p className="import-well-button">
+                        <Link to={`${PATH_PREFIX}/import`}>
+                            {$t('client.accountwizard.menu.import_action')}
+                        </Link>
+                    </p>
+                </div>
+
+                <div onClick={this.handleDemoClick}>
+                    <h3>
+                        <i className="fa fa-laptop small-only" />
+                        {$t('client.accountwizard.menu.demo_title')}
+                    </h3>
+                    <div>
+                        <p>
+                            <i className="fa fa-laptop" />
+                        </p>
+                        <p>{$t('client.accountwizard.menu.demo_desc')}</p>
+                    </div>
+                    <p className="demo-well-button">
+                        <Link to={`${PATH_PREFIX}/demo-mode`}>
+                            {$t('client.accountwizard.menu.demo_action')}
+                        </Link>
+                    </p>
+                </div>
+            </nav>
+        </div>
+    );
 
     renderImport = () => (
         <div>
+            <header>
+                <h1>{$t('client.accountwizard.letsimport')}</h1>
+            </header>
+
             <p>{$t('client.accountwizard.import')}</p>
-            <ImportModule />
+            <div className="accountwizard-import">
+                <ImportModule />
+            </div>
         </div>
     );
 
     render() {
         return (
-            <div className="wizard panel panel-default">
-                <div className="panel-heading">
-                    <h1 className="panel-title">{$t('client.accountwizard.title')}</h1>
-                </div>
-                <div className="panel-body">
-                    <p>{$t('client.accountwizard.content')}</p>
-                    <TabMenu
-                        selected={this.props.location.pathname}
-                        tabs={this.menuItems}
-                        history={this.props.history}
-                        location={this.props.location}
-                    />
-                    <Switch>
-                        <Route path={`${PATH_PREFIX}/new-bank`} render={this.renderBankForm} />
-                        <Route path={`${PATH_PREFIX}/import`} render={this.renderImport} />
-                        <Route path={`${PATH_PREFIX}/advanced`} component={WeboobParameters} />
-                        <Redirect to={`${PATH_PREFIX}/new-bank`} push={false} />
-                    </Switch>
+            <div className="wizard">
+                <div>
+                    <div>
+                        <Switch>
+                            <Route path={`${PATH_PREFIX}/new-bank`} render={this.renderBankForm} />
+                            <Route path={`${PATH_PREFIX}/import`} render={this.renderImport} />
+                            <Route path={`${PATH_PREFIX}`} render={this.renderMenu} />
+                        </Switch>
+                    </div>
                 </div>
             </div>
         );
