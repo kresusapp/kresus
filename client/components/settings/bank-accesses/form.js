ed506cc4c2701d6fea5af1b8e785021a38c25fa3
Phyks (Lucas Verney) <phyks@phyks.me>
2017-10-30 02:05:39 +0100

    Full refactor of the onboarding UI
    * Rework the theme and the messages, fix #648.
    * Let user scroll on small screens, fix #627.
    * Allow user to add default set of categories and alerts, fix #39.
    * Use react-select, fix #483.
    
    Additionally, onboarding screen now has a locale selector to choose the
    locale to use.

--- client/components/settings/bank-accesses/form.js
+++ client/components/settings/bank-accesses/form.js
@@ -1,69 +1,95 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import Select from 'react-select';
 
 import { get, actions } from '../../../store';
-import { assert, translate as $t } from '../../../helpers';
+import { translate as $t } from '../../../helpers';
 
 import PasswordInput from '../../ui/password-input';
-import FoldablePanel from '../../ui/foldable-panel';
 
 import CustomBankField from './custom-bank-field';
+import DefaultCategories from '../../../../shared/default-categories.json';
 
-class NewBankForm extends React.Component {
+class InitForm extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            selectedBankIndex: 0
+        this.initialState = {
+            selectedBankIndex: -1,
+            defaultAlertsEnabled: props.emailEnabled,
+            defaultCategoriesEnabled: props.isOnboarding,
+            password: null,
+            login: null,
+            emailRecipient: props.emailRecipient
         };
+        this.state = Object.assign({}, this.initialState);
 
         this.form = null;
-        this.bankSelector = null;
-        this.loginInput = null;
-        this.passwordInput = null;
-
-        this.password = '';
         this.formCustomFields = new Map();
     }
 
     selectedBank() {
-        return this.props.banks[this.state.selectedBankIndex];
+        if (this.state.selectedBankIndex > -1) {
+            return this.props.banks[this.state.selectedBankIndex];
+        }
+        return null;
     }
 
-    handleCustomFieldChange = (name, value) => {
-        this.formCustomFields.set(name, value);
-    };
-
-    handleReset = event => {
-        this.setState({
-            selectedBankIndex: 0
-        });
-        event.target.reset();
-    };
-
-    handleChangeBank = event => {
-        let uuid = event.target.value;
-        let selectedBankIndex = this.props.banks.findIndex(bank => bank.uuid === uuid);
-
-        if (selectedBankIndex !== -1) {
-            this.setState({ selectedBankIndex });
-        } else {
-            assert(false, "unreachable: the bank didn't exist?");
+    handleChangeBank = selectedValue => {
+        let selectedBankIndex = -1;
+        if (selectedValue) {
+            let uuid = selectedValue.value;
+            selectedBankIndex = this.props.banks.findIndex(bank => bank.uuid === uuid);
         }
+
+        this.setState({ selectedBankIndex });
+    };
+
+    handleChangeDefaultAlerts = event => {
+        this.setState({
+            defaultAlertsEnabled: event.target.checked
+        });
+    };
+
+    handleChangeLogin = event => {
+        this.setState({
+            login: event.target.value
+        });
     };
 
     handleChangePassword = event => {
-        this.password = event.target.value;
+        this.setState({
+            password: event.target.value
+        });
+    };
+
+    handleCustomFieldChange = (name, value) => {
+        // TODO: This should be moved in the state for consistency.
+        this.formCustomFields.set(name, value);
+    };
+
+    handleChangeDefaultCategories = event => {
+        this.setState({
+            defaultCategoriesEnabled: event.target.checked
+        });
+    };
+
+    handleChangeEmail = event => {
+        this.setState({
+            emailRecipient: event.target.value
+        });
+    };
+
+    refForm = element => {
+        this.form = element;
     };
 
     handleSubmit = event => {
         event.preventDefault();
 
-        let uuid = this.bankSelector.value;
-        let login = this.loginInput.value.trim();
-
-        let staticCustomFields = this.selectedBank().customFields;
+        let selectedBank = this.selectedBank();
+        let staticCustomFields = selectedBank.customFields;
 
         let customFields = [];
         if (staticCustomFields.length) {
@@ -83,7 +109,7 @@ class NewBankForm extends React.Component {
             });
         }
 
-        if (!login.length || !this.password.length) {
+        if (!this.state.login.length || !this.state.password.length) {
             alert($t('client.settings.missing_login_or_password'));
             return;
         }
@@ -94,25 +120,47 @@ class NewBankForm extends React.Component {
             return;
         }
 
-        this.props.createAccess(uuid, login, this.password, customFields);
+        const createDefaultAlerts = this.state.defaultAlertsEnabled;
+        // Save email address if required
+        if (createDefaultAlerts && this.state.emailRecipient) {
+            this.props.saveEmail(this.state.emailRecipient);
+        }
+
+        // Create access
+        this.props.createAccess(
+            selectedBank.uuid,
+            this.state.login,
+            this.state.password,
+            customFields,
+            createDefaultAlerts
+        );
+
+        // Handle default categories
+        if (this.state.defaultCategoriesEnabled) {
+            let categories = DefaultCategories.map(category =>
+                Object.assign(category, {
+                    title: $t(category.title) // Translate category title
+                })
+            );
+            this.props.createCategories(categories);
+        }
 
         // Reset the form and internal memories.
         this.form.reset();
-        this.password = '';
+        this.setState(this.initialState);
         this.formCustomFields.clear();
     };
 
     render() {
-        let options = this.props.banks.map(bank => (
-            <option key={bank.id} value={bank.uuid}>
-                {bank.name}
-            </option>
-        ));
+        let options = this.props.banks.map(bank => ({
+            value: bank.uuid,
+            label: bank.name
+        }));
 
         let selectedBankDescr = this.selectedBank();
 
         let maybeCustomFields = null;
-        if (selectedBankDescr.customFields.length > 0) {
+        if (selectedBankDescr && selectedBankDescr.customFields.length > 0) {
             maybeCustomFields = selectedBankDescr.customFields.map(field => {
                 return (
                     <CustomBankField
@@ -125,107 +173,165 @@ class NewBankForm extends React.Component {
             });
         }
 
-        let refBankSelector = element => {
-            this.bankSelector = element;
-        };
-        let refLoginInput = element => {
-            this.loginInput = element;
-        };
-        let refPasswordInput = element => {
-            this.passwordInput = element;
-        };
-        let refForm = element => {
-            this.form = element;
-        };
+        let isDisabledSubmit = false;
+        if (
+            this.selectedBank() === null ||
+            !this.state.login ||
+            !this.state.password ||
+            (this.state.defaultAlertsEnabled && !this.state.emailRecipient)
+        ) {
+            isDisabledSubmit = true;
+        }
+
+        let maybeCategories = null;
+        if (this.props.isOnboarding) {
+            maybeCategories = (
+                <div className="row">
+                    <div className="col-sm-12">
+                        <input
+                            type="checkbox"
+                            id="default-categories"
+                            checked={this.state.defaultCategoriesEnabled}
+                            onChange={this.handleChangeDefaultCategories}
+                        />{' '}
+                        <label htmlFor="default-categories">
+                            {$t('client.accountwizard.default_categories')}
+                        </label>
+                        <p>
+                            <small>{$t('client.accountwizard.default_categories_desc')}</small>
+                        </p>
+                    </div>
+                </div>
+            );
+        }
+
+        let maybeAlerts = null;
+        if (this.props.emailEnabled) {
+            let maybeEmailField = null;
+            if (this.state.defaultAlertsEnabled) {
+                maybeEmailField = (
+                    <div>
+                        <label htmlFor="email">{$t('client.settings.emails.send_to')}</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="email"
+                            placeholder="me@example.com"
+                            value={this.state.emailRecipient}
+                            onChange={this.handleChangeEmail}
+                        />
+                    </div>
+                );
+            }
+            maybeAlerts = (
+                <div className="row">
+                    <div className="col-sm-12">
+                        <input
+                            type="checkbox"
+                            id="default-alerts"
+                            defaultChecked="true"
+                            onChange={this.handleChangeDefaultAlerts}
+                        />{' '}
+                        <label htmlFor="default-alerts">
+                            {$t('client.accountwizard.default_alerts')}
+                        </label>
+                        <p>
+                            <small>{$t('client.accountwizard.default_alerts_desc')}</small>
+                        </p>
+                        {maybeEmailField}
+                    </div>
+                </div>
+            );
+        }
 
         return (
-            <FoldablePanel
-                className="new-bank-panel"
-                initiallyExpanded={this.props.expanded}
-                title={$t('client.settings.new_bank_form_title')}
-                iconTitle={$t('client.settings.add_bank_button')}
-                top={true}>
-                <form ref={refForm} onReset={this.handleReset} onSubmit={this.handleSubmit}>
-                    <div className="form-group">
-                        <label htmlFor="bank">{$t('client.settings.bank')}</label>
-                        <select
-                            className="form-control"
-                            id="bank"
-                            ref={refBankSelector}
-                            onChange={this.handleChangeBank}
-                            defaultValue={selectedBankDescr.uuid}>
-                            {options}
-                        </select>
-                    </div>
-
-                    <div className="form-group">
-                        <div className="row">
-                            <div className="col-sm-6">
-                                <label htmlFor="id">{$t('client.settings.login')}</label>
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    id="id"
-                                    ref={refLoginInput}
-                                />
-                            </div>
-
-                            <div className="col-sm-6">
-                                <label htmlFor="password">{$t('client.settings.password')}</label>
-                                <PasswordInput
-                                    ref={refPasswordInput}
-                                    onChange={this.handleChangePassword}
-                                    id="password"
-                                />
-                            </div>
+            <form className="initform" ref={this.refForm} onSubmit={this.handleSubmit}>
+                <div className="form-group has-overflow">
+                    <div className="row">
+                        <div className="col-sm-3">
+                            <label htmlFor="bank">{$t('client.accountwizard.bank')}</label>
+                        </div>
+                        <div className="col-sm-9">
+                            <Select
+                                id="bank"
+                                className="bankSelect"
+                                onChange={this.handleChangeBank}
+                                placeholder={$t('client.general.select')}
+                                clearValueText={$t('client.search.clear')}
+                                value={selectedBankDescr && selectedBankDescr.uuid}
+                                options={options}
+                            />
                         </div>
                     </div>
+                </div>
 
-                    {maybeCustomFields}
+                <div className="form-group">
+                    <div className="row">
+                        <div className="col-sm-6">
+                            <label htmlFor="id">{$t('client.settings.login')}</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="123456789"
+                                id="id"
+                                onChange={this.handleChangeLogin}
+                            />
+                        </div>
 
-                    <div className="btn-toolbar pull-right">
-                        <input
-                            type="reset"
-                            className="btn btn-default"
-                            value={$t('client.settings.reset')}
-                        />
-
-                        <input
-                            type="submit"
-                            className="btn btn-primary"
-                            value={$t('client.settings.submit')}
-                        />
+                        <div className="col-sm-6">
+                            <label htmlFor="password">{$t('client.settings.password')}</label>
+                            <PasswordInput onChange={this.handleChangePassword} id="password" />
+                        </div>
                     </div>
-                </form>
-            </FoldablePanel>
+                </div>
+
+                {maybeCustomFields}
+
+                <div className="form-group">
+                    {maybeCategories}
+                    {maybeAlerts}
+                </div>
+
+                <div className="btn-toolbar pull-right">
+                    <input
+                        type="submit"
+                        className="btn btn-primary"
+                        value={$t('client.settings.add_bank_button')}
+                        disabled={isDisabledSubmit}
+                    />
+                </div>
+            </form>
         );
     }
 }
 
-NewBankForm.propTypes /* remove-proptypes */ = {
-    // Whether the form is expanded or not.
-    expanded: PropTypes.bool.isRequired,
+InitForm.propTypes /* remove-proptypes */ = {
+    // Whether this form is displayed for onboarding or not (settings section)
+    isOnboarding: PropTypes.bool
+};
 
-    // An array of banks.
-    banks: PropTypes.array.isRequired,
-
-    // A function to create the access with the credentials.
-    createAccess: PropTypes.func.isRequired
+InitForm.defaultProps = {
+    isOnboarding: false
 };
 
 const Export = connect(
     state => {
         return {
-            banks: get.banks(state)
+            banks: get.banks(state),
+            emailEnabled: get.boolSetting(state, 'emails-enabled'),
+            emailRecipient: get.setting(state, 'email-recipient'),
+            categories: get.categories(state)
         };
     },
     dispatch => {
         return {
-            createAccess: (uuid, login, password, fields) => {
-                actions.createAccess(dispatch, uuid, login, password, fields);
-            }
+            createAccess: (uuid, login, password, fields, createDefaultAlerts) => {
+                actions.createAccess(dispatch, uuid, login, password, fields, createDefaultAlerts);
+            },
+            saveEmail: email => actions.setSetting(dispatch, 'email-recipient', email),
+            createCategories: categories => actions.createCategories(dispatch, categories)
         };
     }
-)(NewBankForm);
+)(InitForm);
 
 export default Export;
