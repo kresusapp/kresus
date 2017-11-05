d59d89583a5aca191938e2ffdf9a0223291e110d
Antoine BOSSY <mail+github@abossy.fr>
2017-11-05 15:38:54 +0100

    Migrate sync balance modal.

--- client/components/settings/bank-accesses/sync-account-balance-modal.js
+++ client/components/settings/bank-accesses/sync-account-balance-modal.js
@@ -3,71 +3,81 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import { translate as $t } from '../../../helpers';
-import { actions } from '../../../store';
+import { actions, get } from '../../../store';
+import { registerModal } from '../../ui/new-modal';
 
-import Modal from '../../ui/modal';
+import CancelAndWarning from '../../ui/modal-cancel-and-warning-button';
 
-let SyncAccountBalanceModal = props => {
-    let modalId = props.modalId;
+const MODAL_SLUG = 'sync-account-balance';
 
-    let modalTitle = $t('client.settings.resync_account.title', { title: props.account.title });
-
-    let modalBody = (
-        <div>
-            {$t('client.settings.resync_account.make_sure')}
-            <ul className="bullet">
-                <li>{$t('client.settings.resync_account.sync_operations')}</li>
-                <li>{$t('client.settings.resync_account.manage_duplicates')}</li>
-                <li>{$t('client.settings.resync_account.add_operation')}</li>
-                <li>{$t('client.settings.resync_account.delete_operation')}</li>
-            </ul>
-            {$t('client.settings.resync_account.are_you_sure')}
-        </div>
-    );
-
-    let modalFooter = (
-        <div>
-            <input
-                type="button"
-                className="btn btn-default"
-                data-dismiss="modal"
-                value={$t('client.general.cancel')}
-            />
-            <input
-                type="button"
-                className="btn btn-warning"
-                onClick={props.handleResyncBalance}
-                data-dismiss="modal"
-                value={$t('client.settings.resync_account.submit')}
-            />
-        </div>
-    );
-
-    return (
-        <Modal
-            key={modalId}
-            modalId={modalId}
-            modalBody={modalBody}
-            modalTitle={modalTitle}
-            modalFooter={modalFooter}
-        />
-    );
-};
-
-SyncAccountBalanceModal.propTypes = {
-    // Unique identifier of the modal
-    modalId: PropTypes.string.isRequired,
-
-    // The account to be resynced. (instanceof Account)
-    account: PropTypes.object.isRequired
-};
-
-const Export = connect(null, (dispatch, props) => {
+const Title = connect(state => {
+    let accountId = get.modal(state).state;
+    let { title } = get.accountById(state, accountId);
     return {
-        handleResyncBalance: () => {
-            actions.resyncBalance(dispatch, props.account.id);
+        title
+    };
+})(props => {
+    return <span>{$t('client.settings.resync_account.title', { title: props.title })}</span>;
+});
+
+const Body = () => (
+    <div>
+        {$t('client.settings.resync_account.make_sure')}
+        <ul className="bullet">
+            <li>{$t('client.settings.resync_account.sync_operations')}</li>
+            <li>{$t('client.settings.resync_account.manage_duplicates')}</li>
+            <li>{$t('client.settings.resync_account.add_operation')}</li>
+            <li>{$t('client.settings.resync_account.delete_operation')}</li>
+        </ul>
+        {$t('client.settings.resync_account.are_you_sure')}
+    </div>
+);
+
+const Footer = connect(
+    state => {
+        return {
+            accountId: get.modal(state).state
+        };
+    },
+    dispatch => ({ dispatch }),
+    ({ accountId }, { dispatch }) => {
+        return {
+            onClickWarning() {
+                actions.resyncBalance(dispatch, accountId);
+            },
+            warningLabel: $t('client.settings.resync_account.submit')
+        };
+    }
+)(CancelAndWarning);
+
+registerModal(MODAL_SLUG, () => {
+    return {
+        title: <Title />,
+        body: <Body />,
+        footer: <Footer />
+    };
+});
+
+const SyncAccountButton = connect(null, (dispatch, props) => {
+    return {
+        handleShowSyncModal() {
+            actions.showModal(dispatch, 'sync-account-balance', props.accountId);
         }
     };
-})(SyncAccountBalanceModal);
+})(props => {
+    return (
+        <span
+            className="pull-right fa fa-cog"
+            aria-label="Resync account balance"
+            onClick={props.handleShowSyncModal}
+            title={$t('client.settings.resync_account_button')}
+        />
+    );
+});
 
-export default Export;
+SyncAccountButton.propTypes = {
+    // The unique identifier of the account for which the balance has to be synced.
+    accountId: PropTypes.string.isRequired
+};
+
+export default SyncAccountButton;
