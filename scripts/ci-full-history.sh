60426310e4517622a530db3279d170e897d02413
Antoine BOSSY <mail+github@abossy.fr>
2017-11-05 17:10:02 +0100

    Add missing fetch in the CI script + Abort merge before rebase

--- scripts/ci-full-history.sh
+++ scripts/ci-full-history.sh
@@ -11,6 +11,7 @@ git remote |
     do
         if [ `git remote get-url $remote | grep -e $TARGET_REPO` ]
         then
+            echo "Remote '$remote' already exists"
             REMOTE_NAME=$remote
             break;
         fi
@@ -18,9 +19,13 @@ git remote |
 
     if [ "$REMOTE_NAME" == '' ]
     then
+        echo "Creating remote 'upstream-kresus'"
         git remote add upstream-kresus $TARGET_REPO -f
         REMOTE_NAME='upstream-kresus'
     fi
 
+    # Ensure the remote is up to date.
+    echo "Fetch '$REMOTE_NAME'"
+    git fetch $REMOTE_NAME
     git rebase $REMOTE_NAME/$TARGET_BRANCH -x "git log -1 --oneline && npm install && npm run check"
 }
