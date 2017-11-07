a4ab54ba093b60cd5dede91d482e9299e5d7c3d8
Antoine BOSSY <mail+github@abossy.fr>
2017-11-07 23:35:00 +0100

    Migrate create alert modal

--- client/components/settings/emails/alert-list.js
+++ client/components/settings/emails/alert-list.js
@@ -5,8 +5,8 @@ import PropTypes from 'prop-types';
 import { translate as $t } from '../../../helpers';
 import { get } from '../../../store';
 
-import AlertCreationModal from './alert-form-modal';
 import AlertItem from './alert-item';
+import ShowAlertCreationModal from './alert-form-modal';
 
 const Alerts = props => {
     let items = props.alerts.map(pair => (
@@ -24,23 +24,11 @@ const Alerts = props => {
                 <h3 className="title panel-title">{$t(props.panelTitleKey)}</h3>
 
                 <div className="panel-options">
-                    <span
-                        className="option-legend fa fa-plus-circle"
-                        aria-label="create alert"
-                        data-toggle="modal"
-                        data-target={`#alert-${props.alertType}-creation`}
-                    />
+                    <ShowAlertCreationModal alertType={props.alertType} />
                 </div>
             </div>
 
             <p className="panel-body alert-info">{$t(props.panelDescriptionKey)}</p>
-
-            <AlertCreationModal
-                modalId={`alert-${props.alertType}-creation`}
-                alertType={props.alertType}
-                titleTranslationKey={props.titleTranslationKey}
-                sendIfText={props.sendIfText}
-            />
 
             <div className="table-responsive">
                 <table className="table">
@@ -65,9 +53,6 @@ Alerts.propTypes = {
 
     // Description of the type of alert
     sendIfText: PropTypes.string.isRequired,
-
-    // The title translation key
-    titleTranslationKey: PropTypes.string.isRequired,
 
     // The panel title translation key
     panelTitleKey: PropTypes.string.isRequired,
