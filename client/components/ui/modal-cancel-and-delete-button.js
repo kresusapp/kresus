db9603b8cc0280e9b3cc56b198b575edcd58f922
Antoine BOSSY <mail+github@abossy.fr>
2017-11-07 09:02:51 +0100

    Migrate delete category modal

---
+++ client/components/ui/modal-cancel-and-delete-button.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { translate as $t } from '../../helpers';
+import { actions } from '../../store';
+
+const CancelAndDelete = connect(null, (dispatch, props) => {
+    // If the prop 'onClickCancel' is provided, it overrides the default.
+    return {
+        handleCancel() {
+            if (typeof props.onClickCancel === 'function') {
+                props.onClickCancel();
+            } else {
+                actions.hideModal(dispatch);
+            }
+        }
+    };
+})(props => {
+    return (
+        <React.Fragment>
+            <button type="button" className="btn btn-default" onClick={props.handleCancel}>
+                {$t('client.confirmdeletemodal.dont_delete')}
+            </button>
+            <button type="button" className="btn btn-danger" onClick={props.onClickDelete}>
+                {$t('client.confirmdeletemodal.confirm')}
+            </button>
+        </React.Fragment>
+    );
+});
+
+CancelAndDelete.propTypes = {
+    // A function to be called when clicking on warning button.
+    onClickDelete: PropTypes.func.isRequired
+};
+
+export default CancelAndDelete;
