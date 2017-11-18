c2109bea598472714c1ca9eddb93f389520507da
Antoine BOSSY <mail+github@abossy.fr>
2017-11-18 11:52:05 +0100

    Migrate confirme-delete-access modal to new-modal

---
+++ client/components/settings/bank-accesses/confirm-delete-access.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { translate as $t } from '../../../helpers';
+import { get, actions } from '../../../store';
+
+import { registerModal } from '../../ui/new-modal';
+import ModalContent from '../../ui/new-modal/content';
+import CancelAndDelete from '../../ui/new-modal/cancel-and-delete-buttons';
+
+const MODAL_SLUG = 'confirm-delete-access';
+
+const ConfirmDeleteModal = connect(
+    state => {
+        let accessId = get.modal(state).state;
+        let access = get.accessById(state, accessId);
+        let name = access ? access.name : null;
+        return {
+            name,
+            accessId
+        };
+    },
+    dispatch => {
+        return {
+            makeHandleDelete(accessId) {
+                actions.deleteAccess(dispatch, accessId);
+            }
+        };
+    },
+    ({ name, accessId }, { makeHandleDelete }) => {
+        return {
+            name,
+            handleDelete() {
+                makeHandleDelete(accessId);
+            }
+        };
+    }
+)(props => {
+    return (
+        <ModalContent
+            title={$t('client.confirmdeletemodal.title')}
+            body={$t('client.settings.erase_access', { name: props.name })}
+            footer={<CancelAndDelete onClickDelete={props.handleDelete} />}
+        />
+    );
+});
+
+registerModal(MODAL_SLUG, () => <ConfirmDeleteModal />);
+
+const DeleteAccessButton = connect(
+    null,
+    (dispatch, props) => {
+        return {
+            handleClick() {
+                actions.showModal(dispatch, MODAL_SLUG, props.accessId);
+            }
+        };
+    }
+)(props => {
+    return (
+        <button
+            className="option-legend fa fa-times-circle"
+            aria-label="remove access"
+            onClick={props.handleClick}
+            title={$t('client.settings.delete_access_button')}
+        />
+    );
+});
+
+DeleteAccessButton.propTypes = {
+    // The account's unique id
+    accessId: PropTypes.string.isRequired
+};
+
+export default DeleteAccessButton;
