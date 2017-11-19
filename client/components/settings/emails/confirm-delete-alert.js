efc995e967e6a43c19aec2fc2852ad2047762ef7
Antoine BOSSY <mail+github@abossy.fr>
2017-11-19 11:02:42 +0100

    Migrate confirm delete alert and reports to new-modal

---
+++ client/components/settings/emails/confirm-delete-alert.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { translate as $t } from '../../../helpers';
+import { get, actions } from '../../../store';
+
+import { registerModal } from '../../ui/new-modal';
+import ModalContent from '../../ui/new-modal/content';
+import CancelAndDelete from '../../ui/new-modal/cancel-and-delete-buttons';
+
+const MODAL_SLUG = 'confirm-delete-alert';
+
+const ConfirmDeleteModal = connect(
+    state => {
+        let modalState = get.modal(state).state;
+        return {
+            type: modalState.type,
+            alertId: modalState.alertId
+        };
+    },
+    dispatch => {
+        return {
+            makeHandleDelete(alertId) {
+                actions.deleteAlert(dispatch, alertId);
+            }
+        };
+    },
+    ({ alertId, type }, { makeHandleDelete }) => {
+        return {
+            type,
+            handleDelete() {
+                makeHandleDelete(alertId);
+            }
+        };
+    }
+)(props => {
+    return (
+        <ModalContent
+            title={$t('client.confirmdeletemodal.title')}
+            body={$t(`client.settings.emails.delete_${props.type}_full_text`)}
+            footer={<CancelAndDelete onClickDelete={props.handleDelete} />}
+        />
+    );
+});
+
+registerModal(MODAL_SLUG, () => <ConfirmDeleteModal />);
+
+const DeleteAlertButton = connect(
+    null,
+    (dispatch, props) => {
+        return {
+            handleClick() {
+                actions.showModal(dispatch, MODAL_SLUG, {
+                    alertId: props.alertId,
+                    type: props.type
+                });
+            }
+        };
+    }
+)(props => {
+    return (
+        <button
+            className="pull-right fa fa-times-circle"
+            aria-label="remove alert/report"
+            onClick={props.handleClick}
+            title={$t(`client.settings.emails.delete_${props.type}`)}
+        />
+    );
+});
+
+DeleteAlertButton.propTypes = {
+    // The account's unique id
+    alertId: PropTypes.string.isRequired,
+
+    // The type of alert
+    type: PropTypes.oneOf(['alert', 'report']).isRequired
+};
+
+export default DeleteAlertButton;
--- client/store/ui.js
+++ client/store/ui.js
@@ -227,6 +227,7 @@ const reducers = {
     IMPORT_INSTANCE: makeProcessingReasonReducer('client.spinner.import'),
     CREATE_ACCESS: makeProcessingReasonReducer('client.spinner.fetch_account'),
     CREATE_ALERT: reduceHideModalOnSuccess,
+    DELETE_ALERT: reduceHideModalOnSuccess,
     DELETE_ACCESS: makeProcessingReasonReducer('client.spinner.delete_account', true),
     DELETE_ACCOUNT: makeProcessingReasonReducer('client.spinner.delete_account', true),
     DELETE_CATEGORY: reduceHideModalOnSuccess,
