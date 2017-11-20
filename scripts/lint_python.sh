51bc24a49efa955509d7d07fd7c42fa6a9280e10
Phyks (Lucas Verney) <phyks@phyks.me>
2017-11-20 15:40:14 +0100

    Fix exit code

--- scripts/lint_python.sh
+++ scripts/lint_python.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 set -e
 
-find server -name "*.py" -exec echo '{}' \; -exec pylint --rcfile=.pylintrc '{}' \;
+find server -name "*.py" | while read f
+do
+    echo $f
+    pylint --rcfile=.pylintrc $f
+    if [ $? -ne 0 ]
+    then
+        break
+    fi
+done
--- server/weboob/main.py
+++ server/weboob/main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 """
 Weboob main Python wrapper
 
