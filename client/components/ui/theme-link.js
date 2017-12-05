eb10476e348bc7f26dbd0d36b07eb329f852394f
nicofrand <mail@nicofrand.eu>
2017-12-05 13:32:03 +0100

    Add timer fallback for theme loading in case onload event never fired

--- client/components/ui/theme-link.js
+++ client/components/ui/theme-link.js
@@ -11,9 +11,32 @@ class ThemeLink extends React.Component {
         this.element = null;
         this.onLoadHandler = this.onLoadHandler.bind(this);
         this.onErrorHandler = this.onErrorHandler.bind(this);
+        this.fallbackInterval = null;
+        this.fallbackTimeout = null;
+    }
+
+    isStyleSheetLoaded() {
+        return Array.from(document.styleSheets).some(sheet => {
+            return sheet.href.endsWith(`themes/${this.props.theme}/bundle.css`);
+        });
+    }
+
+    clearTimers() {
+        if (this.fallbackInterval) {
+            window.clearInterval(this.fallbackInterval);
+        }
+
+        if (this.fallbackTimeout) {
+            window.clearTimeout(this.fallbackTimeout);
+        }
+
+        this.fallbackInterval = null;
+        this.fallbackTimeout = null;
     }
 
     onLoadHandler() {
+        this.clearTimers();
+
         this.props.setThemeLoaded(this.props.theme, true);
         if (this.props.onLoad) {
             this.props.onLoad(true);
@@ -21,6 +44,8 @@ class ThemeLink extends React.Component {
     }
 
     onErrorHandler() {
+        this.clearTimers();
+
         this.props.setThemeLoaded(this.props.theme, false);
         if (this.props.onLoad) {
             this.props.onLoad(false);
@@ -31,12 +56,34 @@ class ThemeLink extends React.Component {
         const element = ReactDOM.findDOMNode(this.element);
         element.addEventListener('load', this.onLoadHandler);
         element.addEventListener('error', this.onErrorHandler);
+
+        this.clearTimers();
     }
 
     componentWillUnmount() {
         const element = ReactDOM.findDOMNode(this.element);
         element.removeEventListener('load', this.onLoadHandler);
         element.removeEventListener('error', this.onErrorHandler);
+
+        this.clearTimers();
+    }
+
+    componentWillUpdate() {
+        this.clearTimers();
+
+        this.fallbackInterval = window.setInterval(() => {
+            if (this.isStyleSheetLoaded()) {
+                this.onLoadHandler();
+            }
+        }, 500);
+
+        this.fallbackTimeout = window.setTimeout(() => {
+            if (this.isStyleSheetLoaded()) {
+                this.onLoadHandler();
+            } else {
+                this.onErrorHandler();
+            }
+        }, 30000);
     }
 
     render() {
