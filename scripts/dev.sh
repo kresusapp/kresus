6d0bad14beed2df9c9480a0715aa8bfdef8c9289
Benjamin Bouvier <public@benj.me>
2018-02-03 19:04:32 +0100

    [janitor] Try to improve webpack-dev-server support;

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -3,6 +3,6 @@
 npm run build:server
 
 concurrently -k \
-    "npm run webpack-dev-server" \
+    "npm run webpack-dev-server -- --host 0.0.0.0 --disable-host-check" \
     "./scripts/watch/server.sh" \
     "npm run nodemon -- --watch ./build/server --watch ./bin/kresus.js ./bin/kresus.js -- --config config.ini"
