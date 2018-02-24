9925b55f49dee5622b0e199496631c4041299dbe
nicofrand <mail@nicofrand.eu>
2018-02-24 15:55:12 +0100

    Redesign duplicates section

---
+++ client/components/duplicates/operation-line.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { translate as $t, formatDate } from '../../helpers';
+
+export default props => {
+    let customLabel = null;
+    if (props.customLabel) {
+        customLabel = <span>({props.customLabel})</span>;
+    }
+
+    return (
+        <div>
+            <div>
+                <h3>
+                    {props.title}&nbsp;{customLabel}
+                </h3>
+                <p>
+                    {formatDate.toShortString(props.date)}
+                    &nbsp; ({$t('client.similarity.imported_on')}{' '}
+                    {formatDate.toLongString(props.dateImport)})
+                </p>
+            </div>
+            <div className="duplicate-details">
+                <p>
+                    <span className="label">{$t('client.similarity.category')}</span>
+                    {props.categoryTitle}
+                </p>
+                <p>
+                    <span className="label">{$t('client.similarity.type')}</span>
+                    {$t(`client.${props.type}`)}
+                </p>
+            </div>
+        </div>
+    );
+};
