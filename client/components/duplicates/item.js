ac13330f41d4b942bdafba9ef1be17d11e682d13
nicofrand <mail@nicofrand.eu>
2018-02-25 09:37:15 +0100

    Allow to switch duplicates (fix #717)

--- client/components/duplicates/item.js
+++ client/components/duplicates/item.js
@@ -4,7 +4,126 @@ import { connect } from 'react-redux';
 import { get, actions } from '../../store';
 import { translate as $t, formatDate } from '../../helpers';
 
-export default connect(
+class DuplicateItem extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            switchOps: false
+        };
+    }
+
+    handleSwitch = () => {
+        this.setState({
+            switchOps: !this.state.switchOps
+        });
+    };
+
+    handleMerge = () => {
+        let toKeep, toRemove;
+        if (+this.props.a.dateImport < +this.props.b.dateImport) {
+            [toKeep, toRemove] = [this.props.a, this.props.b];
+        } else {
+            [toKeep, toRemove] = [this.props.b, this.props.a];
+        }
+
+        if (this.state.switchOps) {
+            [toKeep, toRemove] = [toRemove, toKeep];
+        }
+
+        this.props.merge(toKeep, toRemove);
+    };
+
+    render() {
+        let firstItem, secondItem, firstItemCat, secondItemCat;
+        if (+this.props.a.dateImport < +this.props.b.dateImport) {
+            [firstItem, secondItem] = [this.props.a, this.props.b];
+            [firstItemCat, secondItemCat] = [this.props.categoryA, this.props.categoryB];
+        } else {
+            [firstItem, secondItem] = [this.props.b, this.props.a];
+            [firstItemCat, secondItemCat] = [this.props.categoryB, this.props.categoryA];
+        }
+
+        if (this.state.switchOps) {
+            [firstItem, secondItem] = [secondItem, firstItem];
+            [firstItemCat, secondItemCat] = [secondItemCat, firstItemCat];
+        }
+
+        let firstItemCustomLabel = null;
+        if (firstItem.customLabel) {
+            firstItemCustomLabel = <span>({firstItem.customLabel})</span>;
+        }
+        let secondItemCustomLabel = null;
+        if (secondItem.customLabel) {
+            secondItemCustomLabel = <span>({secondItem.customLabel})</span>;
+        }
+
+        return (
+            <div key={`dpair-${firstItem.id}-${secondItem.id}`} className="duplicate">
+                <div>
+                    <div>
+                        <h3>
+                            {firstItem.title}&nbsp;{firstItemCustomLabel}
+                        </h3>
+                        <p>
+                            {formatDate.toShortString(firstItem.date)}
+                            &nbsp; ({$t('client.similarity.imported_on')}{' '}
+                            {formatDate.toLongString(firstItem.dateImport)})
+                        </p>
+                    </div>
+                    <div className="details">
+                        <p>
+                            <span className="label">{$t('client.similarity.category')}:</span>
+                            {firstItemCat.title}
+                        </p>
+                        <p>
+                            <span className="label">{$t('client.similarity.type')}:</span>
+                            {$t(`client.${firstItem.type}`)}
+                        </p>
+                    </div>
+                </div>
+                <button className="btn btn-default switch" onClick={this.handleSwitch}>
+                    <span className="fa fa-retweet" />
+                </button>
+                <div>
+                    <div>
+                        <h3>
+                            {secondItem.title}&nbsp;{secondItemCustomLabel}
+                        </h3>
+                        <p>
+                            {formatDate.toShortString(secondItem.date)}
+                            &nbsp; ({$t('client.similarity.imported_on')}{' '}
+                            {formatDate.toLongString(secondItem.dateImport)})
+                        </p>
+                    </div>
+                    <div className="details">
+                        <p>
+                            <span className="label">{$t('client.similarity.category')}:</span>
+                            {secondItemCat.title}
+                        </p>
+                        <p>
+                            <span className="label">{$t('client.similarity.type')}:</span>
+                            {$t(`client.${secondItem.type}`)}
+                        </p>
+                    </div>
+                </div>
+                <button className="btn btn-primary" onClick={this.handleMerge}>
+                    <span className="fa fa-compress" aria-hidden="true" />
+                    <span>
+                        {$t('client.similarity.amount')}: &nbsp;
+                        {this.props.formatCurrency(firstItem.amount)}
+                    </span>
+                    <span className="merge-title">
+                        &nbsp;/&nbsp;
+                        {$t('client.similarity.merge')}
+                    </span>
+                </button>
+            </div>
+        );
+    }
+}
+
+const Export = connect(
     (state, ownProps) => {
         let categoryA = get.categoryById(state, ownProps.a.categoryId);
         let categoryB = get.categoryById(state, ownProps.b.categoryId);
@@ -21,85 +140,6 @@ export default connect(
             }
         };
     }
-)(props => {
-    function handleMerge(e) {
-        let older, younger;
-        if (+props.a.dateImport < +props.b.dateImport) {
-            [older, younger] = [props.a, props.b];
-        } else {
-            [older, younger] = [props.b, props.a];
-        }
-        props.merge(younger, older);
-        e.preventDefault();
-    }
+)(DuplicateItem);
 
-    let customLabelA = null;
-    if (props.a.customLabel) {
-        customLabelA = <span>({props.a.customLabel})</span>;
-    }
-    let customLabelB = null;
-    if (props.b.customLabel) {
-        customLabelB = <span>({props.b.customLabel})</span>;
-    }
-
-    return (
-        <div key={`dpair-${props.a.id}-${props.b.id}`} className="duplicate">
-            <div>
-                <div>
-                    <h3>
-                        {props.a.title}&nbsp;{customLabelA}
-                    </h3>
-                    <p>
-                        {formatDate.toShortString(props.a.date)}
-                        &nbsp; ({$t('client.similarity.imported_on')}{' '}
-                        {formatDate.toLongString(props.a.dateImport)})
-                    </p>
-                </div>
-                <div className="details">
-                    <p>
-                        <span className="label">{$t('client.similarity.category')}:</span>
-                        {props.categoryA.title}
-                    </p>
-                    <p>
-                        <span className="label">{$t('client.similarity.type')}:</span>
-                        {$t(`client.${props.a.type}`)}
-                    </p>
-                </div>
-            </div>
-            <hr />
-            <div>
-                <div>
-                    <h3>
-                        {props.b.title}&nbsp;{customLabelB}
-                    </h3>
-                    <p>
-                        {formatDate.toShortString(props.b.date)}
-                        &nbsp; ({$t('client.similarity.imported_on')}{' '}
-                        {formatDate.toLongString(props.b.dateImport)})
-                    </p>
-                </div>
-                <div className="details">
-                    <p>
-                        <span className="label">{$t('client.similarity.category')}:</span>
-                        {props.categoryB.title}
-                    </p>
-                    <p>
-                        <span className="label">{$t('client.similarity.type')}:</span>
-                        {$t(`client.${props.b.type}`)}
-                    </p>
-                </div>
-            </div>
-            <button className="btn btn-primary" onClick={handleMerge}>
-                <span className="fa fa-compress" aria-hidden="true" />
-                <span>
-                    {$t('client.similarity.amount')}: &nbsp;
-                    {props.formatCurrency(props.a.amount)}
-                </span>
-                <span className="merge-title">
-                    &nbsp;/&nbsp;
-                    {$t('client.similarity.merge')}
-                </span>
-            </button>
-        </div>
-    );
-});
+export default Export;
