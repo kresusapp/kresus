b8b770753a8481a1102543451ed6b1c964b410d0
nicofrand <mail@nicofrand.eu>
2018-02-27 08:24:46 +0100

    Use CSS instead of bootstrap for operations table

--- client/components/operations/index.js
+++ client/components/operations/index.js
@@ -189,25 +189,15 @@ class OperationsComponent extends React.Component {
                     </div>
 
                     <div className="table-responsive">
-                        <table className="table table-hover table-bordered">
+                        <table className="operation-table table table-hover table-bordered">
                             <thead ref={refThead}>
                                 <tr>
-                                    <th className="hidden-xs" />
-                                    <th className="col-sm-1 col-xs-2">
-                                        {$t('client.operations.column_date')}
-                                    </th>
-                                    <th className="col-sm-2 hidden-xs">
-                                        {$t('client.operations.column_type')}
-                                    </th>
-                                    <th className="col-sm-6 col-xs-8">
-                                        {$t('client.operations.column_name')}
-                                    </th>
-                                    <th className="col-sm-1 col-xs-2">
-                                        {$t('client.operations.column_amount')}
-                                    </th>
-                                    <th className="col-sm-2 hidden-xs">
-                                        {$t('client.operations.column_category')}
-                                    </th>
+                                    <th />
+                                    <th>{$t('client.operations.column_date')}</th>
+                                    <th>{$t('client.operations.column_type')}</th>
+                                    <th>{$t('client.operations.column_name')}</th>
+                                    <th>{$t('client.operations.column_amount')}</th>
+                                    <th>{$t('client.operations.column_category')}</th>
                                 </tr>
                             </thead>
                             <InfiniteList
