134baae56e7acce856a5196cf161acf58525e14d
nicofrand <mail@nicofrand.eu>
2018-03-18 17:00:49 +0100

    Remove bootstrap for tabs

--- client/components/settings/index.js
+++ client/components/settings/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import { translate as $t } from '../../helpers';
@@ -11,54 +10,47 @@ import WeboobParameters from './weboob';
 import ThemesParameters from './themes';
 import LogsSection from './logs';
 
-import TabMenu from '../ui/tab-menu.js';
+import TabsContainer from '../ui/tabs.js';
 
 const SettingsComponents = props => {
     const pathPrefix = '/settings';
 
     let { currentAccountId } = props.match.params;
 
-    let menuItems = new Map();
-    menuItems.set(`${pathPrefix}/accounts/${currentAccountId}`, $t('client.settings.tab_accounts'));
-    menuItems.set(`${pathPrefix}/emails/${currentAccountId}`, $t('client.settings.tab_alerts'));
-    menuItems.set(`${pathPrefix}/backup/${currentAccountId}`, $t('client.settings.tab_backup'));
-    menuItems.set(`${pathPrefix}/weboob/${currentAccountId}`, $t('client.settings.tab_weboob'));
-    menuItems.set(`${pathPrefix}/themes/${currentAccountId}`, $t('client.settings.tab_themes'));
-    menuItems.set(`${pathPrefix}/logs/${currentAccountId}`, $t('client.settings.tab_logs'));
+    let tabs = new Map();
+    tabs.set(`${pathPrefix}/accounts/${currentAccountId}`, {
+        name: $t('client.settings.tab_accounts'),
+        component: BankAccountsList
+    });
+    tabs.set(`${pathPrefix}/emails/${currentAccountId}`, {
+        name: $t('client.settings.tab_alerts'),
+        component: EmailsParameters
+    });
+    tabs.set(`${pathPrefix}/backup/${currentAccountId}`, {
+        name: $t('client.settings.tab_backup'),
+        component: BackupParameters
+    });
+    tabs.set(`${pathPrefix}/weboob/${currentAccountId}`, {
+        name: $t('client.settings.tab_weboob'),
+        component: WeboobParameters
+    });
+    tabs.set(`${pathPrefix}/themes/${currentAccountId}`, {
+        name: $t('client.settings.tab_themes'),
+        component: ThemesParameters
+    });
+    tabs.set(`${pathPrefix}/logs/${currentAccountId}`, {
+        name: $t('client.settings.tab_logs'),
+        component: LogsSection
+    });
 
     return (
-        <div>
-            <TabMenu
-                selected={props.location.pathname}
-                tabs={menuItems}
-                history={props.history}
-                location={props.location}
-            />
-            <Switch>
-                <Route
-                    path={`${pathPrefix}/accounts/${currentAccountId}`}
-                    component={BankAccountsList}
-                />
-                <Route
-                    path={`${pathPrefix}/backup/${currentAccountId}`}
-                    component={BackupParameters}
-                />
-                <Route
-                    path={`${pathPrefix}/weboob/${currentAccountId}`}
-                    component={WeboobParameters}
-                />
-                <Route
-                    path={`${pathPrefix}/emails/${currentAccountId}`}
-                    component={EmailsParameters}
-                />
-                <Route
-                    path={`${pathPrefix}/themes/${currentAccountId}`}
-                    component={ThemesParameters}
-                />
-                <Route path={`${pathPrefix}/logs/${currentAccountId}`} component={LogsSection} />
-                <Redirect to={`${pathPrefix}/accounts/${currentAccountId}`} push={false} />
-            </Switch>
-        </div>
+        <TabsContainer
+            tabs={tabs}
+            defaultTab={`${pathPrefix}/accounts/${currentAccountId}`}
+            selectedTab={props.location.hostname}
+            history={props.history}
+            location={props.location}
+        />
     );
 };
 
