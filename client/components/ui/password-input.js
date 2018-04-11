7d322a76d6e3d1f7228240f7a668412aa48b8ac7
nicofrand <mail@nicofrand.eu>
2018-04-11 08:54:55 +0200

    Remove bootstrap for inputs with addons

--- client/components/ui/password-input.js
+++ client/components/ui/password-input.js
@@ -57,8 +57,10 @@ class PasswordInput extends React.Component {
             accessibleIconClass = $t('client.general.shown');
         }
 
+        let maybeClassName = this.props.className ? this.props.className : '';
+
         return (
-            <div className="input-group">
+            <div className={`input-with-addon ${maybeClassName}`}>
                 <input
                     type={type}
                     className="form-control"
@@ -69,16 +71,14 @@ class PasswordInput extends React.Component {
                     autoComplete="new-password"
                     defaultValue={this.props.defaultValue}
                 />
-                <span className="input-group-btn">
-                    <button
-                        type="button"
-                        className="btn btn-secondary"
-                        onClick={this.handleClick}
-                        title={title}>
-                        <span className="sr-only">{accessibleIconClass}</span>
-                        <i className={`fa fa-${iconClass}`} aria-hidden="true" />
-                    </button>
-                </span>
+                <button
+                    type="button"
+                    className="btn btn-secondary"
+                    onClick={this.handleClick}
+                    title={title}>
+                    <span className="sr-only">{accessibleIconClass}</span>
+                    <i className={`fa fa-${iconClass}`} aria-hidden="true" />
+                </button>
             </div>
         );
     }
@@ -94,8 +94,11 @@ PasswordInput.propTypes = {
     // A function called when the input changes.
     onChange: PropTypes.func,
 
-    // The defaultValu of the input.
-    defaultValue: PropTypes.string
+    // The defaultValue of the input.
+    defaultValue: PropTypes.string,
+
+    // Extra class names to pass to the input
+    className: PropTypes.string
 };
 
 export default PasswordInput;
