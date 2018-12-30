18b189a92fcbf6b4be3fcffa1fb3449159ec205e
Nicolas Frandeboeuf <mail@nicofrand.eu>
2018-12-30 20:33:13 +0100

    Move all webpack config to unique file

--- scripts/webpack/development.js
+++
@@ -1,8 +0,0 @@
-var webpack = require('webpack');
-const config = require('./base.js');
-
-config.mode = "development";
-
-config.devtool = 'cheap-module-eval-source-map';
-
-module.exports = config;
