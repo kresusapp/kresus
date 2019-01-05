796f5d98e0029c5646b2e87a5edfad2d66d30469
Nicolas Frandeboeuf <mail@nicofrand.eu>
2019-01-05 15:35:14 +0100

    Improve manual accounts matching

--- server/lib/diff-accounts.js
+++ server/lib/diff-accounts.js
@@ -1,5 +1,6 @@
 import { assert } from '../helpers';
 import makeDiff from './diff-list';
+import { SOURCE_NAME as MANUAL_BANK_NAME } from './sources/manual';
 
 function isPerfectMatch(known, provided) {
     assert(known.bank === provided.bank, 'data inconsistency');
@@ -29,7 +30,18 @@ function computePairScore(known, provided) {
     // Normalize data.
     let oldTitle = provided.title.replace(/ /g, '').toLowerCase();
     let newTitle = known.title.replace(/ /g, '').toLowerCase();
-    let titleScore = oldTitle === newTitle ? HEURISTICS.SAME_TITLE : 0;
+
+    let titleScore = 0;
+    // The manual bank accounts titles might change when the locale changes. Suppose the title
+    // is identical if the access is the same and rely on the account number.
+    if (
+        oldTitle === newTitle ||
+        (known.bank === provided.bank &&
+            known.bankAccess === provided.bankAccess &&
+            known.bank === MANUAL_BANK_NAME)
+    ) {
+        titleScore = HEURISTICS.SAME_TITLE;
+    }
 
     let accountNumberScore =
         known.accountNumber === provided.accountNumber ? HEURISTICS.SAME_ACCOUNT_NUMBER : 0;
