kresusapp/kresus

commit ee61bc3f6bcb14f7ce3414e5e29f38419b5981d6

Move Operation.prototype.mergeWith to an helper file and add a test;

server/models/operation.js
@@ -1,11 +1,12 @@
 import * as cozydb from 'cozydb';
 
 import { assert, makeLogger, promisify, promisifyModel, UNKNOWN_OPERATION_TYPE } from '../helpers';
+import { mergeWith } from './helpers';
 
 let log = makeLogger('models/operations');
 
 // Whenever you're adding something to the model, don't forget to modify
-// Operation.prototype.mergeWith.
+// the mergeWith function in the helpers file.
 
 let Operation = cozydb.getModel('bankoperation', {
     // ************************************************************************
@@ -194,40 +195,7 @@ Operation.allWithOperationTypesId = async function allWithOperationTypesId(userI
     return await request('allWithOperationTypesId');
 };
 
-const hasCategory = op => typeof op.categoryId === 'string';
-const hasType = op => typeof op.type !== 'undefined' && op.type !== UNKNOWN_OPERATION_TYPE;
-const hasCustomLabel = op => typeof op.customLabel === 'string';
-const hasBudgetDate = op => typeof op.budgetDate !== 'undefined' && op.budgetDate !== null;
-
-Operation.prototype.mergeWith = function(other) {
-    let update = {};
-
-    // Always trigger an update for the import date, to avoid duplicate
-    // transactions to appear in reports around the date where the duplicate
-    // has been imported.
-    // This should be always true, but we stay defensive here.
-    if (typeof other.dateImport !== 'undefined' && other.dateImport !== null) {
-        update.dateImport = other.dateImport;
-    }
-
-    if (!hasCategory(this) && hasCategory(other)) {
-        update.categoryId = other.categoryId;
-    }
-
-    if (!hasType(this) && hasType(other)) {
-        update.type = other.type;
-    }
-
-    if (!hasCustomLabel(this) && hasCustomLabel(other)) {
-        update.customLabel = other.customLabel;
-    }
-
-    if (!hasBudgetDate(this) && hasBudgetDate(other)) {
-        update.budgetDate = other.budgetDate;
-    }
-
-    return update;
-};
+Operation.prototype.mergeWith = mergeWith;
 
 // Checks the input object has the minimum set of attributes required for being an operation:
 // bankAccount

server/models/helpers.js
@@ -1,0 +1,36 @@
+import { UNKNOWN_OPERATION_TYPE } from '../helpers';
+
+const hasCategory = op => typeof op.categoryId === 'string';
+const hasType = op => typeof op.type !== 'undefined' && op.type !== UNKNOWN_OPERATION_TYPE;
+const hasCustomLabel = op => typeof op.customLabel === 'string';
+const hasBudgetDate = op => typeof op.budgetDate !== 'undefined' && op.budgetDate !== null;
+
+export function mergeWith(target, other) {
+    let update = {};
+
+    // Always trigger an update for the import date, to avoid duplicate
+    // transactions to appear in reports around the date where the duplicate
+    // has been imported.
+    // This should be always true, but we stay defensive here.
+    if (typeof other.dateImport !== 'undefined' && other.dateImport !== null) {
+        update.dateImport = other.dateImport;
+    }
+
+    if (!hasCategory(target) && hasCategory(other)) {
+        update.categoryId = other.categoryId;
+    }
+
+    if (!hasType(target) && hasType(other)) {
+        update.type = other.type;
+    }
+
+    if (!hasCustomLabel(target) && hasCustomLabel(other)) {
+        update.customLabel = other.customLabel;
+    }
+
+    if (!hasBudgetDate(target) && hasBudgetDate(other)) {
+        update.budgetDate = other.budgetDate;
+    }
+
+    return update;
+}

tests/server/merge-operations.js
@@ -1,0 +1,152 @@
+import should from 'should';
+
+import { mergeWith } from '../../server/models/helpers';
+import { UNKNOWN_OPERATION_TYPE } from '../../server/helpers';
+
+describe('Merging two transactions together', () => {
+    let target = {
+        accountId: '1234',
+        categoryId: '42',
+        type: '1',
+        title: 'A pony',
+        raw: 'A pony bought at Horse Exchange',
+        customLabel: 'My little pony',
+        date: Date.parse('2018-12-31'),
+        dateImport: Date.parse('2019-01-18'),
+        budgetDate: Date.parse('2019-01-01'),
+        amount: 1337.42,
+        createdByUser: false
+    };
+
+    let someDate = Date.parse('1998-07-14');
+
+    it("should replace the dateImport only when it's set", () => {
+        let update = mergeWith(target, {
+            dateImport: someDate
+        });
+        update.dateImport.should.equal(someDate);
+
+        update = mergeWith(target, {
+            dateImport: null
+        });
+        should.not.exist(update.dateImport);
+
+        update = mergeWith(target, {
+            customLabel: '13'
+        });
+        should.not.exist(update.dateImport);
+    });
+
+    it("should replace the categoryId only when it's not set in the target", () => {
+        let update = mergeWith(target, {
+            categoryId: 'abcd'
+        });
+        should.not.exist(update.categoryId);
+
+        update = mergeWith(target, {
+            categoryId: null
+        });
+        should.not.exist(update.categoryId);
+
+        let copy = Object.assign({}, target, { categoryId: null });
+
+        update = mergeWith(copy, {
+            categoryId: 'abcd'
+        });
+        update.categoryId.should.equal('abcd');
+
+        update = mergeWith(copy, {
+            categoryId: null
+        });
+        should.not.exist(update.categoryId);
+    });
+
+    it("should replace the type only when it's not set in the target", () => {
+        let update = mergeWith(target, {
+            type: '1'
+        });
+        should.not.exist(update.type);
+
+        update = mergeWith(target, {
+            type: UNKNOWN_OPERATION_TYPE
+        });
+        should.not.exist(update.type);
+
+        let copy = Object.assign({}, target, { type: UNKNOWN_OPERATION_TYPE });
+
+        update = mergeWith(copy, {
+            type: '1'
+        });
+        update.type.should.equal('1');
+
+        update = mergeWith(copy, {
+            type: UNKNOWN_OPERATION_TYPE
+        });
+        should.not.exist(update.type);
+    });
+
+    it("should replace the custom label only when it's not set in the target", () => {
+        let update = mergeWith(target, {
+            customLabel: 'horsejs'
+        });
+        should.not.exist(update.customLabel);
+
+        update = mergeWith(target, {
+            customLabel: null
+        });
+        should.not.exist(update.customLabel);
+
+        let copy = Object.assign({}, target, { customLabel: null });
+
+        update = mergeWith(copy, {
+            customLabel: 'horsejs'
+        });
+        update.customLabel.should.equal('horsejs');
+
+        update = mergeWith(copy, {
+            customLabel: null
+        });
+        should.not.exist(update.customLabel);
+    });
+
+    it("should replace the budget date only when it's not set in the target", () => {
+        let update = mergeWith(target, {
+            budgetDate: someDate
+        });
+        should.not.exist(update.budgetDate);
+
+        update = mergeWith(target, {
+            budgetDate: null
+        });
+        should.not.exist(update.budgetDate);
+
+        let copy = Object.assign({}, target, { budgetDate: null });
+
+        update = mergeWith(copy, {
+            budgetDate: someDate
+        });
+        update.budgetDate.should.equal(someDate);
+
+        update = mergeWith(copy, {
+            budgetDate: null
+        });
+        should.not.exist(update.budgetDate);
+    });
+
+    it('should merge several fields at once', () => {
+        let copy = Object.assign({}, target, {
+            categoryId: null,
+            customLabel: null
+        });
+
+        let update = mergeWith(copy, {
+            dateImport: someDate,
+            categoryId: 'trololo'
+        });
+
+        update.should.deepEqual({
+            dateImport: someDate,
+            categoryId: 'trololo'
+        });
+    });
+});

commit 711c9c8d62a9b059409c475b3f39ddb46a3b31c0

Fix regression introduced by ee61bc3f making it impossible to merge 2 operations.

server/models/operation.js
@@ -195,7 +195,9 @@ Operation.allWithOperationTypesId = async function allWithOperationTypesId(userI
     return await request('allWithOperationTypesId');
 };
 
-Operation.prototype.mergeWith = mergeWith;
+Operation.prototype.mergeWith = function(other) {
+    return mergeWith(this, other);
+};
 
 // Checks the input object has the minimum set of attributes required for being an operation:
 // bankAccount