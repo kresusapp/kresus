50c089cda457bb06850b14dceef16de9cc62aa03
Antoine BOSSY <mail+github@abossy.fr>
2019-05-26 16:37:33 +0200

    Use DisplayIf for emails form.

--- client/components/settings/emails/index.js
+++ client/components/settings/emails/index.js
@@ -7,38 +7,34 @@ import { translate as $t } from '../../../helpers';
 import Alerts from './alert-list';
 import EmailConfig from './config';
 import Reports from './report-list';
+import DisplayIf from '../../ui/display-if';
 
 function EmailsParameters(props) {
-    let maybeEditors = null;
-    if (props.enableEditors) {
-        maybeEditors = (
-            <div>
-                <Alerts
-                    alertType="balance"
-                    sendIfText={$t('client.settings.emails.send_if_balance_is')}
-                    titleTranslationKey="client.settings.emails.add_balance"
-                    panelTitleKey="client.settings.emails.balance_title"
-                    panelDescriptionKey="client.settings.emails.balance_desc"
-                />
-
-                <Alerts
-                    alertType="transaction"
-                    sendIfText={$t('client.settings.emails.send_if_transaction_is')}
-                    titleTranslationKey="client.settings.emails.add_transaction"
-                    panelTitleKey="client.settings.emails.transaction_title"
-                    panelDescriptionKey="client.settings.emails.transaction_desc"
-                />
-
-                <Reports />
-            </div>
-        );
-    }
-
     return (
         <div className="emails settings-container">
             <EmailConfig />
-            {maybeEditors ? <hr /> : null}
-            {maybeEditors}
+            <DisplayIf condition={props.enableEditors}>
+                <hr />
+                <div>
+                    <Alerts
+                        alertType="balance"
+                        sendIfText={$t('client.settings.emails.send_if_balance_is')}
+                        titleTranslationKey="client.settings.emails.add_balance"
+                        panelTitleKey="client.settings.emails.balance_title"
+                        panelDescriptionKey="client.settings.emails.balance_desc"
+                    />
+
+                    <Alerts
+                        alertType="transaction"
+                        sendIfText={$t('client.settings.emails.send_if_transaction_is')}
+                        titleTranslationKey="client.settings.emails.add_transaction"
+                        panelTitleKey="client.settings.emails.transaction_title"
+                        panelDescriptionKey="client.settings.emails.transaction_desc"
+                    />
+
+                    <Reports />
+                </div>
+            </DisplayIf>
         </div>
     );
 }
