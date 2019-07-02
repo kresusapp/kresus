1a3774e5aaf9a87f97ae1240f28b4875af7cffb6
Benjamin Bouvier <public@benj.me>
2019-07-02 22:51:37 +0200

    Tests: allow running only a subset of tests;
    For example, yarn check:test ./tests/database

--- scripts/check/test.sh
+++ scripts/check/test.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 set -e
 
+TARGET=""
+
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    TARGET="$TARGET $key"
+    shift # past argument or value
+done
+
+if [ "$TARGET" == "" ]
+then
+    TARGET="./tests"
+fi
+
 # Run proper tests
 BABEL_ENV=tests NODE_ENV=test mocha \
     --require @babel/polyfill \
     --require @babel/register \
     --file ./tests/database/config.js \
-    --recursive ./tests \
+    --recursive $TARGET \
     --timeout 120000
