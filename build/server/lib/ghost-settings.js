4d2be4a1abfd6d01f6f87eb3b48f1ca0ad44fbd5
Benjamin Bouvier <public@benj.me>
2019-11-10 12:44:23 +0100

    Build;

--- build/server/lib/ghost-settings.js
+++ build/server/lib/ghost-settings.js
@@ -8,5 +8,7 @@ exports.ConfigGhostSettings = void 0;
 // saved into the database.
 // *Never* ever remove a name from this list, since these are used also to
 // know which settings shouldn't be imported or exported.
-const ConfigGhostSettings = new Set(['weboob-version', 'weboob-installed', 'standalone-mode', 'url-prefix', 'emails-enabled', 'can-encrypt', 'force-demo-mode', 'theme']);
+const ConfigGhostSettings = new Set(['weboob-version', 'weboob-installed', 'standalone-mode', 'url-prefix', 'emails-enabled', 'can-encrypt', 'force-demo-mode' // 'theme' // It should be a ghost setting! But our migration order doesn't
+// allow for this.
+]);
 exports.ConfigGhostSettings = ConfigGhostSettings;
