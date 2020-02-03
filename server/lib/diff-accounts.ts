2ead960539cc30e6dfa7169a4a4828e9cdd5677e
Antoine BOSSY <mail+github@abossy.fr>
2020-02-04 00:14:16 +0100

    [server] Migrate ./server/lib/diff-accounts.ts to TypeScript.

--- server/lib/diff-accounts.ts
+++ server/lib/diff-accounts.ts
@@ -1,11 +1,12 @@
 import { assert } from '../helpers';
 import makeDiff from './diff-list';
 import { SOURCE_NAME as MANUAL_BANK_NAME } from './sources/manual';
+import { Accounts } from '../models';
 
-function isPerfectMatch(known, provided) {
+function isPerfectMatch(known: Accounts, provided: Accounts): boolean {
     assert(known.vendorId === provided.vendorId, 'data inconsistency');
-    let newLabel = known.label.replace(/ /g, '').toLowerCase();
-    let oldLabel = provided.label.replace(/ /g, '').toLowerCase();
+    const newLabel = known.label.replace(/ /g, '').toLowerCase();
+    const oldLabel = provided.label.replace(/ /g, '').toLowerCase();
     return (
         oldLabel === newLabel &&
         provided.vendorAccountId === known.vendorAccountId &&
@@ -26,10 +27,10 @@ const HEURISTICS = {
 // The minimum similarity to consider two accounts are the same.
 const MIN_SIMILARITY = HEURISTICS.SAME_IBAN + HEURISTICS.SAME_CURRENCY + HEURISTICS.SAME_TYPE + 1;
 
-function computePairScore(known, provided) {
+function computePairScore(known: Accounts, provided: Accounts): number {
     // Normalize data.
-    let oldLabel = provided.label.replace(/ /g, '').toLowerCase();
-    let newLabel = known.label.replace(/ /g, '').toLowerCase();
+    const oldLabel = provided.label.replace(/ /g, '').toLowerCase();
+    const newLabel = known.label.replace(/ /g, '').toLowerCase();
 
     // The manual bank accounts labels might change when the locale changes. Suppose the label
     // is identical if the access is the same and rely on the account number.
@@ -43,13 +44,13 @@ function computePairScore(known, provided) {
         labelScore = HEURISTICS.SAME_LABEL;
     }
 
-    let accountIdScore =
+    const accountIdScore =
         known.vendorAccountId === provided.vendorAccountId ? HEURISTICS.SAME_ACCOUNT_NUMBER : 0;
-    let ibanScore = known.iban === provided.iban ? HEURISTICS.SAME_IBAN : 0;
-    let currencyScore = known.currency === provided.currency ? HEURISTICS.SAME_CURRENCY : 0;
-    let typeScore = known.type === provided.type ? HEURISTICS.SAME_TYPE : 0;
+    const ibanScore = known.iban === provided.iban ? HEURISTICS.SAME_IBAN : 0;
+    const currencyScore = known.currency === provided.currency ? HEURISTICS.SAME_CURRENCY : 0;
+    const typeScore = known.type === provided.type ? HEURISTICS.SAME_TYPE : 0;
     return labelScore + accountIdScore + ibanScore + currencyScore + typeScore;
 }
 
-const diffAccount = makeDiff(isPerfectMatch, computePairScore, MIN_SIMILARITY);
+const diffAccount = makeDiff<Accounts>(isPerfectMatch, computePairScore, MIN_SIMILARITY);
 export default diffAccount;
