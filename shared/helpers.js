8263f134eae265400619c833ac0f2a8f480a5cc1
nicofrand <mail@nicofrand.eu>
2020-02-16 12:09:38 +0100

    Update weboob minimal version to 2.0

--- shared/helpers.js
+++ shared/helpers.js
@@ -159,7 +159,7 @@ export const currency = {
 export const UNKNOWN_OPERATION_TYPE = 'type.unknown';
 export const UNKNOWN_ACCOUNT_TYPE = 'account-type.unknown';
 
-export const MIN_WEBOOB_VERSION = '1.5';
+export const MIN_WEBOOB_VERSION = '2.0';
 export const UNKNOWN_WEBOOB_VERSION = null;
 
 // At least 8 chars, including one lowercase, one uppercase and one digit.
