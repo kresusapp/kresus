3bd634b89a580f63a9b465df6c121d99ecb53311
nicofrand <mail@nicofrand.eu>
2020-02-18 22:12:12 +0100

    TypeScript: describe global interfaces & extend express Request for preload

--- global.d.ts
+++ global.d.ts
@@ -1,6 +1,26 @@
 /* eslint-disable import/unambiguous */
 declare namespace NodeJS {
     interface Process {
-        kresus: any;
+        kresus: {
+            user: {
+                id?: number;
+                login: string;
+            };
+
+            providedUserId?: number;
+
+            // Allow any other property
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            [x: string]: any;
+        };
+    }
+}
+
+// Extend express Request interface to allow a "user" variable for preloads.
+declare namespace Express {
+    export interface Request {
+        user?: {
+            id?: number;
+        };
     }
 }
