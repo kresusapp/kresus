d2e04448b3e6b8f136f63a39c4bdf6581ad76310
Benjamin Bouvier <public@benj.me>
2020-04-07 19:36:38 +0200

    ts: port server/cli.js;

--- server/cli.ts
+++ server/cli.ts
@@ -2,15 +2,15 @@ import { makeLogger } from './helpers';
 
 import { setupOrm, User } from './models';
 
-let log = makeLogger('cli');
+const log = makeLogger('cli');
 
-export async function createUser(login) {
+export async function createUser(login: string) {
     try {
         log.info(`Creating user with login ${login}: setting up database.`);
         await setupOrm();
         log.info('Database set up; creating user...');
-        let user = await User.create({ login });
-        let id = user.id;
+        const user = await User.create({ login });
+        const id = user.id;
         log.info(`User ${login} created with success! id=${id}`);
     } catch (err) {
         log.error(`Couldn't create user ${login}: ${err.message}
