714a2d6678307c14c1c4b20849d047bc89872c30
Benjamin Bouvier <public@benj.me>
2020-09-05 12:30:40 +0200

    Use a Popconfirm for the delete unused categories confirmation;

--- client/components/categories/index.js
+++ client/components/categories/index.js
@@ -3,131 +3,13 @@ import { connect } from 'react-redux';
 
 import { get, actions } from '../../store';
 
-import { translate as $t } from '../../helpers';
+import { translate as $t, NONE_CATEGORY_ID } from '../../helpers';
 
 import CategoryListItem from './item';
-import { MODAL_SLUG as DELETE_UNUSED_MODAL_SLUG } from './delete-unused-modal';
+import { Popconfirm } from '../ui';
 import DisplayIf from '../ui/display-if';
 
-class CategoryList extends React.Component {
-    state = {
-        showForm: false,
-    };
-
-    refNewCategory = React.createRef();
-
-    handleAddDefault = () => {
-        this.props.createDefaultCategories();
-    };
-
-    handleShowForm = e => {
-        e.preventDefault();
-
-        this.setState(
-            {
-                showForm: !this.state.showForm,
-            },
-            () => {
-                // then
-                if (this.state.showForm) {
-                    this.refNewCategory.current.selectLabel();
-                }
-            }
-        );
-    };
-
-    render = () => {
-        let items = this.props.categories.map(cat => (
-            <CategoryListItem
-                cat={cat}
-                categories={this.props.categories}
-                updateCategory={this.props.updateCategory}
-                deleteCategory={this.props.deleteCategory}
-                key={cat.id}
-            />
-        ));
-
-        let addButtonType = 'plus';
-        let addButtonAria = 'add';
-        let addButtonLabel = 'client.category.add';
-        if (this.state.showForm) {
-            addButtonType = 'minus';
-            addButtonAria = 'cancel';
-            addButtonLabel = 'client.general.cancel';
-        }
-
-        let numUnusedCategories = this.props.unusedCategories.length;
-        let deleteUnusedButtonLabel;
-        if (numUnusedCategories === 0) {
-            deleteUnusedButtonLabel = $t('client.category.no_unused_categories');
-        } else {
-            deleteUnusedButtonLabel = $t('client.category.delete_unused', {
-                // eslint-disable-next-line camelcase
-                smart_count: numUnusedCategories,
-            });
-        }
-
-        return (
-            <div className="categories">
-                <p className="actions">
-                    <button
-                        className="btn"
-                        aria-label={addButtonAria}
-                        onClick={this.handleShowForm}>
-                        <span className={`fa fa-${addButtonType}-circle`} />
-                        <span>{$t(addButtonLabel)}</span>
-                    </button>
-
-                    <button
-                        className="btn"
-                        aria-label="add default"
-                        onClick={this.handleAddDefault}>
-                        <span className={'fa fa-plus-circle'} />
-                        <span>{$t('client.category.add_default')}</span>
-                    </button>
-
-                    <button
-                        className="btn danger"
-                        aria-label="delete unused"
-                        onClick={this.props.handleShowDeleteUnusedModal}
-                        disabled={numUnusedCategories === 0}>
-                        <span className={'fa fa-trash'} />
-                        <span>{deleteUnusedButtonLabel}</span>
-                    </button>
-                </p>
-
-                <table className="striped">
-                    <thead>
-                        <tr>
-                            <th className="category-color">
-                                {$t('client.category.column_category_color')}
-                            </th>
-                            <th>{$t('client.category.column_category_name')}</th>
-
-                            <th className="category-action">
-                                {$t('client.category.column_action')}
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <DisplayIf condition={this.state.showForm}>
-                            <CategoryListItem
-                                cat={{}}
-                                categories={this.props.categories}
-                                createCategory={this.props.createCategory}
-                                onCancelCreation={this.handleShowForm}
-                                ref={this.refNewCategory}
-                            />
-                        </DisplayIf>
-                        {items}
-                    </tbody>
-                </table>
-            </div>
-        );
-    };
-}
-
-const Export = connect(
+export default connect(
     state => {
         return {
             categories: get.categoriesButNone(state),
@@ -147,22 +29,145 @@ const Export = connect(
                 actions.updateCategory(dispatch, former, newer);
             },
 
-            showDeleteUnusedModal(unusedCategories) {
-                actions.showModal(dispatch, DELETE_UNUSED_MODAL_SLUG, unusedCategories);
-            },
-        };
-    },
-
-    (state, dispatch) => {
-        let { showDeleteUnusedModal, ...otherDispatch } = dispatch;
-        return {
-            ...state,
-            ...otherDispatch,
-            handleShowDeleteUnusedModal() {
-                showDeleteUnusedModal(state.unusedCategories);
+            deleteCategory(id) {
+                actions.deleteCategory(dispatch, id, NONE_CATEGORY_ID);
             },
         };
     }
-)(CategoryList);
+)(
+    class CategoryList extends React.Component {
+        state = {
+            showForm: false,
+        };
 
-export default Export;
+        refNewCategory = React.createRef();
+
+        handleAddDefault = () => {
+            this.props.createDefaultCategories();
+        };
+
+        handleShowForm = e => {
+            e.preventDefault();
+
+            this.setState(
+                {
+                    showForm: !this.state.showForm,
+                },
+                () => {
+                    // then
+                    if (this.state.showForm) {
+                        this.refNewCategory.current.selectLabel();
+                    }
+                }
+            );
+        };
+
+        deleteUnusedCategories = () => {
+            for (let { id } of this.props.unusedCategories) {
+                this.props.deleteCategory(id);
+            }
+        };
+
+        render = () => {
+            let items = this.props.categories.map(cat => (
+                <CategoryListItem
+                    cat={cat}
+                    categories={this.props.categories}
+                    updateCategory={this.props.updateCategory}
+                    deleteCategory={this.props.deleteCategory}
+                    key={cat.id}
+                />
+            ));
+
+            let addButtonType = 'plus';
+            let addButtonAria = 'add';
+            let addButtonLabel = 'client.category.add';
+            if (this.state.showForm) {
+                addButtonType = 'minus';
+                addButtonAria = 'cancel';
+                addButtonLabel = 'client.general.cancel';
+            }
+
+            let numUnusedCategories = this.props.unusedCategories.length;
+            let deleteUnusedButtonLabel;
+            if (numUnusedCategories === 0) {
+                deleteUnusedButtonLabel = $t('client.category.no_unused_categories');
+            } else {
+                deleteUnusedButtonLabel = $t('client.category.delete_unused', {
+                    // eslint-disable-next-line camelcase
+                    smart_count: numUnusedCategories,
+                });
+            }
+
+            return (
+                <div className="categories">
+                    <p className="actions">
+                        <button
+                            className="btn"
+                            aria-label={addButtonAria}
+                            onClick={this.handleShowForm}>
+                            <span className={`fa fa-${addButtonType}-circle`} />
+                            <span>{$t(addButtonLabel)}</span>
+                        </button>
+
+                        <button
+                            className="btn"
+                            aria-label="add default"
+                            onClick={this.handleAddDefault}>
+                            <span className={'fa fa-plus-circle'} />
+                            <span>{$t('client.category.add_default')}</span>
+                        </button>
+
+                        <Popconfirm
+                            onConfirm={this.deleteUnusedCategories}
+                            trigger={
+                                <button
+                                    className="btn danger"
+                                    aria-label="delete unused"
+                                    disabled={numUnusedCategories === 0}>
+                                    <span className={'fa fa-trash'} />
+                                    <span>{deleteUnusedButtonLabel}</span>
+                                </button>
+                            }>
+                            <h3>{$t('client.deleteunusedcategoriesmodal.title')}</h3>
+                            <p>{$t('client.deleteunusedcategoriesmodal.explanation')}</p>
+                            <ul>
+                                {this.props.unusedCategories.map(c => (
+                                    <li key={c.id}>{c.label}</li>
+                                ))}
+                            </ul>
+                            <p>{$t('client.deleteunusedcategoriesmodal.question')}</p>
+                        </Popconfirm>
+                    </p>
+
+                    <table className="striped">
+                        <thead>
+                            <tr>
+                                <th className="category-color">
+                                    {$t('client.category.column_category_color')}
+                                </th>
+                                <th>{$t('client.category.column_category_name')}</th>
+
+                                <th className="category-action">
+                                    {$t('client.category.column_action')}
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <DisplayIf condition={this.state.showForm}>
+                                <CategoryListItem
+                                    cat={{}}
+                                    categories={this.props.categories}
+                                    createCategory={this.props.createCategory}
+                                    onCancelCreation={this.handleShowForm}
+                                    ref={this.refNewCategory}
+                                />
+                            </DisplayIf>
+                            {items}
+                        </tbody>
+                    </table>
+                </div>
+            );
+        };
+    }
+);
