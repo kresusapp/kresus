9ab259fc47651de11d99bb3940112230037a09bd
Antoine BOSSY <mail+github@abossy.fr>
2020-11-10 20:28:08 +0100

    [client] Define the type for require.ensure. Fixes the following tsc error:
    client/init.js:8:13 - error TS2339: Property 'ensure' does not exist on type 'NodeRequire'.
    
    8     require.ensure(['whatwg-fetch'], require => {
    
    The `require` we use here is the require provided by webpack. This type definition allows not to install a type definition.

--- client/global.d.ts
+++ client/global.d.ts
@@ -1,4 +1,5 @@
 /* eslint-disable import/unambiguous */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 
 interface Window {
     __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: (...arg: any[]) => any;
@@ -8,4 +9,18 @@ interface Window {
 declare module 'raw-loader!*' {
     const content: any;
     export default content;
+}
+
+// Define the type for require.ensure
+declare namespace NodeJS {
+    interface Require {
+        // See here for the type definition:
+        // https://webpack.js.org/api/module-methods/#requireensure
+        ensure: (
+            dependencies: string[],
+            callback: (require: Require) => void,
+            errorCallback?: (error: Error) => void,
+            chunkName?: string
+        ) => void;
+    }
 }
