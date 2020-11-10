ae92ccb81e2ea881835d6c53ba4bc16949c32252
Antoine BOSSY <mail+github@abossy.fr>
2020-11-10 19:49:35 +0100

    [client] Define type declaration for raw-loaded modules (like LICENSE). Fixes the following tsc error :
    client/components/about/index.js:10:21 - error TS2307: Cannot find module 'raw-loader!../../../LICENSE' or its corresponding type declarations.
    10 import LICENSE from 'raw-loader!../../../LICENSE';

--- client/global.d.ts
+++ client/global.d.ts
@@ -3,3 +3,9 @@
 interface Window {
     __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: (...arg: any[]) => any;
 }
+
+// Define the type for raw-loaded modules (like LICENSE).
+declare module 'raw-loader!*' {
+    const content: any;
+    export default content;
+}
