67160be0a5d6c836ba31477c16c9f35ce9e3237e
Antoine <mail+github@abossy.fr>
2020-12-07 21:10:33 +0100

    [client] Allow to close a popover with Escape key.

--- client/components/ui/popover.tsx
+++ client/components/ui/popover.tsx
@@ -1,11 +1,21 @@
-import React, { useState, useImperativeHandle, ReactNode, ReactElement, useCallback } from 'react';
-import Tippy from '@tippyjs/react/headless';
+import React, {
+    useState,
+    useImperativeHandle,
+    ReactNode,
+    ReactElement,
+    useCallback,
+    useEffect,
+} from 'react';
+import Tippy, { TippyProps } from '@tippyjs/react/headless';
 
 import './popover.css';
 
 function appendToBody() {
     return document.body;
 }
+
+// Retrieve the type of the first parameter of TippyProps.render.
+type Attrs = Parameters<NonNullable<TippyProps['render']>>[0];
 
 interface PopoverProps {
     // DOM node that's used to open the popover.
@@ -34,6 +44,17 @@ const Popover = React.forwardRef<ExposedMethods, PopoverProps>((props, ref) => {
         },
     });
 
+    const onKeyDown = useCallback(
+        (event: KeyboardEvent) => {
+            if (event.key === 'Escape') {
+                close();
+                // Remove the listener when hiding the popover.
+                document.removeEventListener('keydown', onKeyDown);
+            }
+        },
+        [close]
+    );
+
     // Expose the close() function through a reference.
     useImperativeHandle(ref, () => ({
         close,
@@ -41,7 +62,7 @@ const Popover = React.forwardRef<ExposedMethods, PopoverProps>((props, ref) => {
 
     const { content, small } = props;
     const render = useCallback(
-        attrs => {
+        (attrs: Attrs) => {
             const smallClass = small ? 'small' : '';
 
             return (
@@ -55,6 +76,14 @@ const Popover = React.forwardRef<ExposedMethods, PopoverProps>((props, ref) => {
         },
         [content, small]
     );
+
+    useEffect(() => {
+        // Add the listener when the popover is displayed.
+        if (isOpen) {
+            document.addEventListener('keydown', onKeyDown);
+        }
+        return () => document.removeEventListener('keydown', onKeyDown);
+    }, [onKeyDown, isOpen]);
 
     return (
         <Tippy
