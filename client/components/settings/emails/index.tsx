279b074fa118ba64b990979e1c5ea5f374dfd38a
nicofrand <mail@nicofrand.eu>
2021-02-16 20:18:36 +0100

    Migrate alerts/reports list to TypeScript

--- client/components/settings/emails/index.tsx
+++ client/components/settings/emails/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
 import { get } from '../../../store';
-import { translate as $t } from '../../../helpers';
+import { translate as $t, useKresusState } from '../../../helpers';
 import { EMAILS_ENABLED, NOTIFICATIONS_ENABLED } from '../../../../shared/instance';
 import { APPRISE_URL, EMAIL_RECIPIENT } from '../../../../shared/settings';
 
@@ -14,16 +13,37 @@ import DisplayIf from '../../ui/display-if';
 
 import './alerts.css';
 
-function EmailsParameters(props) {
+const AlertsAndReports = () => {
+    const areEmailsEnabled = useKresusState(state => {
+        return (
+            get.boolInstanceProperty(state, EMAILS_ENABLED) &&
+            get.setting(state, EMAIL_RECIPIENT).length > 0
+        );
+    });
+
+    const areNotificationsEnabled = useKresusState(state => {
+        return (
+            get.boolInstanceProperty(state, NOTIFICATIONS_ENABLED) &&
+            get.setting(state, APPRISE_URL).length > 0
+        );
+    });
+
+    // Only enable the alerts editors if emails are enabled and a recipient email
+    // address has been set or if notifications are enabled.
+    const enableAlerts = areEmailsEnabled || areNotificationsEnabled;
+
+    // Only enable the reports if emails are enabled as notifications do not support them yet.
+    const enableReports = areEmailsEnabled;
+
     return (
         <div className="emails settings-container">
             <EmailConfig />
             <hr />
             <NotificationsConfig />
-            <DisplayIf condition={props.enableAlerts || props.enableReports}>
+            <DisplayIf condition={enableAlerts || enableReports}>
                 <hr />
             </DisplayIf>
-            <DisplayIf condition={props.enableAlerts}>
+            <DisplayIf condition={enableAlerts}>
                 <Alerts
                     alertType="balance"
                     sendIfText={$t('client.settings.emails.send_if_balance_is')}
@@ -40,27 +60,13 @@ function EmailsParameters(props) {
                     panelDescriptionKey="client.settings.emails.transaction_desc"
                 />
             </DisplayIf>
-            <DisplayIf condition={props.enableReports}>
+            <DisplayIf condition={enableReports}>
                 <Reports />
             </DisplayIf>
         </div>
     );
-}
+};
 
-export default connect(state => {
-    const emailsEnabled =
-        get.boolInstanceProperty(state, EMAILS_ENABLED) &&
-        get.setting(state, EMAIL_RECIPIENT).length > 0;
-    const notificationsEnabled =
-        get.boolInstanceProperty(state, NOTIFICATIONS_ENABLED) &&
-        get.setting(state, APPRISE_URL).length > 0;
+AlertsAndReports.displayName = 'AlertsAndReports';
 
-    return {
-        // Only enable the alerts editors if emails are enabled and a recipient email
-        // address has been set or if notifications are enabled.
-        enableAlerts: emailsEnabled || notificationsEnabled,
-
-        // Only enable the reports if emails are enabled as notifications do not support them yet.
-        enableReports: emailsEnabled,
-    };
-})(EmailsParameters);
+export default AlertsAndReports;
