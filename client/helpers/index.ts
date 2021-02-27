505eb04a70108068a88dde1c48291a9bf0329833
Benjamin Bouvier <public@benj.me>
2021-02-27 23:45:53 +0100

    client: don't trigger zombie children assertions when disabling demo mode (hack)

--- client/helpers/index.ts
+++ client/helpers/index.ts
@@ -45,13 +45,14 @@ const ASSERTS = true;
 
 export function assert(x: boolean, wat: string): asserts x {
     if (!x) {
-        const text = `Assertion error: ${wat ? wat : ''}\n${new Error().stack}`;
+        const shortText = `Assertion error: ${wat}`;
+        const text = `${shortText}\n${new Error().stack}`;
         if (process.env.NODE_ENV === 'test') {
             // During testing, errors should be fatal.
             throw new Error(text);
         }
         if (ASSERTS) {
-            window.alert(text);
+            window.alert(shortText);
             /* eslint-disable-next-line no-console */
             console.error(text);
         }
