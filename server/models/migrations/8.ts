721a7d371b130a356fd0d73226655e21f3c67074
Nicolas Frandeboeuf <mail@nicofrand.eu>
2021-04-08 20:17:29 +0200

    Fix budget constraint removal + add tests

--- server/models/migrations/8.ts
+++ server/models/migrations/8.ts
@@ -1,19 +1,29 @@
 import { MigrationInterface, QueryRunner, TableUnique } from 'typeorm';
 
+const COLUMN_NAMES = ['userId', 'year', 'month', 'categoryId'];
+
 export class UniqueBudget1608817798703 implements MigrationInterface {
     public async up(q: QueryRunner): Promise<void> {
         // Add the unique constraint.
-        await q.createUniqueConstraint(
-            'budget',
-            new TableUnique({ columnNames: ['userId', 'year', 'month', 'categoryId'] })
-        );
+        await q.createUniqueConstraint('budget', new TableUnique({ columnNames: COLUMN_NAMES }));
     }
 
     public async down(q: QueryRunner): Promise<void> {
-        // Remove the unique constraint.
-        await q.dropUniqueConstraint(
-            'budget',
-            new TableUnique({ columnNames: ['userId', 'year', 'month', 'categoryId'] })
-        );
+        // Remove the unique constraint. Unfortunately we cannot rely on a previously defined
+        // constraint name to remove it, as a random name will be forced, so we have to compare the
+        // column names.
+        const table = await q.getTable('budget');
+        if (table) {
+            for (const uniqueConstraint of table.uniques) {
+                if (
+                    uniqueConstraint.name &&
+                    uniqueConstraint.columnNames instanceof Array &&
+                    uniqueConstraint.columnNames.length === COLUMN_NAMES.length &&
+                    uniqueConstraint.columnNames.every(col => COLUMN_NAMES.includes(col))
+                ) {
+                    await q.dropUniqueConstraint('budget', uniqueConstraint.name);
+                }
+            }
+        }
     }
 }
