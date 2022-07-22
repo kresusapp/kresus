a10706168e50a4145c602c9d32c7b7541cb528a8
Nicolas Frandeboeuf <mail@nicofrand.eu>
2022-07-22 14:46:20 +0000

    Disable old migration to remove vendorId update in account

--- server/models/data-migrations/banks-20220609.ts
+++ server/models/data-migrations/banks-20220609.ts
@@ -10,23 +10,5 @@ export async function updateBanks(): Promise<void> {
 
     log.info('Migration is obsolete, property was removed from model.');
 
-    await manager.update(
-        Account,
-        {
-            vendorId: 'bnporc',
-            ...userCondition,
-        },
-        { vendorId: 'bnp' }
-    );
-
-    await manager.update(
-        Account,
-        {
-            vendorId: 'cmmc',
-            ...userCondition,
-        },
-        { vendorId: 'creditmutuel' }
-    );
-
     log.info('Finished running data migration on banks (2022-06-09)');
 }
