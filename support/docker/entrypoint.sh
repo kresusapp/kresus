0b9f3cea48644c317c5b934fb12b278afdbfeb75
Benjamin Bouvier <public@benj.me>
2023-03-05 11:47:34 +0000

    [docker] install woob from `pipy` + upgrade docker base to node 18

--- support/docker/entrypoint.sh
+++ support/docker/entrypoint.sh
@@ -16,22 +16,9 @@ echo "[ENTRYPOINT] Setting fake values for git config..."
 git config --global user.email kresus@example.com
 git config --global user.name "Kresus Root"
 
-cd /woob
-if [ ! -d /woob/.git ]; then
-    echo "[ENTRYPOINT] Installing woob..."
-    git clone --depth 1 https://gitlab.com/woob/woob.git .
-    echo "[ENTRYPOINT] Done installing."
-else
-    echo "[ENTRYPOINT] Updating woob..."
-    git pull || echo "Couldn't update; maybe the Woob's server is unreachable?"
-    echo "[ENTRYPOINT] Done updating."
-fi
-
-echo "[ENTRYPOINT] Updating Woob dependencies..."
-python ./setup.py requirements > /tmp/requirements.txt
-pip install -r /tmp/requirements.txt
-rm /tmp/requirements.txt
-echo "[ENTRYPOINT] Done updating Woob dependencies."
+echo "[ENTRYPOINT] Installing or upgrading woob..."
+pip install -U woob
+echo "[ENTRYPOINT] Done installing."
 
 if [ -z $IS_NIGHTLY ]; then
     echo "[ENTRYPOINT] Trying to update kresus..."
@@ -41,9 +28,6 @@ fi
 
 echo "[ENTRYPOINT] Changing rights on user home directory..."
 chown -R user:user /home/user
-
-echo "[ENTRYPOINT] Changing rights on /woob directory..."
-chown -R user:user /woob
 
 # Change config rights
 chown user:user /opt/config.ini
