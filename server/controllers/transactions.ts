kresusapp/kresus

commit 5b357c6c69a77f6f1024ee18eb9733edfcf851a8

When creating a transaction, allow more fields to be prefilled

server/controllers/transactions.ts
@@ -162,10 +162,10 @@ export async function create(req: IdentifiedRequest<Transaction>, res: express.R
             }
         }
 
-        // We fill the missing fields.
-        transaction.rawLabel = transaction.label;
-        transaction.importDate = new Date();
-        transaction.debitDate = transaction.date;
+        // We fill potentially missing fields.
+        transaction.rawLabel = transaction.rawLabel || transaction.label;
+        transaction.importDate = transaction.importDate || new Date();
+        transaction.debitDate = transaction.debitData || transaction.date;
         transaction.createdByUser = true;
         if (
             typeof transaction.type !== 'undefined' &&
@@ -173,18 +173,18 @@ export async function create(req: IdentifiedRequest<Transaction>, res: express.R
         ) {
             transaction.isUserDefinedType = true;
         }
-        const op = await Transaction.create(userId, transaction);
+        const newTransaction = await Transaction.create(userId, transaction);
 
         // Send back the transaction as well as the (possibly) updated account balance.
-        const account = await Account.find(userId, op.accountId);
+        const account = await Account.find(userId, newTransaction.accountId);
         if (!account) {
             throw new KError('bank account not found', 404);
         }
 
         res.status(201).json({
-            transaction: op,
+            transaction: newTransaction,
             accountBalance: account.balance,
-            accountId: op.accountId,
+            accountId: newTransaction.accountId,
         });
     } catch (err) {
         asyncErr(res, err, 'when creating transaction for a bank account');

commit 57d62c0f9d632e956d46d1321e7d1db74e3e785e

Fix typo (regression from 5b357c6c69a77f6f1024ee18eb9733edfcf851a8) on...

server/controllers/transactions.ts
@@ -165,7 +165,7 @@ export async function create(req: IdentifiedRequest<Transaction>, res: express.R
         // We fill potentially missing fields.
         transaction.rawLabel = transaction.rawLabel || transaction.label;
         transaction.importDate = transaction.importDate || new Date();
-        transaction.debitDate = transaction.debitData || transaction.date;
+        transaction.debitDate = transaction.debitDate || transaction.date;
         transaction.createdByUser = true;
         if (
             typeof transaction.type !== 'undefined' &&