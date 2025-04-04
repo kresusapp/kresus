ee978a2b1d466030a604082043317fd8f8aa213f
Nicolas Frandeboeuf <mail@nicofrand.eu>
2025-04-04 13:22:11 +0000

    fix(server): in migration 7, only select useful columns to avoid request with columns not yet existing in database but present in model

--- server/models/migrations/7.ts
+++ server/models/migrations/7.ts
@@ -8,7 +8,9 @@ import { Budget } from '..';
 
 export class RemoveDuplicateBudgets1608817776804 implements MigrationInterface {
     public async up(q: QueryRunner): Promise<void> {
-        const allBudgets = await q.manager.find(Budget);
+        const allBudgets = await q.manager.find(Budget, {
+            select: ['id', 'userId', 'year', 'month', 'categoryId']
+        });
 
         const setOfUniqueBudgetKeys = new Set<string>();
         const budgetIdsToDelete: number[] = [];
