4662972a32e3622b457b3b79bda677aaed1f0860
Benjamin Bouvier <benjamin@bouvier.cc>
2025-09-11 14:07:37 +0200

    Build;

--- build/server/models/migrations/23.js
+++ build/server/models/migrations/23.js
@@ -7,7 +7,6 @@ exports.AddViews1734262035140 = void 0;
 const typeorm_1 = require("typeorm");
 const helpers_1 = require("../helpers");
 const accounts_1 = __importDefault(require("../entities/accounts"));
-const views_1 = __importDefault(require("../entities/views"));
 class AddViews1734262035140 {
     async up(q) {
         await q.createTable(new typeorm_1.Table({
@@ -49,16 +48,20 @@ class AddViews1734262035140 {
         }));
         // For each existing account, create a view with this account.
         const allAccounts = await q.manager.find(accounts_1.default);
-        allAccounts.forEach(async (acc) => {
-            await views_1.default.create(acc.userId, {
-                label: acc.customLabel || acc.label,
-                accounts: [
-                    {
-                        accountId: acc.id,
-                    },
-                ],
-            });
-        });
+        for (const acc of allAccounts) {
+            // Don't use View.create: TypeORM's entity manager (q.manager) uses the current database
+            // schema, which is only updated after the migration finishes, so the view table is not
+            // yet known to the ORM, View.create will fail with "relation does not exist".
+            // Insert into view table
+            const result = await q.query(`INSERT INTO view ("userId", "label", "createdByUser") VALUES ($1, $2, $3) RETURNING id`, [acc.userId, acc.customLabel || acc.label, false]);
+            // Postgresql returns an array whereas SQLite return the inserted id.
+            const viewId = result instanceof Array ? result[0].id : result;
+            // Insert into view-accounts table
+            await q.query(`INSERT INTO "view-accounts" ("viewId", "accountId") VALUES ($1, $2)`, [
+                viewId,
+                acc.id,
+            ]);
+        }
     }
     async down(q) {
         await q.dropTable('view-accounts');
