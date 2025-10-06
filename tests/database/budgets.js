kresusapp/kresus

commit 276c8d9d2dbb22a7fae3b8dfccd93ceb8c7faaba

feat(views): Rename views automatically when an account is renamed

server/models/entities/accounts.ts
@@ -7,6 +7,7 @@ import {
     ManyToOne,
     Repository,
     AfterInsert,
+    AfterUpdate,
 } from 'typeorm';
 
 import { getRepository } from '..';
@@ -178,6 +179,17 @@ export default class Account {
         });
     }
 
+    @AfterUpdate()
+    async renameAssociatedViews() {
+        const newLabel = this.customLabel || this.label;
+        const allViews = await View.all(this.userId);
+        for (const view of allViews) {
+            if (view.accounts.length === 1 && view.accounts[0].accountId === this.id) {
+                await View.update(view.userId, view.id, { label: newLabel });
+            }
+        }
+    }
+
     static async ensureBalance(account: Account): Promise<void> {
         // If there is no balance for an account, compute one based on the initial amount and the
         // transactions.
@@ -253,7 +265,13 @@ export default class Account {
         accountId: number,
         attributes: Partial<Account>
     ): Promise<Account> {
-        await Account.repo().update({ userId, id: accountId }, attributes);
-        return unwrap(await Account.find(userId, accountId));
+        // Do not use Account.repo().update as it would not trigger the AfterUpdate hook.
+        // See https://github.com/typeorm/typeorm/issues/5385
+        const account = unwrap(await Account.find(userId, accountId));
+
+        Object.assign(account, attributes);
+        await Account.repo().save(account);
+
+        return account;
     }
 }

tests/database/views.js
@@ -10,12 +10,12 @@ describe('Views database CRUD tests', () => {
         USER_ID = process.kresus.user.id;
     });
 
-    let livretA, compteCheque, compteJoint;
+    let classicAccess, livretA, compteCheque, compteJoint;
     before(async () => {
         await Access.destroyAll(USER_ID);
         await View.destroyAll(USER_ID);
 
-        const classicAccess = await Access.create(USER_ID, {
+        classicAccess = await Access.create(USER_ID, {
             login: 'login',
             password: 'password',
             vendorId: 'whatever',
@@ -111,5 +111,32 @@ describe('Views database CRUD tests', () => {
         assert(views.some(v => v.label === compteJoint.label));
         assert(views.some(v => v.label === 'Look ma, I did this'));
         assert(views.some(v => v.label === 'Again and again'));
+    });
+
+    it('should rename the associated view when an account is renamed', async () => {
+        await Account.destroyAll(USER_ID);
+        await View.destroyAll(USER_ID);
+
+        const accToRename = await Account.create(USER_ID, {
+            accessId: classicAccess.id,
+            vendorAccountId: 55555,
+            label: 'Badly named',
+            initialBalance: 300,
+            importDate: new Date(),
+            lastCheckDate: 0,
+        });
+
+        let views = await View.all(USER_ID);
+        views.length.should.equal(1);
+        views[0].label.should.equal(accToRename.label);
+
+        // Rename the account
+        await Account.update(USER_ID, accToRename.id, {
+            customLabel: 'Better name',
+        });
+
+        views = await View.all(USER_ID);
+        views.length.should.equal(1);
+        views[0].label.should.equal('Better name');
     });
 });

commit 309a5ba279e615cf0fcfa716ad34eeeefa268443

fix: account updates also save the computed balance (regression from 276c8d9d2dbb22a7fae3b8dfccd93ceb8c7faaba)

server/models/entities/accounts.ts
@@ -239,8 +239,10 @@ export default class Account {
         attributes: Partial<Account>
     ): Promise<Account> {
         // Do not use Account.repo().update as it would not trigger the AfterUpdate hook.
-        // See https://github.com/typeorm/typeorm/issues/5385
-        const account = unwrap(await Account.find(userId, accountId));
+        // See https://github.com/typeorm/typeorm/issues/5385.
+        // Do not use Account.find as this would set the balance to a computed value, which would
+        // then be saved by the Account.save call.
+        const account = unwrap(await Account.repo().findOne({ where: { userId, id: accountId } }));
 
         Object.assign(account, attributes);
         await Account.repo().save(account);

server/models/migrations/26.ts
@@ -1,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+import { resetManualBankAccountsBalance } from '../data-migrations/reset-manual-accounts-balance';
+
+// Same as migration 12, but a bug was introduce in version 0.23.0 that set a balance to
+// the manual bank accounts.
+export class ResetManualAccountsBalance1759743359421 implements MigrationInterface {
+    public async up(q: QueryRunner): Promise<void> {
+        await resetManualBankAccountsBalance(null, q.manager);
+    }
+
+    public async down(): Promise<void> {
+        // Empty
+    }
+}

tests/.eslintrc.yml
@@ -5,6 +5,7 @@
         "describe": true
         "beforeEach": true
         "before": true
+        "after": true
 
     rules:
         max-nested-callbacks:

tests/database/account.js
@@ -1,6 +1,6 @@
 import should from 'should';
 
-import { Access, Account } from '../../server/models';
+import { Access, Account, View } from '../../server/models';
 import { importData } from '../../server/controllers/all';
 
 describe('Account model API', () => {
@@ -102,11 +102,12 @@ describe('Account model API', () => {
         USER_ID = process.kresus.user.id;
     });
 
-    describe('Account computeBalance', () => {
-        before(async () => {
-            await Access.destroyAll(USER_ID);
-        });
+    after(async () => {
+        await Access.destroyAll(USER_ID);
+        await View.destroyViewsWithoutAccounts(USER_ID);
+    });
 
+    describe('Account computeBalance', () => {
         it('should return the right amount', async () => {
             await importData(USER_ID, world);
 
@@ -122,6 +123,37 @@ describe('Account model API', () => {
             const result = await account[0].computeBalance(1337);
             const expected = world.transactions.reduce((sum, tr) => sum + tr.amount, 1337);
             result.should.be.approximately(expected, 0.01);
+        });
+    });
+
+    describe('Account update', () => {
+        it('should not override NULL balances', async () => {
+            const access = await Access.create(USER_ID, {
+                login: 'login',
+                password: 'password',
+                vendorId: 'manual',
+            });
+
+            const account = await Account.create(USER_ID, {
+                accessId: access.id,
+                vendorAccountId: 55555,
+                label: 'Whatever',
+                initialBalance: 0,
+                importDate: new Date(),
+                lastCheckDate: 0,
+            });
+
+            // Rename the account
+            await Account.update(USER_ID, account.id, {
+                customLabel: 'Better name',
+            });
+
+            // Account's balance in database should remain NULL
+            const updatedAccount = await Account.repo().findOneBy({
+                userId: USER_ID,
+                id: account.id,
+            });
+            should(updatedAccount.balance).be.null();
         });
     });
 });

tests/database/budgets.js
@@ -1,12 +1,45 @@
 import should from 'should';
 import { QueryFailedError } from 'typeorm';
-import { Budget, Category, View } from '../../server/models';
+import { Access, Budget, Category, View } from '../../server/models';
+import { importData } from '../../server/controllers/all';
 
 describe('Budgets model API', () => {
+    const world = {
+        accesses: [
+            {
+                id: 0,
+                vendorId: 'manual',
+                login: 'whatever-manual-acc--does-not-care',
+                customLabel: 'Optional custom label',
+            },
+        ],
+
+        accounts: [
+            {
+                id: 0,
+                accessId: 0,
+                vendorAccountId: 'manualaccount-randomid',
+                type: 'account-type.checking',
+                initialBalance: 0,
+                label: 'Compte Courant',
+                iban: 'FR4830066645148131544778523',
+                currency: 'EUR',
+                importDate: new Date('2019-01-01:00:00.000Z'),
+            },
+        ],
+    };
+
     let USER_ID = null;
-    before(() => {
+    before(async () => {
         // applyConfig must have already been called.
         USER_ID = process.kresus.user.id;
+
+        await importData(USER_ID, world);
+    });
+
+    after(async () => {
+        await Access.destroyAll(USER_ID);
+        await View.destroyViewsWithoutAccounts(USER_ID);
     });
 
     describe('Duplicates management', () => {