4aef24507c48fff7e79205e9a995bd92409f9a60
nicofrand <mail@nicofrand.eu>
2025-10-12 09:10:53 +0200

    fix: views' accountIds are not remapped after re-indexing in export (fix #1321)

--- tests/server/clean-data.js
+++ tests/server/clean-data.js
@@ -74,3 +74,85 @@ describe('Ensure transaction rules conditions are properly exported', () => {
         should.throws(func);
     });
 });
+
+describe('Ensure account ids are properly remapped after re-indexing', () => {
+    const world = {
+        accesses: [
+            {
+                id: 0,
+                vendorId: 'manual',
+            },
+        ],
+        accounts: [
+            {
+                id: 10,
+                name: 'Account 1',
+                balance: 100,
+                accessId: 0,
+                vendorId: 'manual',
+                currency: 'EUR',
+            },
+            {
+                id: 20,
+                name: 'Account 2',
+                balance: 200,
+                accessId: 0,
+                vendorId: 'manual',
+                currency: 'EUR',
+            },
+        ],
+        views: [
+            {
+                id: 0,
+                name: 'View 1',
+                accountIds: [10],
+            },
+            {
+                id: 1,
+                name: 'View 2',
+                accountIds: [20],
+            },
+            {
+                id: 2,
+                name: 'View 3',
+                accountIds: [10, 20],
+            },
+        ],
+        transactions: [
+            {
+                id: 0,
+                accountId: 10,
+                date: '2020-01-01',
+                label: 'Transaction 1',
+                amount: -10,
+                currency: 'EUR',
+            },
+            {
+                id: 1,
+                accountId: 20,
+                date: '2020-01-02',
+                label: 'Transaction 2',
+                amount: -20,
+                currency: 'EUR',
+            },
+        ],
+    };
+
+    it('Should be properly remapped in transactions', () => {
+        const betterWorld = cleanData(world);
+        const accountsIds = betterWorld.accounts.map(a => a.id);
+        for (const transaction of betterWorld.transactions) {
+            accountsIds.should.containEql(transaction.accountId);
+        }
+    });
+
+    it('Should be properly remapped in views', () => {
+        const betterWorld = cleanData(world);
+        const accountsIds = betterWorld.accounts.map(a => a.id);
+        for (const view of betterWorld.views) {
+            for (const accountId of view.accountIds) {
+                accountsIds.should.containEql(accountId);
+            }
+        }
+    });
+});
