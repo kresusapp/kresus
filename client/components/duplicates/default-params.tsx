d91cda70094a44b98743648fffd05bda17140776
nicofrand <mail@nicofrand.eu>
2025-12-23 16:49:44 +0100

    feat: remove precise duplicates threshold input in default parameters (fix #1325)

--- client/components/duplicates/default-params.tsx
+++ client/components/duplicates/default-params.tsx
@@ -3,33 +3,17 @@ import React, { useCallback, useState } from 'react';
 import { useKresusDispatch, useKresusState } from '../../store';
 import * as SettingsStore from '../../store/settings';
 import { translate as $t } from '../../helpers';
-import {
-    DUPLICATE_IGNORE_DIFFERENT_CUSTOM_FIELDS,
-    DUPLICATE_THRESHOLD,
-} from '../../../shared/settings';
+import { DUPLICATE_IGNORE_DIFFERENT_CUSTOM_FIELDS } from '../../../shared/settings';
 
 import { Switch, Form, Popform } from '../ui';
 import { useGenericError } from '../../hooks';
 
 const DefaultParameters = () => {
-    const initialThreshold = useKresusState(state =>
-        SettingsStore.get(state.settings, DUPLICATE_THRESHOLD)
-    );
     const initialIgnore = useKresusState(state =>
         SettingsStore.getBool(state.settings, DUPLICATE_IGNORE_DIFFERENT_CUSTOM_FIELDS)
     );
 
-    const [threshold, setThreshold] = useState(initialThreshold);
     const [ignore, setIgnore] = useState(initialIgnore);
-
-    const handleThresholdChange = useCallback(
-        (event: React.ChangeEvent<HTMLInputElement>) => {
-            if (event.target.value) {
-                setThreshold(event.target.value);
-            }
-        },
-        [setThreshold]
-    );
 
     const handleCustomLabelsCheckChange = useCallback(
         (checked: boolean) => {
@@ -41,15 +25,12 @@ const DefaultParameters = () => {
     const dispatch = useKresusDispatch();
     const handleSubmit = useGenericError(
         useCallback(async () => {
-            if (threshold !== initialThreshold) {
-                await dispatch(SettingsStore.set(DUPLICATE_THRESHOLD, threshold)).unwrap();
-            }
             if (ignore !== initialIgnore) {
                 await dispatch(
                     SettingsStore.setBool(DUPLICATE_IGNORE_DIFFERENT_CUSTOM_FIELDS, ignore)
                 ).unwrap();
             }
-        }, [dispatch, threshold, initialThreshold, initialIgnore, ignore])
+        }, [dispatch, initialIgnore, ignore])
     );
 
     return (
@@ -63,23 +44,6 @@ const DefaultParameters = () => {
             confirmClass="success"
             onConfirm={handleSubmit}>
             <h3>{$t('client.general.default_parameters')}</h3>
-
-            <Form.Input
-                id="default_threshold"
-                label={$t('client.similarity.default_threshold')}
-                help={$t('client.similarity.default_help')}>
-                <div className="input-with-addon block">
-                    <input
-                        id="duplicateThreshold"
-                        type="number"
-                        min="0"
-                        step="1"
-                        value={threshold}
-                        onChange={handleThresholdChange}
-                    />
-                    <span>{$t('client.units.hours')}</span>
-                </div>
-            </Form.Input>
 
             <Form.Input
                 inline={true}
