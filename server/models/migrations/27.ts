7cfca3e7507f8ff0cb904dd063a44d5f793fb438
nicofrand <mail@nicofrand.eu>
2026-02-02 20:18:46 +0100

    fix(server): disable SQLite foreign keys for migration 27 to prevent accounts deletion on access table update

--- server/models/migrations/27.ts
+++ server/models/migrations/27.ts
@@ -12,19 +12,39 @@ export class MoveLoginPasswordToFields1756391927839 implements MigrationInterfac
             SELECT "userId", "id", 'password', "password" FROM access WHERE "password" IS NOT NULL
         `);
 
-        // Remove columns from access. SQLite will remove the table and recreate it, removing access
+        // Remove columns from access.
+
+        // SQLite will remove the table and recreate it, removing access
         // fields in CASCADE. Disable the foreign keys checks beforehand and re-enable them afterwards.
+        // However, doing the "PRAGMA foreign_keys" thing in a transaction is a no-op
+        // (see https://www.sqlite.org/pragma.html), so we commit the transaction first.
         const dbType = queryRunner.connection.driver.options.type;
         const isSqlite = dbType === 'sqlite' || dbType === 'better-sqlite3';
+        let isInATransaction = false;
 
         if (isSqlite) {
-            await queryRunner.query('PRAGMA foreign_keys = OFF;');
+            try {
+                await queryRunner.commitTransaction();
+
+                isInATransaction = true;
+
+                // eslint-disable-next-line no-empty
+            } catch (ignore) {}
+
+            await queryRunner.query('PRAGMA foreign_keys = OFF');
         }
 
         await queryRunner.dropColumns('access', ['login', 'password']);
 
         if (isSqlite) {
-            await queryRunner.query('PRAGMA foreign_keys = ON;');
+            await queryRunner.query('PRAGMA foreign_keys = ON');
+
+            if (isInATransaction) {
+                try {
+                    await queryRunner.startTransaction();
+                    // eslint-disable-next-line no-empty
+                } catch (ignore) {}
+            }
         }
     }
 
